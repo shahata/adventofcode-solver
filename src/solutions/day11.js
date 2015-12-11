d753d9ffbba05449cf7343c38b5e09b7f674b95f
Shahar Talmi <shahart@wix.com>
2015-12-11 08:01:48 +0200

    refactor day 11

--- src/solutions/day11.js
+++ src/solutions/day11.js
@@ -1,24 +1,21 @@
 'use strict';
 
 export function day11(input) {
-  let abc = 'abcdefghijklmnopqrstuvwxyz';
-  let substrs = [];
-  for (let i = 0; abc.substr(i, 3).length === 3; i++) {
-    substrs.push(abc.substr(i, 3));
-  }
+  const abc = 'abcdefghijklmnopqrstuvwxyz';
+  const substrs = abc.split('').map((x, i) => abc.substr(i, 3)).filter(x => x.length === 3);
 
   function ok(s) {
     return s.match(/^[a-z]*$/) &&
-           s.match(/(.)\1.*(.)\2/) &&
            s.match(/^[^iol]*$/) &&
+           s.match(/(.)\1.*(.)\2/) &&
            substrs.some(sub => s.indexOf(sub) > -1);
   }
 
   function next(password) {
-    let s = parseInt(password, 36) + 1;
-    while (!ok(s.toString(36))) {
+    let s = parseInt(password, 36);
+    do {
       s++;
-    }
+    } while (!ok(s.toString(36)));
     return s.toString(36);
   }
 
