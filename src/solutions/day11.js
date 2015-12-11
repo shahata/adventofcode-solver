ce7bd2c90a84b37a07b02cac88b54a0b1bb3fdef
Shahar Talmi <shahart@wix.com>
2015-12-11 07:34:16 +0200

    added solution for day 11

--- src/solutions/day11.js
+++ src/solutions/day11.js
@@ -1,7 +1,28 @@
 'use strict';
 
 export function day11(input) {
-  let part1 = input;
-  let part2 = input;
+  let abc = 'abcdefghijklmnopqrstuvwxyz';
+  let substrs = [];
+  for (let i = 0; abc.substr(i, 3).length === 3; i++) {
+    substrs.push(abc.substr(i, 3));
+  }
+
+  function ok(s) {
+    return s.match(/^[a-z]*$/) &&
+           s.match(/(.)\1.*(.)\2/) &&
+           s.match(/^[^iol]*$/) &&
+           substrs.some(sub => s.indexOf(sub) > -1);
+  }
+
+  function next(password) {
+    let s = parseInt(password, 36) + 1;
+    while (!ok(s.toString(36))) {
+      s++;
+    }
+    return s.toString(36);
+  }
+
+  let part1 = next(input);
+  let part2 = next(part1);
   return [part1, part2];
 }
