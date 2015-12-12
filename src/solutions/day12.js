a569eb372098f5694cff6971a47544e673391f50
Shahar Talmi <shahart@wix.com>
2015-12-12 08:00:25 +0200

    added day12

--- src/solutions/day12.js
+++ src/solutions/day12.js
@@ -1,7 +1,18 @@
 'use strict';
 
 export function day12(input) {
-  let part1 = input;
-  let part2 = input;
+  function deepSum(obj, ignore) {
+    if (Array.isArray(obj)) {
+      return obj.reduce((sum, x) => sum + deepSum(x, ignore), 0);
+    } else if (typeof obj === 'object') {
+      let values = Object.keys(obj).map(x => obj[x]);
+      return ignore && values.indexOf(ignore) > -1 ? 0 : deepSum(values, ignore);
+    } else {
+      return typeof obj === 'number' ? obj : 0;
+    }
+  }
+
+  let part1 = deepSum(JSON.parse(input));
+  let part2 = deepSum(JSON.parse(input), 'red');
   return [part1, part2];
 }
