0db795fb5479124b953dcdc0f780393bd830639a
Shahar Talmi <shahart@wix.com>
2015-12-16 09:46:12 +0200

    indentation

--- src/solutions/index.js
+++ src/solutions/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-export {day1} from './day01';
-export {day2} from './day02';
-export {day3} from './day03';
-export {day4} from './day04';
-export {day5} from './day05';
-export {day6} from './day06';
-export {day7} from './day07';
-export {day8} from './day08';
-export {day9} from './day09';
+export {day1}  from './day01';
+export {day2}  from './day02';
+export {day3}  from './day03';
+export {day4}  from './day04';
+export {day5}  from './day05';
+export {day6}  from './day06';
+export {day7}  from './day07';
+export {day8}  from './day08';
+export {day9}  from './day09';
 export {day10} from './day10';
 export {day11} from './day11';
 export {day12} from './day12';
