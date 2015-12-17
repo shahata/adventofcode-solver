eaea322f2edf2cc82211de88bb9dd7e87f755294
Shahar Talmi <shahart@wix.com>
2015-12-17 09:45:01 +0200

    added day 17

--- src/solutions/day17.js
+++ src/solutions/day17.js
@@ -1,7 +1,18 @@
 'use strict';
 
 export function day17(input) {
-  let part1 = input;
-  let part2 = input;
+  let containers = input.split('\n').map(x => parseInt(x, 10));
+  let result = new Array(containers.length).fill(0);
+  let pad = result.join('');
+
+  for (let i = Math.pow(2, containers.length) - 1; i > 0; i--) {
+    let select = (pad + i.toString(2)).slice(-containers.length).split('').map(x => parseInt(x, 10));
+    if (select.map((x, index) => x * containers[index]).reduce((sum, x) => sum + x) === 150) {
+      result[select.filter(x => x).length - 1]++;
+    }
+  }
+
+  let part1 = result.reduce((sum, x) => sum + x);
+  let part2 = result.filter(x => x).shift();
   return [part1, part2];
 }
