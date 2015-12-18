0d3be090b1b2b918479905d2021220fb9365a8e1
Shahar Talmi <shahart@wix.com>
2015-12-18 10:02:58 +0200

    prepare for day 19

---
+++ src/solutions/day19.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export function day19(input) {
+  let part1 = input;
+  let part2 = input;
+  return [part1, part2];
+}
--- src/solutions/index.js
+++ src/solutions/index.js
@@ -18,3 +18,4 @@ export {day15} from './day15';
 export {day16} from './day16';
 export {day17} from './day17';
 export {day18} from './day18';
+export {day19} from './day19';
