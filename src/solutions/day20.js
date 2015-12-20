45807bf5965362721e7776452b4208b5f20ba1ad
Shahar Talmi <shahart@wix.com>
2015-12-20 09:34:59 +0200

    removed some unneeded vars

--- src/solutions/day20.js
+++ src/solutions/day20.js
@@ -2,11 +2,11 @@
 
 export function day20(input) {
   function divisors(x) {
-    let nums = [], sqrt = Math.sqrt(x);
-    for (let i = 1; i <= sqrt; i++) {
+    let nums = [];
+    for (let i = 1; i <= Math.sqrt(x); i++) {
       if (x % i === 0) {
         nums.push(i);
-        if (i !== sqrt) {
+        if (i !== Math.sqrt(x)) {
           nums.push(x / i);
         }
       }
@@ -16,15 +16,14 @@ export function day20(input) {
 
   input = parseInt(input);
   let part1, part2;
-  let target1 = input / 10, target2 = input / 11;
   for (let i = 1; !part1 || !part2; i++) {
-    let nums = divisors(i), ceil = Math.ceil(i / 50);
+    let nums = divisors(i);
     let sum = nums.reduce((sum, x) => sum + x, 0);
-    let sub = nums.filter(x => x < ceil).reduce((sum, x) => sum + x, 0);
-    if (!part1 && sum >= target1) {
+    let sub = nums.filter(x => x < Math.ceil(i / 50)).reduce((sum, x) => sum + x, 0);
+    if (!part1 && sum * 10 >= input) {
       part1 = i;
     }
-    if (!part2 && sum - sub >= target2) {
+    if (!part2 && (sum - sub) * 11 >= input) {
       part2 = i;
     }
   }
