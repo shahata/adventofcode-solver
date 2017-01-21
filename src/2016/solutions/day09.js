69762c155e4190b82cbd7fb00679b83443894eba
Shahar Talmi <shahart@wix.com>
2017-01-21 19:14:25 +0200

    solved day 9

---
+++ src/2016/solutions/day09.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function decodeLength(s, v2) {
+  let [, prefix, length, times, rest] = s.match(/^([^(]*)(?:\((\d+)x(\d+)\))?(.*)$/);
+  if (length) {
+    length = parseInt(length);
+    times = parseInt(times);
+    const repeat = times * (v2 ? decodeLength(rest.slice(0, length), v2) : length);
+    return prefix.length + repeat + decodeLength(rest.slice(length), v2);
+  } else {
+    return prefix.length;
+  }
+}
+
+function day(input) {
+  const part1 = decodeLength(input);
+  const part2 = decodeLength(input, true);
+  return [part1, part2];
+}
+
+module.exports = day;
---
+++ src/2016/solutions/day09.spec.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const day = require('./day09');
+const {expect} = require('chai');
+const input = fs.readFileSync(path.join(__dirname, 'day09.txt')).toString();
+
+describe('template', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(day('ADVENT').shift()).to.equal('ADVENT'.length);
+      expect(day('A(1x5)BC').shift()).to.equal('ABBBBBC'.length);
+      expect(day('(3x3)XYZ').shift()).to.equal('XYZXYZXYZ'.length);
+      expect(day('(6x1)(1x3)A').shift()).to.equal('(1x3)A'.length);
+      expect(day('X(8x2)(3x3)ABCY').shift()).to.equal('X(3x3)ABC(3x3)ABCY'.length);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(day(input).shift()).to.equal(74532);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(day('(3x3)XYZ').pop()).to.equal('XYZXYZXYZ'.length);
+      expect(day('X(8x2)(3x3)ABCY').pop()).to.equal('XABCABCABCABCABCABCY'.length);
+      expect(day('(27x12)(20x12)(13x14)(7x10)(1x12)A').pop()).to.equal(241920);
+      expect(day('(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN').pop()).to.equal(445);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(day(input).pop()).to.equal(11558231665);
+    });
+  });
+});
