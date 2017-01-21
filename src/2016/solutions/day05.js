c6d2c9f21fd3678490666adbb6c3f5a449d62cf1
Shahar Talmi <shahart@wix.com>
2017-01-21 03:55:00 +0200

    solved day 5

---
+++ src/2016/solutions/day05.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const md5 = require('md5');
+
+function day(input) {
+  console.log('Please wait patiently for result...');
+  let hash, count = 0;
+  let password1 = '';
+  const password2 = [];
+  let index = -1;
+  while (count < 8) {
+    do {
+      index++;
+      hash = md5(input + index);
+    } while (!hash.startsWith('00000'));
+    const i = parseInt(hash[5], 10);
+    password1 += hash[5];
+    if (i >= 0 && i <= 7 && !password2[i]) {
+      password2[i] = hash[6];
+      count++;
+    }
+  }
+  const part1 = password1.slice(0, 8);
+  const part2 = password2.join('');
+  return [part1, part2];
+}
+
+module.exports = day;
---
+++ src/2016/solutions/day05.spec.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+require('describe-heavy');
+const day = require('./day05');
+const {expect} = require('chai');
+const input = fs.readFileSync(`${__dirname}/day05.txt`).toString();
+
+describe.heavy('day05', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(day('abc').shift()).to.equal('18f47a30');
+    });
+
+    it('should work for part 1 input', () => {
+      expect(day(input).shift()).to.equal('f97c354d');
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(day('abc').pop()).to.equal('05ace8e3');
+    });
+
+    it('should work for part 2 input', () => {
+      expect(day(input).pop()).to.equal('863dde27');
+    });
+  });
+});
