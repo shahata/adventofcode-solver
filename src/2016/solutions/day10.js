626b6909e2ee649308a612c8d774c2af76428934
Shahar Talmi <shahart@wix.com>
2017-01-21 22:05:22 +0200

    solved day 10

---
+++ src/2016/solutions/day10.js
@@ -0,0 +1,43 @@
+'use strict';
+
+function day(input, low = 17, high = 61) {
+  let part1;
+
+  function assign(state, dest, index, value) {
+    Object.assign(state[dest], {[index]: (state[dest][index] || []).concat(value).sort((a, b) => a - b)});
+    if (dest === 'bots' && state[dest][index][0] === low && state[dest][index][1] === high) {
+      part1 = index;
+    }
+    return state;
+  }
+
+  function parse(line) {
+    const match = line.match(/^bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)$/);
+    const [, bot, lowDest, lowIndex, highDest, highIndex] = match;
+    return state => {
+      if (state.bots[bot] && state.bots[bot].length === 2) {
+        state = assign(state, `${lowDest}s`, parseInt(lowIndex, 10), state.bots[bot][0]);
+        state = assign(state, `${highDest}s`, parseInt(highIndex, 10), state.bots[bot][1]);
+        delete state.bots[bot];
+        return true;
+      }
+    };
+  }
+
+  function init(lines) {
+    return lines.reduce((state, line) => {
+      const [, value, bot] = line.match(/^value (\d+) goes to bot (\d+)$/);
+      return assign(state, 'bots', parseInt(bot, 10), parseInt(value, 10));
+    }, {bots: {}, outputs: {}});
+  }
+
+  const lines = input.split('\n');
+  const state = init(lines.filter(x => x.startsWith('value')));
+  const instructions = lines.filter(x => x.startsWith('bot')).map(parse);
+  while (instructions.some(update => update(state))) {/**/}
+
+  const part2 = state.outputs[0] * state.outputs[1] * state.outputs[2];
+  return [part1, part2];
+}
+
+module.exports = {day};
---
+++ src/2016/solutions/day10.spec.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const {day} = require('./day10');
+const {expect} = require('chai');
+const input = fs.readFileSync(path.join(__dirname, 'day10.txt')).toString();
+
+describe('day10', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(day(`value 5 goes to bot 2
+bot 2 gives low to bot 1 and high to bot 0
+value 3 goes to bot 1
+bot 1 gives low to output 1 and high to bot 0
+bot 0 gives low to output 2 and high to output 0
+value 2 goes to bot 2`, 2, 5).shift()).to.equal(2);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(day(input).shift()).to.equal(181);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(day(`value 5 goes to bot 2
+bot 2 gives low to bot 1 and high to bot 0
+value 3 goes to bot 1
+bot 1 gives low to output 1 and high to bot 0
+bot 0 gives low to output 2 and high to output 0
+value 2 goes to bot 2`, 2, 5).pop()).to.equal(30);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(day(input).pop()).to.equal(12567);
+    });
+  });
+});
