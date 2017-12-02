fa27c5d4a51973b397de592fd707ddf94d213cf3
Shahar Talmi <shahar.talmi@gmail.com>
2017-12-02 21:17:33 +0200

    solved day17

---
+++ src/2016/solutions/day17.js
@@ -0,0 +1,36 @@
+const md5 = require('md5');
+
+function getNeighbors(point, [U, D, L, R]) {
+  return [
+    U && {x: point.x, y: point.y - 1, path: point.path + 'U'},
+    D && {x: point.x, y: point.y + 1, path: point.path + 'D'},
+    L && {x: point.x - 1, y: point.y, path: point.path + 'L'},
+    R && {x: point.x + 1, y: point.y, path: point.path + 'R'}
+  ].filter(p => p && p.x >= 0 && p.y >= 0 && p.x <= 3 && p.y <= 3);
+}
+
+function solve(input, shortest = true) {
+  let queue = [{x: 0, y: 0, path: ''}], max = -1;
+  while (queue.length > 0) {
+    const next = queue.shift();
+    if (next.x === 3 && next.y === 3) {
+      if (shortest) {
+        return next.path;
+      } else {
+        max = Math.max(max, next.path.length);
+      }
+    } else {
+      const doors = md5(input + next.path).slice(0, 4).split('').map(x => 'bcdef'.includes(x));
+      queue = queue.concat(getNeighbors(next, doors));
+    }
+  }
+  return max;
+}
+
+function day(input) {
+  const part1 = solve(input, true);
+  const part2 = solve(input, false);
+  return [part1, part2];
+}
+
+module.exports = {day};
