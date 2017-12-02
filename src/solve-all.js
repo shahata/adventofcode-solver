60c4f7ae984ebbc9e7de61fa9749f5e5634a782f
Shahar Talmi <shahar.talmi@gmail.com>
2017-12-02 11:01:05 +0200

    added create solver

--- src/solve-all.js
+++ src/solve-all.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const fetch = require('node-fetch');
+const inquirer = require('inquirer');
 
-async function getDayInput(year, index, session) {
+async function getDayInput(year, day, session) {
+  const index = parseInt(day.slice(-2));
   const response = await fetch(`http://adventofcode.com/${year}/day/${index}/input`, {
     headers: {
       Cookie: `session=${session}`
@@ -11,9 +13,9 @@ async function getDayInput(year, index, session) {
   return await response.text();
 }
 
-async function solveDay(year, index, fn, session) {
-  const input = await getDayInput(year, index, session);
-  console.log(`Solution for day ${index}!!!`);
+async function solveDay(year, day, fn, session) {
+  const input = await getDayInput(year, day, session);
+  console.log(`Solution for ${day}!!!`);
   console.log('----------------------------');
   const result = fn(input.trim());
   console.log(`Part1: ${result[0]}`);
@@ -23,10 +25,10 @@ async function solveDay(year, index, fn, session) {
 
 function getSolvers(year) {
   try {
-    const folder = path.join(__dirname, `${year}/solutions`);
+    const folder = path.join(__dirname, year, 'solutions');
     const days = fs.readdirSync(folder).filter(x => x.match(/^day\d+\.js$/));
     return days.reduce((obj, day) => Object.assign(obj, {
-      [day]: require(`./${path.join(`${year}/solutions`, day)}`).day
+      [day.split('.').shift()]: require(`./${path.join(`${year}/solutions`, day)}`).day
     }), {});
   } catch (e) {
     console.error(e);
@@ -35,13 +37,35 @@ function getSolvers(year) {
   }
 }
 
+async function createSolver(year, day, session) {
+  const answers = await inquirer.prompt([{type: 'confirm', name: 'create', message: `Create solver ${year}/${day}?`}]);
+  if (answers.create) {
+    const prefix = path.join(__dirname, year, 'solutions', day);
+    const template = path.join(__dirname, 'template', 'template');
+    fs.writeFileSync(`${prefix}.js`, fs.readFileSync(`${template}.template`).toString().replace('template', day));
+    console.log(`Created ${prefix}.js`);
+    fs.writeFileSync(`${prefix}.spec.js`, fs.readFileSync(`${template}.spec.template`).toString().replace('template', day));
+    console.log(`Created ${prefix}.spec.js`);
+    fs.writeFileSync(`${prefix}.txt`, await getDayInput(year, day, session));
+    console.log(`Created ${prefix}.txt`);
+  }
+}
+
 async function solveAll(session) {
   const solvers = getSolvers(process.argv[2]);
-  const keys = Object.keys(solvers);
-  for (let i = 0; i < keys.length; i++) {
-    if (!process.argv[3] || parseInt(process.argv[3], 10) === i + 1) {
-      await solveDay(process.argv[2], i + 1, solvers[keys[i]], session);
+  if (process.argv[3]) {
+    const dayName = num => `day${num.length === 1 ? '0' : ''}${num}`;
+    const day = dayName(process.argv[3]);
+    if (solvers[day]) {
+      await solveDay(process.argv[2], day, solvers[day], session);
+    } else {
+      await createSolver(process.argv[2], day, session);
     }
+  } else {
+    await Object.keys(solvers).reduce(async (prev, key) => {
+      await prev;
+      await solveDay(process.argv[2], key, solvers[key], session);
+    }, Promise.resolve());
   }
 }
 
