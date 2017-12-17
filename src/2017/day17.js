86ddf7edde281485d3fe1bc60d3ffce40e8387ec
Shahar Talmi <shahar.talmi@gmail.com>
2017-12-17 07:47:12 +0200

    solved day 17

---
+++ src/2017/day17.js
@@ -0,0 +1,43 @@
+function add1({steps, current, state}, number) {
+  current = ((current + steps) % number) + 1;
+  state.splice(current, 0, number);
+  return {steps, current, state};
+}
+
+function part1(input, lookFor = 2017) {
+  let spinner = {
+    steps: parseInt(input, 10),
+    current: 0,
+    state: [0]
+  };
+  for (let i = 1; i <= 2017; i++) {
+    spinner = add1(spinner, i);
+  }
+  return spinner.state[(spinner.state.indexOf(lookFor) + 1) % spinner.state.length];
+}
+
+function add2({steps, current, zeroPosition, result}, number) {
+  current = ((current + steps) % number) + 1;
+  if (current === zeroPosition + 1) {
+    result = number;
+  } else if (current <= zeroPosition) {
+    zeroPosition++;
+  }
+  return {steps, current, zeroPosition, result};
+}
+
+function part2(input, times = 50 * 1000 * 1000) {
+  let spinner = {
+    steps: parseInt(input, 10),
+    current: 0,
+    zeroPosition: 0,
+    result: undefined
+  };
+  for (let i = 1; i <= times; i++) {
+    spinner = add2(spinner, i);
+  }
+  return spinner.result;
+}
+
+const day = input => [part1(input), part2(input)];
+module.exports = {day, part1, part2};
---
+++ src/2017/day17.spec.js
@@ -0,0 +1,27 @@
+const {part1, part2} = require('./day17');
+const {expect} = require('chai');
+const readInput = require('../read-input');
+const input = readInput(__filename);
+
+describe('day17 2017', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(part1('3')).to.equal(638);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).to.equal(2000);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(part1('3', 0)).to.equal(part2('3', 2017));
+      expect(part1(input, 0)).to.equal(part2(input, 2017));
+    });
+
+    itHeavy('should work for part 2 input', () => {
+      expect(part2(input)).to.equal(10242889);
+    });
+  });
+});
