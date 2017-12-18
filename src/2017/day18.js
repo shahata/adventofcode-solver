d4418a82347e455cf6bc54686901867b69fa29e0
Shahar Talmi <shahar.talmi@gmail.com>
2017-12-18 08:45:15 +0200

    solved day18

---
+++ src/2017/day18.js
@@ -0,0 +1,84 @@
+function getter(state, p) {
+  if (typeof p === 'string') {
+    if (state[p] === undefined) {
+      state[p] = 0;
+    }
+    return state[p];
+  } else {
+    return p;
+  }
+}
+
+function parse(input, ops2 = {}) {
+  const ops = Object.assign({
+    snd: p1 => state => state.sound = getter(state, p1),
+    set: (p1, p2) => state => state[p1] = getter(state, p2),
+    add: (p1, p2) => state => state[p1] = getter(state, p1) + getter(state, p2),
+    mul: (p1, p2) => state => state[p1] = getter(state, p1) * getter(state, p2),
+    mod: (p1, p2) => state => state[p1] = getter(state, p1) % getter(state, p2),
+    rcv: p1 => state => getter(p1) !== 0 ? state.recovered = state.sound : null,
+    jgz: (p1, p2) => state => getter(state, p1) > 0 ? state.instruction += getter(state, p2) - 1 : null
+  }, ops2);
+
+  return input.split('\n').map(str => {
+    let [cmd, p1, p2] = str.split(' ');
+    p1 = p1.match(/^-?\d+$/) ? parseInt(p1, 10) : p1;
+    p2 = p2 && p2.match(/^-?\d+$/) ? parseInt(p2, 10) : p2;
+    return ops[cmd](p1, p2);
+  });
+}
+
+function parse2(input) {
+  return parse(input, {
+    snd: p1 => state => {
+      state.send.push(getter(state, p1));
+      state.sent++;
+    },
+    rcv: p1 => state => {
+      if (state.queue.length > 0) {
+        state[p1] = state.queue.shift();
+      } else {
+        throw 'waiting...';
+      }
+    }
+  });
+}
+
+function part1(input) {
+  const commands = parse(input);
+  const state = {instruction: 0};
+  while (state.instruction < commands.length && !state.recovered) {
+    commands[state.instruction](state);
+    state.instruction++;
+  }
+  return state.recovered;
+}
+
+function execute(commands, state) {
+  let run = 0;
+  try {
+    while (state.instruction < commands.length) {
+      commands[state.instruction](state);
+      state.instruction++;
+      run++;
+    }
+  } catch (e) {}
+  return run;
+}
+
+function part2(input) {
+  const commands = parse2(input);
+  const state1 = {instruction: 0, queue: [], sent: 0, p: 0};
+  const state2 = {instruction: 0, queue: [], sent: 0, p: 1};
+  state1.send = state2.queue;
+  state2.send = state1.queue;
+
+  let running = true;
+  while (running) {
+    running = execute(commands, state1) > 0 || execute(commands, state2) > 0;
+  }
+  return state2.sent;
+}
+
+const day = input => [part1(input), part2(input)];
+module.exports = {day, part1, part2};
---
+++ src/2017/day18.spec.js
@@ -0,0 +1,35 @@
+const {part1, part2} = require('./day18');
+const {expect} = require('chai');
+const readInput = require('../read-input');
+const input = readInput(__filename);
+
+describe('day18 2017', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(part1(`set a 1
+add a 2
+mul a a
+mod a 5
+snd a
+set a 0
+rcv a
+jgz a -1
+set a 1
+jgz a -2`)).to.equal(4);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).to.equal(4601);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      // expect(part2('1')).to.equal('1');
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).to.equal(6858);
+    });
+  });
+});
