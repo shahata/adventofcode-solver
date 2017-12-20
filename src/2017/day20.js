c15eb7bf9d94adf6da845f2c006485acdaef8f1b
Shahar Talmi <shahar.talmi@gmail.com>
2017-12-20 10:02:11 +0200

    solved day20

---
+++ src/2017/day20.js
@@ -0,0 +1,67 @@
+function positionAfter(p, v, a, t) {
+  return p + (t * ((2 * (v + a)) + ((t - 1) * a)) / 2);
+}
+
+function particleAfter(particle, t) {
+  let {i, p, v, a} = particle;
+  p = p.map((x, i) => positionAfter(p[i], v[i], a[i], t));
+  return {i, p, v, a};
+}
+
+function byPosition(a, b) {
+  if (a.p[0] < b.p[0]) {
+    return -1;
+  } else if (a.p[0] > b.p[0]) {
+    return 1;
+  } else if (a.p[1] < b.p[1]) {
+    return -1;
+  } else if (a.p[1] > b.p[1]) {
+    return 1;
+  } else if (a.p[2] < b.p[2]) {
+    return -1;
+  } else if (a.p[2] > b.p[2]) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function findCollisions(particles) {
+  const collide = (a, b) => b && a.p.every((x, i) => x === b.p[i]);
+  return particles.sort(byPosition).filter((x, i, a) => collide(x, a[i - 1]) || collide(x, a[i + 1]));
+}
+
+function parse(input) {
+  return input.split('\n').map((line, i) => {
+    const parser = /^p=<(-?\d+),(-?\d+),(-?\d+)>, v=<(-?\d+),(-?\d+),(-?\d+)>, a=<(-?\d+),(-?\d+),(-?\d+)>$/;
+    const numbers = line.match(parser).slice(1).map(x => parseInt(x, 10));
+    return {i, p: numbers.slice(0, 3), v: numbers.slice(3, 6), a: numbers.slice(6, 9)};
+  });
+}
+
+function closest(particles) {
+  const distances = particles.map(x => Math.abs(x.p[0]) + Math.abs(x.p[1]) + Math.abs(x.p[2]));
+  const min = Math.min(...distances);
+  return distances.indexOf(min);
+}
+
+function after(particles, t) {
+  return particles.map(x => particleAfter(x, t));
+}
+
+function part1(input) {
+  const particles = after(parse(input), 1000);
+  return closest(particles);
+}
+
+function part2(input) {
+  const particles = parse(input);
+  for (let t = 0; t < 1000; t++) {
+    const collisions = findCollisions(after(particles, t));
+    collisions.forEach(x => delete particles[x.i]);
+  }
+  return particles.filter(x => x).length;
+}
+
+const day = input => [part1(input), part2(input)];
+module.exports = {day, part1, part2};
---
+++ src/2017/day20.spec.js
@@ -0,0 +1,30 @@
+const {part1, part2} = require('./day20');
+const {expect} = require('chai');
+const readInput = require('../read-input');
+const input = readInput(__filename);
+
+describe('day20 2017', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(part1(`p=<3,0,0>, v=<2,0,0>, a=<-1,0,0>
+p=<4,0,0>, v=<0,0,0>, a=<-2,0,0>`)).to.equal(0);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).to.equal(376);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(part2(`p=<-6,0,0>, v=<3,0,0>, a=<0,0,0>
+p=<-4,0,0>, v=<2,0,0>, a=<0,0,0>
+p=<-2,0,0>, v=<1,0,0>, a=<0,0,0>
+p=<3,0,0>, v=<-100,0,0>, a=<0,0,0>`)).to.equal(1);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).to.equal(574);
+    });
+  });
+});
