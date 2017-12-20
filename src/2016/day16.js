aa918f6b90f90070fc175b10fcb9af9a3c71083f
Shahar Talmi <shahar.talmi@gmail.com>
2017-12-20 21:07:45 +0200

    optimizations

--- src/2016/day16.js
+++ src/2016/day16.js
@@ -1,22 +1,21 @@
 function generate(input, size) {
+  input = input.split('');
   while (input.length < size) {
     input = dragonCurve(input);
   }
-  return input.slice(0, size);
+  return input.slice(0, size).join('');
 }
 
 function dragonCurve(a) {
-  const b = a.split('').reverse().map(x => x === '0' ? '1' : '0').join('');
-  return `${a}0${b}`;
+  const b = a.map(x => x === '0' ? '1' : '0').reverse();
+  return a.concat('0', b);
 }
 
 function checksum(data) {
-  const result = data.match(/../g).map(x => x === '00' || x === '11' ? '1' : '0').join('');
-  if (result.length % 2 === 0) {
-    return checksum(result);
-  } else {
-    return result;
+  while (data.length % 2 === 0) {
+    data = data.replace(/../g, x => x === '00' || x === '11' ? '1' : '0');
   }
+  return data;
 }
 
 const part1 = (input, size = 272) => checksum(generate(input, size));
