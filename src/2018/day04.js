f3a1df0a99e3fefaf9cc971da1b482d56460a94a
Shahar Talmi <shahart@wix.com>
2018-12-04 14:57:32 +0200

    refactor

--- src/2018/day04.js
+++ src/2018/day04.js
@@ -3,26 +3,22 @@ function calcSleep(input) {
     .split('\n')
     .sort()
     .map(x => {
-      const regex = /^\[([\d-]+) ([\d:]+)\] .* (#|asleep|up)(\d+)?/;
-      const [, date, time, verb, id] = x.match(regex);
-      return { date, time, verb, id };
+      const regex = /^\[[\d-]+ \d+:([\d]+)\] .* (#|asleep|up)(\d+)?/;
+      const [, time, verb, id] = x.match(regex);
+      return { time: parseInt(time), verb, id };
     });
   const sleep = new Map();
+  const initial = () => ({ total: 0, mins: new Map() });
   let current, start;
   lines.forEach(line => {
     if (line.verb === '#') {
       current = line.id;
     } else if (line.verb === 'asleep') {
-      start = { date: line.date, time: line.time };
+      start = line.time;
     } else {
-      const guard = sleep.get(current) || {
-        total: 0,
-        mins: new Map(),
-      };
-      const a = parseInt(line.time.split(':').pop());
-      const b = parseInt(start.time.split(':').pop());
-      for (let i = b; i < a; i++) {
-        guard.total += 1;
+      const guard = sleep.get(current) || initial();
+      for (let i = start; i < line.time; i++) {
+        guard.total++;
         guard.mins.set(i, (guard.mins.get(i) || 0) + 1);
       }
       sleep.set(current, guard);
@@ -43,21 +39,19 @@ function sleepyGuard(sleep, fn) {
     .pop();
 }
 
-function part1(input) {
+function checksum(input, fn) {
   const sleep = calcSleep(input);
-  const sleepy = sleepyGuard(sleep, (a, b) => a.total - b.total);
+  const sleepy = sleepyGuard(sleep, fn);
   const min = sleepyMin(sleepy[1]);
   return min[0] * sleepy[0];
 }
 
+function part1(input) {
+  return checksum(input, (a, b) => a.total - b.total);
+}
+
 function part2(input) {
-  const sleep = calcSleep(input);
-  const sleepy = sleepyGuard(
-    sleep,
-    (a, b) => sleepyMin(a)[1] - sleepyMin(b)[1],
-  );
-  const min = sleepyMin(sleepy[1]);
-  return min[0] * sleepy[0];
+  return checksum(input, (a, b) => sleepyMin(a)[1] - sleepyMin(b)[1]);
 }
 
 module.exports = { part1, part2 };
