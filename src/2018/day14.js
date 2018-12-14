2d83ab390e002611da6cd5ff5164e1c5f75d97fe
Shahar Talmi <shahart@wix.com>
2018-12-14 07:59:18 +0200

    solved day14

---
+++ src/2018/day14.js
@@ -0,0 +1,28 @@
+function cook(check) {
+  let [elf1, elf2] = [0, 1];
+  const board = [3, 7, 1, 0];
+  while (check(board)) {
+    elf1 = (elf1 + board[elf1] + 1) % board.length;
+    elf2 = (elf2 + board[elf2] + 1) % board.length;
+    const newRecipes = String(board[elf1] + board[elf2]).split('');
+    board.push(...newRecipes.map(x => parseInt(x)));
+  }
+  return board;
+}
+
+function part1(input) {
+  const count = parseInt(input);
+  const board = cook(board => board.length < count + 10);
+  return board.slice(count, count + 10).join('');
+}
+
+function part2(input) {
+  let suffix = '';
+  const board = cook(board => {
+    suffix = board.slice(-1 * input.length - 1).join('');
+    return !suffix.includes(input);
+  });
+  return board.length - input.length - (1 - suffix.indexOf(input));
+}
+
+module.exports = { part1, part2 };
