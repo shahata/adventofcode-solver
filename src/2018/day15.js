5ba5b176598095b4c862dedc32c6a632906c5c24
Shahar Talmi <shahart@wix.com>
2018-12-15 18:08:59 +0200

    refactor

--- src/2018/day15.js
+++ src/2018/day15.js
@@ -1,11 +1,10 @@
+const pos = ({ x, y }) => `${x},${y}`;
+const count = (units, type) => units.filter(u => u.type === type).length;
+
 function attackIfPossible(map, units, unit) {
+  const inRange = u => Math.abs(u.x - unit.x) + Math.abs(u.y - unit.y) === 1;
   const attack = units
-    .filter(
-      u =>
-        u.type !== unit.type &&
-        ((u.x === unit.x && Math.abs(u.y - unit.y) === 1) ||
-          (u.y === unit.y && Math.abs(u.x - unit.x) === 1)),
-    )
+    .filter(u => u.type !== unit.type && inRange(u))
     .sort((a, b) => a.hit - b.hit || a.y - b.y || a.x - b.x)
     .shift();
   if (attack) {
@@ -17,81 +16,58 @@ function attackIfPossible(map, units, unit) {
   return !!attack;
 }
 
-function neighbors(map, units, next, type) {
-  return [
+function neighbors(map, units, next, type, forbidden) {
+  const result = [
     { x: next.x, y: next.y - 1 },
     { x: next.x - 1, y: next.y },
     { x: next.x + 1, y: next.y },
     { x: next.x, y: next.y + 1 },
   ]
-    .filter(
-      p =>
-        map[p.y] &&
-        map[p.y][p.x] === '.' &&
-        !units.find(u => u.x === p.x && u.y === p.y && u.type === type),
-    )
-    .map(p => ({ ...p, path: next.path.concat({ ...p }) }));
-}
-
-function find(map, units, unit) {
-  const solutions = [];
-  const queue = [{ x: unit.x, y: unit.y, path: [] }];
-  const visited = new Set(`${unit.x},${unit.y}`);
-  while (queue.length) {
-    const next = queue.shift();
-    if (solutions.length > 0 && solutions[0].path.length < next.path.length) {
-      break;
-    }
-    if (
-      next.path.length > 0 &&
-      units.find(u => u.x === next.x && u.y === next.y)
-    ) {
-      solutions.push(next);
-    }
-    const options = neighbors(map, units, next, unit.type).filter(
-      n => !visited.has(`${n.x},${n.y}`),
-    );
-    options.forEach(n => visited.add(`${n.x},${n.y}`));
-    queue.push(...options);
-  }
-  if (solutions.length > 0) {
-    return solutions
-      .sort((a, b) => a.y - b.y || a.x - b.x)
-      .shift()
-      .path.shift();
-  }
+    .filter(p => map[p.y] && map[p.y][p.x] === '.' && !forbidden.has(pos(p)))
+    .map(p => ({ ...p, path: next.path.concat(p) }));
+  result.forEach(n => forbidden.add(pos(n)));
+  return result;
 }
 
 function moveIfPossible(map, units, unit) {
-  const move = find(map, units, unit);
-  if (move) {
+  let solutionLength = Infinity;
+  const solutions = [];
+  const queue = [{ x: unit.x, y: unit.y, path: [] }];
+  const forbidden = new Set(units.filter(u => u.type === unit.type).map(pos));
+  while (queue.length > 0 && queue[0].path.length <= solutionLength) {
+    const next = queue.shift();
+    if (units.find(u => pos(u) === pos(next) && u.type !== unit.type)) {
+      solutions.push(next);
+      solutionLength = next.path.length;
+    }
+    queue.push(...neighbors(map, units, next, unit.type, forbidden));
+  }
+  if (solutions.length > 0) {
+    const solution = solutions.sort((a, b) => a.y - b.y || a.x - b.x).shift();
+    const move = solution.path.shift();
     unit.x = move.x;
     unit.y = move.y;
   }
-  return !!move;
 }
 
 function turn(map, units) {
   units.sort((a, b) => a.y - b.y || a.x - b.x);
   for (let i = 0; i < units.length; i++) {
-    if (
-      units.filter(u => u.type === 'E').length === 0 ||
-      units.filter(u => u.type === 'G').length === 0
-    ) {
+    if (count(units, 'E') === 0 || count(units, 'G') === 0) {
       return false;
     }
     const unit = units[i];
     if (!attackIfPossible(map, units, unit)) {
-      if (moveIfPossible(map, units, unit)) {
-        attackIfPossible(map, units, unit);
-      }
+      moveIfPossible(map, units, unit);
+      attackIfPossible(map, units, unit);
     }
-    i = units.indexOf(unit);
+    i = units.indexOf(unit); //in case something was removed
   }
   return true;
 }
 
-function parse(input, elfBoost = 0) {
+function fight(input, elfBoost = 0) {
+  let i = 0;
   const units = [];
   const map = input.replace(/[EG]/g, '.').split('\n');
   input.split('\n').forEach((row, y) =>
@@ -103,36 +79,25 @@ function parse(input, elfBoost = 0) {
       }
     }),
   );
-  return { map, units };
+  while (turn(map, units)) {
+    i++;
+  }
+  return { i, units };
 }
 
 function part1(input) {
-  let i = 0;
-  const { map, units } = parse(input);
-  while (turn(map, units)) {
-    i++;
-  }
+  const { i, units } = fight(input);
   return i * units.reduce((sum, u) => (sum += u.hit), 0);
 }
 
-function play({ map, units }) {
-  let i = 0;
-  const elfCount = units.filter(x => x.type === 'E').length;
-  while (turn(map, units)) {
-    i++;
-  }
-  if (elfCount === units.filter(x => x.type === 'E').length) {
-    return i * units.reduce((sum, u) => (sum += u.hit), 0);
-  }
-}
-
 function part2(input) {
-  let result;
-  let elfBoost = 1;
-  while (!(result = play(parse(input, elfBoost)))) {
-    elfBoost++;
+  const elfCount = input.match(/E/g).length;
+  for (let elfBoost = 1; true; elfBoost++) {
+    const { i, units } = fight(input, elfBoost);
+    if (elfCount === count(units, 'E')) {
+      return i * units.reduce((sum, u) => (sum += u.hit), 0);
+    }
   }
-  return result;
 }
 
 module.exports = { part1, part2 };
