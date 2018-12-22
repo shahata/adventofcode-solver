5a7228befb11b85c966fa0ece0ab337f6b6ab80c
Shahar Talmi <shahart@wix.com>
2018-12-22 12:57:52 +0200

    added x2 optimization by removing junk from queue

--- src/2018/day22.js
+++ src/2018/day22.js
@@ -1,18 +1,13 @@
 let cache = {};
 const pos = ({ x, y }) => `${x},${y}`;
-const risk = (point, depth) => erosion(point, depth) % 3;
+const terrain = (point, depth) => erosion(point, depth) % 3;
 
 function erosion({ x, y }, depth) {
   let geo;
-  if (cache[pos({ x, y })] !== undefined) {
-    return cache[pos({ x, y })];
-  } else if (y === 0) {
-    geo = x * 16807;
-  } else if (x === 0) {
-    geo = y * 48271;
-  } else {
-    geo = erosion({ x: x - 1, y }, depth) * erosion({ x, y: y - 1 }, depth);
-  }
+  if (cache[pos({ x, y })] !== undefined) return cache[pos({ x, y })];
+  else if (y === 0) geo = x * 16807;
+  else if (x === 0) geo = y * 48271;
+  else geo = erosion({ x: x - 1, y }, depth) * erosion({ x, y: y - 1 }, depth);
   cache[pos({ x, y })] = (geo + depth) % 20183;
   return cache[pos({ x, y })];
 }
@@ -24,44 +19,42 @@ function parse(input) {
 
 function part1(input) {
   const { depth, target } = parse(input);
-  let sum = 0;
+  let risk = 0;
   cache = { [pos(target)]: 0 };
   for (let x = 0; x <= target.x; x++) {
     for (let y = 0; y <= target.y; y++) {
-      sum += risk({ x, y }, depth);
+      risk += terrain({ x, y }, depth);
     }
   }
-  return sum;
+  return risk;
 }
 
-function neighbors({ position, equip, time }, depth, target) {
+function neighbors({ point, equip, time }, depth, target) {
   const options = [];
-  const now = risk(position, depth);
+  const current = terrain(point, depth);
   const add = (p, e) =>
-    options.push({ position: p, equip: e, time: time + (e === equip ? 1 : 8) });
-  const positions = [
-    { x: position.x - 1, y: position.y + 0 },
-    { x: position.x + 1, y: position.y + 0 },
-    { x: position.x + 0, y: position.y - 1 },
-    { x: position.x + 0, y: position.y + 1 },
+    options.push({ point: p, equip: e, time: time + (e === equip ? 1 : 8) });
+  const points = [
+    { x: point.x - 1, y: point.y + 0 },
+    { x: point.x + 1, y: point.y + 0 },
+    { x: point.x + 0, y: point.y - 1 },
+    { x: point.x + 0, y: point.y + 1 },
   ].filter(p => p.x >= 0 && p.y >= 0);
-  positions.forEach(p => {
-    const type = risk(p, depth);
-    if (p.x === target.x && p.y === target.y) {
-      if (now !== 1 || equip === 'gear') add(p, 'torch');
-      else {
-        //neither -> gear -> walk -> torch
-        options.push({ position: p, equip: 'torch', time: time + 15 });
-      }
-    } else if (type === 0) {
-      if (now !== 1) add(p, 'torch');
-      if (now !== 2) add(p, 'gear');
-    } else if (type === 1) {
-      if (now !== 0) add(p, 'neither');
-      if (now !== 2) add(p, 'gear');
-    } else if (type === 2) {
-      if (now !== 1) add(p, 'torch');
-      if (now !== 0) add(p, 'neither');
+  points.forEach(point => {
+    const next = terrain(point, depth);
+    if (point.x === target.x && point.y === target.y) {
+      if (current !== 1 || equip === 'gear') add(point, 'torch');
+      //neither -> gear -> walk -> torch (7 + 1 + 7 = 15)
+      else options.push({ point, equip: 'torch', time: time + 15 });
+    } else if (next === 0) {
+      if (current !== 1) add(point, 'torch');
+      if (current !== 2) add(point, 'gear');
+    } else if (next === 1) {
+      if (current !== 0) add(point, 'neither');
+      if (current !== 2) add(point, 'gear');
+    } else if (next === 2) {
+      if (current !== 1) add(point, 'torch');
+      if (current !== 0) add(point, 'neither');
     }
   });
   return options;
@@ -70,22 +63,20 @@ function neighbors({ position, equip, time }, depth, target) {
 function part2(input) {
   const { depth, target } = parse(input);
   const visited = new Map();
-  const queue = [{ position: { x: 0, y: 0 }, equip: 'torch', time: 0 }];
+  const queue = [{ point: { x: 0, y: 0 }, equip: 'torch', time: 0 }];
   const score = a =>
-    a.time +
-    Math.abs(a.position.x - target.x) +
-    Math.abs(a.position.y - target.y);
+    a.time + Math.abs(a.point.x - target.x) + Math.abs(a.point.y - target.y);
   cache = { [pos(target)]: 0 };
   while (queue.length > 0) {
-    const current = queue.shift();
-    if (current.position.x === target.x && current.position.y === target.y) {
-      return current.time;
+    const n = queue.shift();
+    if (n.point.x === target.x && n.point.y === target.y) {
+      return n.time;
     }
-    const options = neighbors(current, depth, target);
-    options.forEach(o => {
-      const v = visited.get(`${o.position.x},${o.position.y},${o.equip}`);
-      if (!v || o.time < v) {
-        visited.set(`${o.position.x},${o.position.y},${o.equip}`, o.time);
+    neighbors(n, depth, target).forEach(o => {
+      const v = visited.get(`${o.point.x},${o.point.y},${o.equip}`);
+      if (!v || o.time < v.time) {
+        if (v) queue.splice(queue.indexOf(v), 1);
+        visited.set(`${o.point.x},${o.point.y},${o.equip}`, o);
         queue.push(o);
       }
     });
