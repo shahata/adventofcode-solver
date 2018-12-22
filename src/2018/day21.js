76e9004060d809c516f4b3dff745831d1f84e7ad
Shahar Talmi <shahart@wix.com>
2018-12-22 21:28:31 +0200

    make day21 solve mroe inputs

--- src/2018/day21.js
+++ src/2018/day21.js
@@ -16,10 +16,16 @@ const ops = {
   gtrr: (l, i1, i2, o) => `case ${l}: r${o} = r${i1} > r${i2} ? 1 : 0; break;`,
   eqir: (l, i1, i2, o) => `case ${l}: r${o} = ${i1} === r${i2} ? 1 : 0; break;`,
   eqri: (l, i1, i2, o) => `case ${l}: r${o} = r${i1} === ${i2} ? 1 : 0; break;`,
-  eqrr: (l, i1, i2, o) => `case ${l}: r${o} = r${i1} === r${i2} ?1 : 0; break;`,
+  eqrr: (l, i1, i2, o) => {
+    if (i1 === '0') {
+      return `case ${l}: r${o} = cb(r${i2}) === r${i2} ? 1 : 0; break;`;
+    } else if (i2 === '0') {
+      return `case ${l}: r${o} = r${i1} === cb(r${i1}) ? 1 : 0; break;`;
+    } else {
+      return `case ${l}: r${o} = r${i1} === r${i2} ? 1 : 0; break;`;
+    }
+  },
 };
-
-const numbers = arr => arr.map(x => parseInt(x));
 
 function run(input, tap, cb) {
   const lines = input.split('\n');
@@ -31,9 +37,9 @@ function run(input, tap, cb) {
   );
   const commands = lines.map((x, i) => {
     const [op, ...params] = x.split(' ');
-    return ops[op](i, ...numbers(params));
+    return ops[op](i, ...params);
   });
-  const script = `(tap, cb) => {
+  const script = `(cb) => {
     let r0 = 0, r1 = 0, r2 = 0, r3 = 0, r4 = 0, r5 = 0;
     while (true) {
       switch (r${ip}) {
@@ -41,10 +47,6 @@ function run(input, tap, cb) {
         default: return null;
       }
       r${ip}++;
-      if (r${ip} === tap) {
-        const result = cb([r0, r1, r2, r3, r4, r5]);
-        if (result) return result;
-      }
     }
   }`;
   const exec = vm.runInThisContext(script);
@@ -52,15 +54,16 @@ function run(input, tap, cb) {
 }
 
 function part1(input) {
-  return run(input, 28, r => r[3]);
+  let result;
+  run(input, r => (result = r));
+  return result;
 }
 
 function part2(input) {
   let prev;
   const all = new Set();
-  return run(input, 28, r =>
-    all.has(r[3]) ? prev : all.add((prev = r[3])) && undefined,
-  );
+  run(input, r => (all.has(r) ? r : all.add((prev = r)) && undefined));
+  return prev;
 }
 
 module.exports = { part1, part2 };
