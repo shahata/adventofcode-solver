dd7f69c6f11674260f5448103c9c060da0cbfeae
Shahar Talmi <shahart@wix.com>
2018-12-23 10:10:06 +0200

    solved day23

---
+++ src/2018/day23.js
@@ -0,0 +1,78 @@
+function parse(input) {
+  return input
+    .split('\n')
+    .map(x =>
+      x.match(/pos=<(-?\d+),(-?\d+),(-?\d+)>, r=(-?\d+)/).map(x => parseInt(x)),
+    )
+    .map(([, x, y, z, r]) => ({ x, y, z, r }));
+}
+
+function distance(a, b = { x: 0, y: 0, z: 0 }) {
+  return Math.abs(a.x - b.x) + Math.abs(a.y - b.y) + Math.abs(a.z - b.z);
+}
+
+function part1(input) {
+  const bots = parse(input);
+  const strong = bots.sort((a, b) => b.r - a.r)[0];
+  const inRange = bots.filter(a => distance(a, strong) <= strong.r);
+  return inRange.length;
+}
+
+function scaled(bots, scale) {
+  return bots.map(bot => ({
+    x: bot.x / scale,
+    y: bot.y / scale,
+    z: bot.z / scale,
+    r: bot.r / scale,
+  }));
+}
+
+function max(bots, x, y, z, best) {
+  const count = bots.filter(b => distance({ x, y, z }, b) <= b.r).length;
+  if (count > best.count) {
+    return { x, y, z, count };
+  } else if (count === best.count && distance({ x, y, z }) < distance(best)) {
+    return { x, y, z, count };
+  } else {
+    return best;
+  }
+}
+
+function part2(input) {
+  const bots = parse(input);
+  let scale = Math.pow(2, 25);
+  let ranges = scaled(bots, scale).reduce(
+    (r, b) => ({
+      x: { min: Math.min(r.x.min, b.x), max: Math.max(r.x.max, b.x) },
+      y: { min: Math.min(r.y.min, b.y), max: Math.max(r.y.max, b.y) },
+      z: { min: Math.min(r.z.min, b.z), max: Math.max(r.z.max, b.z) },
+    }),
+    {
+      x: { min: Infinity, max: -Infinity },
+      y: { min: Infinity, max: -Infinity },
+      z: { min: Infinity, max: -Infinity },
+    },
+  );
+
+  let best;
+  while (scale >= 1) {
+    const scaledBots = scaled(bots, scale);
+    best = { count: 0 };
+    for (let x = ranges.x.min; x <= ranges.x.max; x++) {
+      for (let y = ranges.y.min; y <= ranges.y.max; y++) {
+        for (let z = ranges.z.min; z <= ranges.z.max; z++) {
+          best = max(scaledBots, x, y, z, best);
+        }
+      }
+    }
+    ranges = {
+      x: { min: (best.x - 1) * 2, max: (best.x + 1) * 2 },
+      y: { min: (best.y - 1) * 2, max: (best.y + 1) * 2 },
+      z: { min: (best.z - 1) * 2, max: (best.z + 1) * 2 },
+    };
+    scale = scale / 2;
+  }
+  return distance(best, { x: 0, y: 0, z: 0 });
+}
+
+module.exports = { part1, part2 };
---
+++ src/2018/day23.spec.js
@@ -0,0 +1,51 @@
+const { part1, part2 } = require('./day23');
+const readInput = require('../utils/read-input');
+
+const input = readInput(__filename);
+
+describe('day23 2018', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(
+          [
+            'pos=<0,0,0>, r=4',
+            'pos=<1,0,0>, r=1',
+            'pos=<4,0,0>, r=3',
+            'pos=<0,2,0>, r=1',
+            'pos=<0,5,0>, r=3',
+            'pos=<0,0,3>, r=1',
+            'pos=<1,1,1>, r=1',
+            'pos=<1,1,2>, r=1',
+            'pos=<1,3,1>, r=1',
+          ].join('\n'),
+        ),
+      ).toEqual(7);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(933);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(
+        part2(
+          [
+            'pos=<10,12,12>, r=2',
+            'pos=<12,14,12>, r=2',
+            'pos=<16,12,12>, r=4',
+            'pos=<14,14,14>, r=6',
+            'pos=<50,50,50>, r=200',
+            'pos=<10,10,10>, r=5',
+          ].join('\n'),
+        ),
+      ).toEqual(36);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(70887840);
+    });
+  });
+});
