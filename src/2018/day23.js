bd5c8ca4aa1cf0de874d670c0c1543cae82411d1
Shahar Talmi <shahart@wix.com>
2018-12-25 00:40:35 +0200

    moved to amazing z3 as suggested by few ppl to solve the generic case

--- src/2018/day23.js
+++ src/2018/day23.js
@@ -1,3 +1,5 @@
+const { execSync } = require('child_process');
+
 function parse(input) {
   return input
     .split('\n')
@@ -18,62 +20,26 @@ function part1(input) {
   return inRange.length;
 }
 
-function scaled(bots, scale) {
-  return bots.map(bot => ({
-    x: bot.x / scale,
-    y: bot.y / scale,
-    z: bot.z / scale,
-    r: bot.r / scale,
-  }));
-}
-
-function max(bots, x, y, z, best) {
-  const count = bots.filter(b => distance({ x, y, z }, b) <= b.r).length;
-  if (count > best.count) {
-    return { x, y, z, count };
-  } else if (count === best.count && distance({ x, y, z }) < distance(best)) {
-    return { x, y, z, count };
-  } else {
-    return best;
-  }
-}
-
+// brew install z3
 function part2(input) {
   const bots = parse(input);
-  let scale = Math.pow(2, 25);
-  let scaledBots = scaled(bots, scale);
-  let ranges = scaledBots.reduce(
-    (r, b) => ({
-      x: { min: Math.min(r.x.min, b.x), max: Math.max(r.x.max, b.x) },
-      y: { min: Math.min(r.y.min, b.y), max: Math.max(r.y.max, b.y) },
-      z: { min: Math.min(r.z.min, b.z), max: Math.max(r.z.max, b.z) },
-    }),
-    {
-      x: { min: Infinity, max: -Infinity },
-      y: { min: Infinity, max: -Infinity },
-      z: { min: Infinity, max: -Infinity },
-    },
-  );
-
-  let best;
-  while (scale >= 1) {
-    best = { count: 0 };
-    for (let x = ranges.x.min; x <= ranges.x.max; x++) {
-      for (let y = ranges.y.min; y <= ranges.y.max; y++) {
-        for (let z = ranges.z.min; z <= ranges.z.max; z++) {
-          best = max(scaledBots, x, y, z, best);
-        }
-      }
-    }
-    ranges = {
-      x: { min: (best.x - 1) * 2, max: (best.x + 1) * 2 },
-      y: { min: (best.y - 1) * 2, max: (best.y + 1) * 2 },
-      z: { min: (best.z - 1) * 2, max: (best.z + 1) * 2 },
-    };
-    scale = scale / 2;
-    scaledBots = scaled(bots, scale);
-  }
-  return distance(best, { x: 0, y: 0, z: 0 });
+  const smt = `
+(declare-const x Int)
+(declare-const y Int)
+(declare-const z Int)
+(declare-const distance Int)
+(define-fun in_range ((nx Int) (ny Int) (nz Int) (nr Int)) Int
+  (if (<= (+ (abs (- x nx)) (abs (- y ny)) (abs (- z nz))) nr) 1 0)
+)
+(assert (= distance (+ (abs x) (abs y) (abs z))))
+(maximize (+
+  ${bots.map(b => `(in_range ${b.x} ${b.y} ${b.z} ${b.r})`).join('\n  ')}
+))
+(minimize distance)
+(check-sat)
+(get-model)`;
+  const output = execSync('z3 -in', { input: smt }).toString();
+  return parseInt(output.match(/\d+/g).pop(), 10);
 }
 
 module.exports = { part1, part2 };
