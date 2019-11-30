396d22d22494c4facee817c78fd6f29423922f59
Shahar Talmi <shahart@wix.com>
2019-11-30 21:56:48 +0200

    add wallaby

--- wallaby.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(wallaby) {
-  const wallabyCommon = require('yoshi/config/wallaby-jest')(wallaby);
-  delete wallabyCommon.workers;
-  delete wallabyCommon.compilers;
-  wallabyCommon.tests = [{ pattern: 'src/2018/day25.spec.js' }];
-  return wallabyCommon;
-};
