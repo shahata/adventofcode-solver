65f72cc5f92e29f397ca155635d8ec8ace0a8228
Shahar Talmi <shahart@wix.com>
2019-12-01 07:22:25 +0200

    2019 1

---
+++ src/2019/day01.js
@@ -0,0 +1,23 @@
+export function part1(input) {
+  return input
+    .split('\n')
+    .map(x => parseInt(x, 10))
+    .map(x => Math.floor(x / 3) - 2)
+    .reduce((a, b) => a + b);
+}
+
+export function part2(input) {
+  return input
+    .split('\n')
+    .map(x => parseInt(x, 10))
+    .map(x => {
+      let fuel = Math.floor(x / 3) - 2;
+      let extra = fuel;
+      while (extra > 0) {
+        extra = Math.max(Math.floor(extra / 3) - 2, 0);
+        fuel += extra;
+      }
+      return fuel;
+    })
+    .reduce((a, b) => a + b);
+}
