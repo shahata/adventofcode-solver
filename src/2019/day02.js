5df131b3d2d76f74f811a260cbd1ad71c8c18ce7
Shahar Talmi <shahart@wix.com>
2019-12-02 07:23:39 +0200

    2019 2

---
+++ src/2019/day02.js
@@ -0,0 +1,27 @@
+export function part1(input, noun = 12, verb = 2) {
+  const ops = input.split(',').map(x => parseInt(x));
+  let ip = 0;
+  ops[1] = noun;
+  ops[2] = verb;
+
+  while (ops[ip] !== 99) {
+    if (ops[ip] === 1) {
+      ops[ops[ip + 3]] = ops[ops[ip + 1]] + ops[ops[ip + 2]];
+    } else if (ops[ip] === 2) {
+      ops[ops[ip + 3]] = ops[ops[ip + 1]] * ops[ops[ip + 2]];
+    }
+    ip += 4;
+  }
+  return ops[0];
+}
+
+export function part2(input) {
+  for (let noun = 0; noun <= 99; noun++) {
+    for (let verb = 0; verb <= 99; verb++) {
+      if (part1(input, noun, verb) === 19690720) {
+        return noun * 100 + verb;
+      }
+    }
+  }
+  return 0;
+}
---
+++ src/2019/day02.spec.js
@@ -0,0 +1,26 @@
+import { part1, part2 } from './day02.js';
+import readInput from '../utils/read-input';
+
+const input = readInput(import.meta.url);
+
+describe('day02 2019', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(part1('1,9,10,3,2,3,11,0,99,30,40,50', 9, 10)).toEqual(3500);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(5098658);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      // expect(part2('1')).toEqual(0);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(5064);
+    });
+  });
+});
