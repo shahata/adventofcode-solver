948b5e740c8872e110cb2b1f75f58822effaf234
Shahar Talmi <shahart@wix.com>
2019-12-05 08:39:31 +0200

    2019 5

---
+++ src/2019/day05.js
@@ -0,0 +1,46 @@
+function get(ops, ip, offset) {
+  const mode = Math.floor(ops[ip] / Math.pow(10, offset + 1)) % 10;
+  return mode === 0 ? ops[ops[ip + offset]] : ops[ip + offset];
+}
+
+function process(ops, ip, user) {
+  switch (ops[ip] % 100) {
+    case 1:
+      ops[ops[ip + 3]] = get(ops, ip, 1) + get(ops, ip, 2);
+      return ip + 4;
+    case 2:
+      ops[ops[ip + 3]] = get(ops, ip, 1) * get(ops, ip, 2);
+      return ip + 4;
+    case 3:
+      ops[ops[ip + 1]] = user.input;
+      return ip + 2;
+    case 4:
+      user.output = get(ops, ip, 1);
+      return ip + 2;
+    case 5:
+      return get(ops, ip, 1) !== 0 ? get(ops, ip, 2) : ip + 3;
+    case 6:
+      return get(ops, ip, 1) === 0 ? get(ops, ip, 2) : ip + 3;
+    case 7:
+      ops[ops[ip + 3]] = get(ops, ip, 1) < get(ops, ip, 2) ? 1 : 0;
+      return ip + 4;
+    case 8:
+      ops[ops[ip + 3]] = get(ops, ip, 1) === get(ops, ip, 2) ? 1 : 0;
+      return ip + 4;
+  }
+}
+
+export function part1(input, inputValue = 1) {
+  const user = { input: inputValue, output: undefined };
+  const ops = input.split(',').map(x => parseInt(x));
+  let ip = 0;
+
+  while (ops[ip] % 100 !== 99) {
+    ip = process(ops, ip, user);
+  }
+  return user.output;
+}
+
+export function part2(input, inputValue = 5) {
+  return part1(input, inputValue);
+}
---
+++ src/2019/day05.spec.js
@@ -0,0 +1,51 @@
+import { part1, part2 } from './day05.js';
+import readInput from '../utils/read-input';
+
+const input = readInput(import.meta.url);
+
+describe('day05 2019', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      // expect(part1('1')).toEqual(0);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(15314507);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(part2('3,9,8,9,10,9,4,9,99,-1,8', 8)).toEqual(1);
+      expect(part2('3,9,8,9,10,9,4,9,99,-1,8', 9)).toEqual(0);
+
+      expect(part2('3,9,7,9,10,9,4,9,99,-1,8', 7)).toEqual(1);
+      expect(part2('3,9,7,9,10,9,4,9,99,-1,8', 8)).toEqual(0);
+
+      expect(part2('3,3,1108,-1,8,3,4,3,99', 8)).toEqual(1);
+      expect(part2('3,3,1108,-1,8,3,4,3,99', 9)).toEqual(0);
+
+      expect(part2('3,3,1107,-1,8,3,4,3,99', 7)).toEqual(1);
+      expect(part2('3,3,1107,-1,8,3,4,3,99', 8)).toEqual(0);
+
+      expect(part2('3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9', 0)).toEqual(0);
+      expect(part2('3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9', 5)).toEqual(1);
+
+      expect(part2('3,3,1105,-1,9,1101,0,0,12,4,12,99,1', 0)).toEqual(0);
+      expect(part2('3,3,1105,-1,9,1101,0,0,12,4,12,99,1', 5)).toEqual(1);
+
+      const long = [
+        '3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31',
+        '1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104',
+        '999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99',
+      ].join(',');
+      expect(part2(long, 7)).toEqual(999);
+      expect(part2(long, 8)).toEqual(1000);
+      expect(part2(long, 9)).toEqual(1001);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(652726);
+    });
+  });
+});
