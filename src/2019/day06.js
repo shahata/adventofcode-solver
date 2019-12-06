4ded90b3286cba8c38fbdb08b0f9816ec702c89a
Shahar Talmi <shahart@wix.com>
2019-12-06 09:40:05 +0200

    2019 6

---
+++ src/2019/day06.js
@@ -0,0 +1,50 @@
+function calc(map, x) {
+  if (map[x] === undefined) {
+    return 0;
+  } else if (map[x].length === undefined) {
+    map[x].length = calc(map, map[x].orbiting) + 1;
+  }
+  return map[x].length;
+}
+
+export function part1(input) {
+  const orbits = input.split('\n').map(x => x.split(')'));
+  const map = {};
+  orbits.forEach(orbit => {
+    map[orbit[1]] = { orbiting: orbit[0] };
+  });
+  return Object.keys(map)
+    .map(x => calc(map, x))
+    .reduce((a, b) => a + b);
+}
+
+export function part2(input) {
+  const orbits = input.split('\n').map(x => x.split(')'));
+  const map = {};
+  let start, destination;
+  orbits.forEach(orbit => {
+    if (orbit[1] === 'YOU') {
+      start = orbit[0];
+    } else if (orbit[1] === 'SAN') {
+      destination = orbit[0];
+    }
+    map[orbit[1]] = (map[orbit[1]] || []).concat([orbit[0]]);
+    map[orbit[0]] = (map[orbit[0]] || []).concat([orbit[1]]);
+  });
+
+  let queue = [{ distance: 0, position: start }];
+  let visited = ['YOU'];
+  while (queue.length !== 0) {
+    let next = queue.shift();
+    if (next.position === destination) {
+      return next.distance;
+    }
+    const neighbors = map[next.position].filter(
+      x => map[x] && !visited.includes(x),
+    );
+    queue = queue.concat(
+      neighbors.map(x => ({ position: x, distance: next.distance + 1 })),
+    );
+    visited.push(next.position);
+  }
+}
---
+++ src/2019/day06.spec.js
@@ -0,0 +1,60 @@
+import { part1, part2 } from './day06.js';
+import readInput from '../utils/read-input';
+
+const input = readInput(import.meta.url);
+
+describe('day06 2019', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(
+          [
+            'COM)B',
+            'B)C',
+            'C)D',
+            'D)E',
+            'E)F',
+            'B)G',
+            'G)H',
+            'D)I',
+            'E)J',
+            'J)K',
+            'K)L',
+          ].join('\n'),
+        ),
+      ).toEqual(42);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(621125);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(
+        part2(
+          [
+            'COM)B',
+            'B)C',
+            'C)D',
+            'D)E',
+            'E)F',
+            'B)G',
+            'G)H',
+            'D)I',
+            'E)J',
+            'J)K',
+            'K)L',
+            'K)YOU',
+            'I)SAN',
+          ].join('\n'),
+        ),
+      ).toEqual(4);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(550);
+    });
+  });
+});
