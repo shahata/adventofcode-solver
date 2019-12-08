1ed9bd56d0e1d5ff315706b40cc6ad86edc359aa
Shahar Talmi <shahart@wix.com>
2019-12-08 07:49:11 +0200

    2019 8

---
+++ src/2019/day08.js
@@ -0,0 +1,37 @@
+export function part1(input, wide = 25, tall = 6) {
+  const bits = input.split('');
+  const layers = [];
+  bits.forEach((bit, index) => {
+    const layer = Math.floor(index / (wide * tall));
+    layers[layer] = (layers[layer] || []).concat(bit);
+  });
+  const counts = layers.map(x => ({
+    zeros: x.filter(bit => bit === '0').length,
+    result:
+      x.filter(bit => bit === '1').length * x.filter(bit => bit === '2').length,
+  }));
+  return counts.sort((a, b) => a.zeros - b.zeros).shift().result;
+}
+
+export function part2(input, wide = 25, tall = 6) {
+  const bits = input.split('');
+  const layers = [];
+  bits.forEach((bit, index) => {
+    const layer = Math.floor(index / (wide * tall));
+    layers[layer] = (layers[layer] || []).concat(bit);
+  });
+  const result = new Array(wide * tall).fill('2');
+  layers.forEach(layer => {
+    layer.forEach((bit, index) => {
+      result[index] = result[index] === '2' ? bit : result[index];
+    });
+  });
+  const rows = [];
+  result.forEach((bit, index) => {
+    const row = Math.floor(index / wide);
+    rows[row] = (rows[row] || '') + bit;
+  });
+  return `\n${rows
+    .map(row => row.replace(/0/g, ' ').replace(/1/g, '*'))
+    .join('\n')}`;
+}
---
+++ src/2019/day08.spec.js
@@ -0,0 +1,36 @@
+import { part1, part2 } from './day08.js';
+import readInput from '../utils/read-input';
+
+const input = readInput(import.meta.url);
+
+describe('day08 2019', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(part1('123456789012', 3, 2)).toEqual(1);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(1806);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(part2('0222112222120000', 2, 2)).toEqual('\n *\n* ');
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(
+        [
+          '',
+          '  **  **  **** ***   **  ',
+          '   * *  * *    *  * *  * ',
+          '   * *  * ***  *  * *  * ',
+          '   * **** *    ***  **** ',
+          '*  * *  * *    * *  *  * ',
+          ' **  *  * *    *  * *  * ',
+        ].join('\n'),
+      );
+    });
+  });
+});
