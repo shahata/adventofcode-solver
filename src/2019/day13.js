0920d7f241a39328213e5a82423e0d43b4538792
Shahar Talmi <shahart@wix.com>
2019-12-13 08:24:36 +0200

    2019 13

---
+++ src/2019/day13.js
@@ -0,0 +1,46 @@
+import { execute } from './day11.js';
+
+export function part1(input) {
+  const output = [];
+  const user = { input: [], output: x => output.push(x), base: 0 };
+  const ops = input.split(',').map(x => parseInt(x));
+  let ip = 0;
+
+  while (ops[ip] % 100 !== 99) {
+    ip = execute(ops, ip, user);
+  }
+  return output.filter((x, i) => i % 3 === 2).filter(x => x === 2).length;
+}
+
+export function part2(input) {
+  const output = [];
+  const board = [];
+  let score = 0;
+
+  function move() {
+    while (output.length) {
+      const [x, y, id] = output.splice(0, 3);
+      if (x === -1 && y === 0) {
+        score = id;
+      } else {
+        board[y] = board[y] || [];
+        board[y][x] = id;
+      }
+    }
+    const paddle = Math.max(...board.map(row => row.indexOf(3)));
+    const ball = Math.max(...board.map(row => row.indexOf(4)));
+    return (ball - paddle) / Math.abs(ball - paddle);
+  }
+
+  const user = { input: move, output: x => output.push(x), base: 0 };
+  const ops = input.split(',').map(x => parseInt(x));
+  let ip = 0;
+  ops[0] = 2;
+
+  while (ops[ip] % 100 !== 99) {
+    ip = execute(ops, ip, user);
+  }
+  move();
+
+  return score;
+}
