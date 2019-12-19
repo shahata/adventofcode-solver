1108fe690aff7c6e1c222cacbf71114c23947f83
Shahar Talmi <shahart@wix.com>
2019-12-19 02:02:58 +0200

    2019 18 better solution

--- src/2019/day18.js
+++ src/2019/day18.js
@@ -1,56 +1,8 @@
-function calcKey(point, keys) {
-  return `${point.x},${point.y},${keys.sort().join('')}`;
-}
-
-function getNeighbors(map, next, visited) {
-  const key = calcKey(next.point, next.keys);
-  visited.add(key);
-  const options = [
-    map[next.point.y - 1] && map[next.point.y - 1][next.point.x],
-    map[next.point.y + 1] && map[next.point.y + 1][next.point.x],
-    map[next.point.y][next.point.x - 1],
-    map[next.point.y][next.point.x + 1],
-  ]
-    .filter(p => p && p.c !== '#')
-    .filter(p => {
-      if (p.c.match(/[A-Z]/) && !next.keys.includes(p.c.toLowerCase())) {
-        return false;
-      } else {
-        return true;
-      }
-    });
-  const full = options.map(p => {
-    let keys = next.keys;
-    if (p.c.match(/[a-z]/) && !keys.includes(p.c)) {
-      keys = keys.concat(p.c);
-    }
-    return { point: p, keys, distance: next.distance + 1 };
-  });
-  const result = full.filter(x => !visited.has(calcKey(x.point, x.keys)));
-  result.forEach(x => visited.add(calcKey(x.point, x.keys)));
-  return result;
-}
-
-function calcTotalKeys(map, current) {
-  let queue = [current];
-  const visited = [];
-  while (queue.length) {
-    const next = queue.shift();
-    visited.push(next);
-    const neighbors = [
-      map[next.y - 1] && map[next.y - 1][next.x],
-      map[next.y + 1] && map[next.y + 1][next.x],
-      map[next.y][next.x - 1],
-      map[next.y][next.x + 1],
-    ].filter(p => p && p.c !== '#' && !visited.includes(p));
-    queue = queue.concat(neighbors);
-  }
-
-  const keys = new Set();
-  visited.filter(p => p.c.match(/[a-z]/)).forEach(p => keys.add(p.c));
-  visited.filter(p => !keys.has(p.c.toLowerCase())).forEach(p => (p.c = '.'));
-  current.c = '@';
-  return keys.size;
+function calcKey(points, keys) {
+  return [
+    points.map(p => `(${p.x},${p.y})`).join(','),
+    keys.sort().join(''),
+  ].join(':');
 }
 
 function blockDeadEnds(map, current, visited = []) {
@@ -73,35 +25,67 @@ function blockDeadEnds(map, current, visited = []) {
   }
 }
 
-export function part1(input) {
-  const map = input
-    .split('\n')
-    .map((line, y) => line.split('').map((c, x) => ({ c, x, y })));
-  const line = map.find(line => line.find(p => p.c === '@'));
-  const current = line.find(p => p.c === '@');
-  const totalKeys = calcTotalKeys(map, current);
-  blockDeadEnds(map, current);
-
-  let queue = [{ point: current, keys: [], distance: 0 }];
+function findNextKeys(map, point, keys) {
+  const nextKeys = [];
+  let queue = [{ point, distance: 0 }];
   const visited = new Set();
   while (queue.length) {
     const next = queue.shift();
-    if (next.keys.length === totalKeys) {
-      return next.distance;
-    }
-    queue = queue.concat(getNeighbors(map, next, visited));
+    visited.add(next.point);
+    [
+      map[next.point.y - 1] && map[next.point.y - 1][next.point.x],
+      map[next.point.y + 1] && map[next.point.y + 1][next.point.x],
+      map[next.point.y][next.point.x - 1],
+      map[next.point.y][next.point.x + 1],
+    ]
+      .filter(p => p && p.c !== '#' && !visited.has(p))
+      .filter(p => !p.c.match(/[A-Z]/) || keys.includes(p.c.toLowerCase()))
+      .forEach(p => {
+        if (p.c.match(/[a-z]/) && !keys.includes(p.c)) {
+          nextKeys.push({ key: p.c, point: p, distance: next.distance + 1 });
+        } else {
+          queue.push({ point: p, distance: next.distance + 1 });
+        }
+      });
   }
-  return 0;
+  return nextKeys;
+}
+
+function minimumDistance(map, points, keys = [], memo = {}) {
+  const memoKey = calcKey(points, keys);
+  if (!memo[memoKey]) {
+    const nextKeysPerPoint = points.map(point =>
+      findNextKeys(map, point, keys),
+    );
+    if (nextKeysPerPoint.reduce((sum, x) => sum + x.length, 0) === 0) {
+      memo[memoKey] = 0;
+    } else {
+      const distances = nextKeysPerPoint.map((nextKeys, i) => {
+        const distances = nextKeys.map(x => {
+          const nextPoints = points.map((p, j) => (i === j ? x.point : p));
+          return (
+            x.distance +
+            minimumDistance(map, nextPoints, keys.concat([x.key]), memo)
+          );
+        });
+        return Math.min(...distances);
+      });
+      memo[memoKey] = Math.min(...distances);
+    }
+  }
+  return memo[memoKey];
 }
 
 function mutate(map) {
   const line = map.find(line => line.find(p => p.c === '@'));
   const current = line.find(p => p.c === '@');
-  map[current.y][current.x].c = '#';
-  map[current.y + 1][current.x].c = '#';
-  map[current.y - 1][current.x].c = '#';
-  map[current.y][current.x + 1].c = '#';
-  map[current.y][current.x - 1].c = '#';
+  [
+    map[current.y][current.x],
+    map[current.y + 1][current.x],
+    map[current.y - 1][current.x],
+    map[current.y][current.x + 1],
+    map[current.y][current.x - 1],
+  ].forEach(p => (p.c = '#'));
   const currents = [
     map[current.y + 1][current.x + 1],
     map[current.y + 1][current.x - 1],
@@ -112,50 +96,23 @@ function mutate(map) {
   return currents;
 }
 
+export function part1(input) {
+  const map = input
+    .split('\n')
+    .map((line, y) => line.split('').map((c, x) => ({ c, x, y })));
+  const line = map.find(line => line.find(p => p.c === '@'));
+  const current = line.find(p => p.c === '@');
+
+  blockDeadEnds(map, current);
+  return minimumDistance(map, [current]);
+}
+
 export function part2(input) {
   const map = input
     .split('\n')
     .map((line, y) => line.split('').map((c, x) => ({ c, x, y })));
   const currents = mutate(map);
 
-  //nasty trick, will work only with inputs with only one path to reach a key
-  const steps = currents.map(current => {
-    current.c = '@';
-    const section = map
-      .map(line => line.reduce((prev, p) => (prev += p.c), ''))
-      .join('\n');
-    current.c = '.';
-    return part1(section);
-  });
-  return steps.reduce((a, b) => a + b);
+  currents.forEach(point => blockDeadEnds(map, point));
+  return minimumDistance(map, currents);
 }
-
-// function getNeighbors2(map, next, visited) {
-//   let result = [];
-//   next.points.forEach((p, i) => {
-//     const neighbors = getNeighbors(map, { ...next, point: p }, visited[i]);
-//     const rest = next.points.filter(x => x !== p);
-//     result = result.concat(
-//       neighbors.map(n => ({ ...n, points: rest.concat(n.point) })),
-//     );
-//   });
-//   return result;
-// }
-
-// export function part2(input) {
-//   const totalKeys = input.match(/[a-z]/g).length;
-//   const map = input
-//     .split('\n')
-//     .map((line, y) => line.split('').map((c, x) => ({ c, x, y })));
-//   const currents = mutate(map);
-//   let queue = [{ points: currents, keys: [], distance: 0 }];
-//   const visited = [new Set(), new Set(), new Set(), new Set()];
-//   while (queue.length) {
-//     const next = queue.shift();
-//     if (next.keys.length === totalKeys) {
-//       return next.distance;
-//     }
-//     queue = queue.concat(getNeighbors2(map, next, visited));
-//   }
-//   return 0;
-// }
