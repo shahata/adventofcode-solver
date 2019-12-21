9e74b5e68f35a7d7452e9eceebd6dfa0c22bccc1
Shahar Talmi <shahart@wix.com>
2019-12-21 23:04:48 +0200

    fix 2015 21

--- src/2015/day21.js
+++ src/2015/day21.js
@@ -1,67 +1,46 @@
-let options;
-
 function calcOptions() {
-  if (options) {
-    return options;
-  }
+  const items = {
+    weapons: [
+      { name: 'dagger', cost: 8, damage: 4, armor: 0 },
+      { name: 'shortsword', cost: 10, damage: 5, armor: 0 },
+      { name: 'warhammer', cost: 25, damage: 6, armor: 0 },
+      { name: 'longsword', cost: 40, damage: 7, armor: 0 },
+      { name: 'greataxe', cost: 74, damage: 8, armor: 0 },
+    ],
+    armor: [
+      { name: 'leather', cost: 13, damage: 0, armor: 1 },
+      { name: 'chainmail', cost: 31, damage: 0, armor: 2 },
+      { name: 'splintmail', cost: 53, damage: 0, armor: 3 },
+      { name: 'bandedmail', cost: 75, damage: 0, armor: 4 },
+      { name: 'platemail', cost: 102, damage: 0, armor: 5 },
+      { name: 'nothing', cost: 0, damage: 0, armor: 0 },
+    ],
+    rings: [
+      { name: 'damage +1', cost: 25, damage: 1, armor: 0 },
+      { name: 'damage +2', cost: 50, damage: 2, armor: 0 },
+      { name: 'damage +3', cost: 100, damage: 3, armor: 0 },
+      { name: 'defense +1', cost: 20, damage: 0, armor: 1 },
+      { name: 'defense +2', cost: 40, damage: 0, armor: 2 },
+      { name: 'defense +3', cost: 80, damage: 0, armor: 3 },
+      { name: 'nothing', cost: 0, damage: 0, armor: 0 },
+    ],
+  };
 
-  const table = ` Weapons:    Cost  Damage  Armor
-                  Dagger        8     4       0
-                  Shortsword   10     5       0
-                  Warhammer    25     6       0
-                  Longsword    40     7       0
-                  Greataxe     74     8       0
-
-                  Armor:      Cost  Damage  Armor
-                  Leather      13     0       1
-                  Chainmail    31     0       2
-                  Splintmail   53     0       3
-                  Bandedmail   75     0       4
-                  Platemail   102     0       5
-
-                  Rings:      Cost  Damage  Armor
-                  Damage +1    25     1       0
-                  Damage +2    50     2       0
-                  Damage +3   100     3       0
-                  Defense +1   20     0       1
-                  Defense +2   40     0       2
-                  Defense +3   80     0       3`;
-
-  const items = table
-    .split('\n')
-    .filter(x => !x.match(/^\s*$/))
-    .map(x => x.match(/^\s*([^:]+):?\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s*$/))
-    .reduce((state, x) => {
-      if (x[2] === 'Cost') {
-        state.currentCategory = x[1].trim().toLowerCase();
-        state[state.currentCategory] = [];
-      } else {
-        state[state.currentCategory].push({
-          name: x[1].trim().toLowerCase(),
-          cost: parseInt(x[2]),
-          damage: parseInt(x[3]),
-          armor: parseInt(x[4]),
-        });
-      }
-      return state;
-    }, {});
-
-  items.armor.push({ name: 'nothing', cost: 0, damage: 0, armor: 0 });
-  items.rings.push({ name: 'nothing', cost: 0, damage: 0, armor: 0 });
-
-  options = [];
+  const options = [];
 
   items.weapons.forEach(weapon => {
     items.armor.forEach(armor => {
       items.rings.forEach(ring1 => {
         items.rings.forEach(ring2 => {
-          options.push([weapon, armor, ring1, ring2]);
+          if (ring1 !== ring2) {
+            options.push([weapon, armor, ring1, ring2]);
+          }
         });
       });
     });
   });
 
-  return (options = options
+  return options
     .map(x =>
       x.reduce((sum, x) => ({
         cost: sum.cost + x.cost,
@@ -69,7 +48,7 @@ function calcOptions() {
         armor: sum.armor + x.armor,
       })),
     )
-    .sort((a, b) => a.cost - b.cost));
+    .sort((a, b) => a.cost - b.cost);
 }
 
 function parse(input) {
