b0e2c0811a32a88d4bfdd97eafc73a6bc4334421
Shahar Talmi <shahart@wix.com>
2019-12-24 08:32:32 +0200

    2019 24

---
+++ src/2019/day24.js
@@ -0,0 +1,75 @@
+function getBugs(life, x, y) {
+  return [
+    life[y - 1] && life[y - 1][x],
+    life[y + 1] && life[y + 1][x],
+    life[y][x - 1],
+    life[y][x + 1],
+  ].filter(x => x === '#').length;
+}
+
+function getBugs2(lifez, x, y, z) {
+  const up = lifez[z - 1];
+  const down = lifez[z + 1];
+  const result = [];
+  if ((x === 2) & (y === 2)) return 0;
+  if (up) {
+    if (x === 0) result.push(up[2][1]);
+    if (y === 0) result.push(up[1][2]);
+    if (x === 4) result.push(up[2][3]);
+    if (y === 4) result.push(up[3][2]);
+  }
+  if (down) {
+    if (x == 2 && y === 1) result.push(...down[0]);
+    if (x == 2 && y === 3) result.push(...down[4]);
+    if (y == 2 && x === 1) result.push(...down.map(l => l[0]));
+    if (y == 2 && x === 3) result.push(...down.map(l => l[4]));
+  }
+  return result.filter(x => x === '#').length + getBugs(lifez[z], x, y);
+}
+
+function calc(c, bugs) {
+  if (c === '#') {
+    if (bugs !== 1) return '.';
+  } else {
+    if (bugs === 1 || bugs === 2) return '#';
+  }
+  return c;
+}
+
+export function part1(input) {
+  let life = input.split('\n').map(line => line.split(''));
+  const memo = {};
+
+  while (!memo[life.map(line => line.join('')).join('')]) {
+    memo[life.map(line => line.join('')).join('')] = true;
+    life = life.map((line, y) =>
+      line.map((c, x) => calc(c, getBugs(life, x, y))),
+    );
+  }
+
+  const chars = life.reduce((prev, line) => prev.concat(line), []);
+  const ratings = chars.map((x, i) => (x === '.' ? 0 : Math.pow(2, i)));
+  return ratings.reduce((a, b) => a + b);
+}
+
+export function part2(input, minutes = 200) {
+  const level = input.split('\n').map(line => line.split(''));
+  let lifez = new Array(Math.ceil(minutes * 2.5)).fill().map(() => {
+    return new Array(5).fill().map(() => new Array(5).fill('.'));
+  });
+  lifez[Math.ceil(minutes * 1.25)] = level;
+
+  for (let i = 0; i < minutes; i++) {
+    lifez = lifez.map((life, z) =>
+      life.map((line, y) =>
+        line.map((c, x) => calc(c, getBugs2(lifez, x, y, z))),
+      ),
+    );
+  }
+
+  let count = 0;
+  lifez.forEach(life =>
+    life.forEach(line => (count += line.filter(c => c === '#').length)),
+  );
+  return count;
+}
---
+++ src/2019/day24.spec.js
@@ -0,0 +1,28 @@
+import { part1, part2 } from './day24.js';
+import readInput from '../utils/read-input';
+
+const input = readInput(import.meta.url);
+
+describe('day24 2019', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      // expect(part1('1')).toEqual(0);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(14539258);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(
+        part2(['....#', '#..#.', '#..##', '..#..', '#....'].join('\n'), 10),
+      ).toEqual(99);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(1977);
+    });
+  });
+});
