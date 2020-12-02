02ebc70abdc348d376a41722ec602cd3c4948d77
Shahar Talmi <shahart@wix.com>
2020-12-02 07:20:39 +0200

    solved day 2

---
+++ src/2020/day02.js
@@ -0,0 +1,22 @@
+export function part1(input) {
+  const result = input.split('\n').filter(x => {
+    const [, start, end, character, password] = x.match(
+      /^(\d+)-(\d+) (.): (.*)$/,
+    );
+    const count = password.split('').filter(x => x === character).length;
+    return count >= +start && count <= +end;
+  }).length;
+  return result;
+}
+
+export function part2(input) {
+  const result = input.split('\n').filter(x => {
+    const [, start, end, character, password] = x.match(
+      /^(\d+)-(\d+) (.): (.*)$/,
+    );
+    const a = password[start - 1] === character ? 1 : 0;
+    const b = password[end - 1] === character ? 1 : 0;
+    return a + b === 1;
+  }).length;
+  return result;
+}
---
+++ src/2020/day02.spec.js
@@ -0,0 +1,30 @@
+import { part1, part2 } from './day02.js';
+import readInput from '../utils/read-input';
+
+const input = readInput(import.meta.url);
+
+describe('day02 2020', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc'].join('\n')),
+      ).toEqual(2);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(638);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(
+        part2(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc'].join('\n')),
+      ).toEqual(1);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(699);
+    });
+  });
+});
