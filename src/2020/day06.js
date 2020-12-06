1c3bb5300c35b8d198a76f259d36b5f669cfdd88
Shahar Talmi <shahart@wix.com>
2020-12-06 23:01:45 +0200

    update leaderboard

--- src/2020/day06.js
+++ src/2020/day06.js
@@ -1,7 +1,7 @@
 export function part1(input) {
   return input
     .split('\n\n')
-    .map(x => new Set(x.split('\n').join('').split('')).size)
+    .map(x => new Set(x.replace(/\n/g, '').split('')).size)
     .reduce((a, b) => a + b);
 }
 
@@ -9,7 +9,7 @@ export function part2(input) {
   return input
     .split('\n\n')
     .map(x => {
-      const merged = x.split('\n').join('').split('').sort().join('');
+      const merged = x.replace(/\n/g, '').split('').sort().join('');
       const count = x.split('\n').length;
       return merged.match(new RegExp(`(.)\\1{${count - 1}}`, 'g'))?.length || 0;
     })
