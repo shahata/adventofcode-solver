b4e20efa398244aa8bb42d9b860c13626135c534
Shahar Talmi <shahart@wix.com>
2020-12-10 07:44:08 +0200

    solved day 10

---
+++ src/2020/day10.js
@@ -0,0 +1,34 @@
+export function part1(input) {
+  const numbers = input
+    .split('\n')
+    .map(x => +x)
+    .concat(0)
+    .sort((a, b) => a - b);
+  let map = new Map([
+    [1, 0],
+    [2, 0],
+    [3, 1],
+  ]);
+  for (let i = 1; i < numbers.length; i++) {
+    const diff = numbers[i] - numbers[i - 1];
+    map.set(diff, map.get(diff) + 1);
+  }
+  return map.get(1) * map.get(3);
+}
+
+export function part2(input) {
+  const numbers = input
+    .split('\n')
+    .map(x => +x)
+    .concat(0)
+    .sort((a, b) => a - b);
+  const map = new Map([[0, 1]]);
+  for (let i = 1; i < numbers.length; i++) {
+    const ways =
+      (map.get(numbers[i] - 1) || 0) +
+      (map.get(numbers[i] - 2) || 0) +
+      (map.get(numbers[i] - 3) || 0);
+    map.set(numbers[i], ways);
+  }
+  return map.get(numbers[numbers.length - 1]);
+}
---
+++ src/2020/day10.spec.js
@@ -0,0 +1,114 @@
+import { part1, part2 } from './day10.js';
+import readInput from '../utils/read-input';
+
+const input = readInput(import.meta.url);
+
+describe('day10 2020', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(
+          [
+            '28',
+            '33',
+            '18',
+            '42',
+            '31',
+            '14',
+            '46',
+            '20',
+            '48',
+            '47',
+            '24',
+            '23',
+            '49',
+            '45',
+            '19',
+            '38',
+            '39',
+            '11',
+            '1',
+            '32',
+            '25',
+            '35',
+            '8',
+            '17',
+            '7',
+            '9',
+            '4',
+            '2',
+            '34',
+            '10',
+            '3',
+          ].join('\n'),
+        ),
+      ).toEqual(220);
+
+      expect(
+        part1(
+          ['16', '10', '15', '5', '1', '11', '7', '19', '6', '12', '4'].join(
+            '\n',
+          ),
+        ),
+      ).toEqual(35);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(1820);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(
+        part2(
+          ['16', '10', '15', '5', '1', '11', '7', '19', '6', '12', '4'].join(
+            '\n',
+          ),
+        ),
+      ).toEqual(8);
+
+      expect(
+        part2(
+          [
+            '28',
+            '33',
+            '18',
+            '42',
+            '31',
+            '14',
+            '46',
+            '20',
+            '48',
+            '47',
+            '24',
+            '23',
+            '49',
+            '45',
+            '19',
+            '38',
+            '39',
+            '11',
+            '1',
+            '32',
+            '25',
+            '35',
+            '8',
+            '17',
+            '7',
+            '9',
+            '4',
+            '2',
+            '34',
+            '10',
+            '3',
+          ].join('\n'),
+        ),
+      ).toEqual(19208);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(3454189699072);
+    });
+  });
+});
