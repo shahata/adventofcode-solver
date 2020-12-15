1a0d527eea07af9e9e1228a8220594ae4b015ef9
Shahar Talmi <shahart@wix.com>
2020-12-15 08:51:34 +0200

    ocd

--- src/2020/day15.js
+++ src/2020/day15.js
@@ -2,8 +2,8 @@ export function part1(input, count = 2020) {
   const start = input.split(',').map(x => +x);
   const map = new Map(start.map((x, i) => [x, i]));
 
-  let next = 0;
   let curr;
+  let next = 0;
   for (let i = start.length; i < count; i++) {
     curr = next;
     next = map.has(curr) ? i - map.get(curr) : 0;
