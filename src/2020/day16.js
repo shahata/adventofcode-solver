3a236739c68a1e61c9b0b49fa894635c24ba98ab
Shahar Talmi <shahart@wix.com>
2020-12-16 08:42:14 +0200

    solved day 16

---
+++ src/2020/day16.js
@@ -0,0 +1,59 @@
+function parse(input) {
+  let [rules, ticket, tickets] = input.split('\n\n');
+  rules = rules
+    .split('\n')
+    .map(x => x.match(/^(.*): (\d+)-(\d+) or (\d+)-(\d+)$/))
+    .map(([, field, a, b, c, d]) => ({ field, a: +a, b: +b, c: +c, d: +d }));
+
+  ticket = ticket.split('\n').pop();
+
+  tickets = tickets
+    .split('\n')
+    .slice(1)
+    .concat([ticket])
+    .map(x => x.split(',').map(x => +x));
+
+  return { rules, ticket: tickets[tickets.length - 1], tickets };
+}
+
+const valid = (n, x) => (n >= x.a && n <= x.b) || (n >= x.c && n <= x.d);
+
+function validate(ticket, rules) {
+  return ticket.reduce((error, n) => {
+    return error + (rules.every(x => !valid(n, x)) ? n : 0);
+  }, 0);
+}
+
+export function part1(input) {
+  let { rules, tickets } = parse(input);
+  return tickets.map(x => validate(x, rules)).reduce((a, b) => a + b);
+}
+
+export function part2(input) {
+  let { rules, ticket, tickets } = parse(input);
+  tickets = tickets.filter(x => validate(x, rules) === 0);
+
+  const done = [];
+  while (done.length < ticket.length) {
+    rules
+      .filter(x => x.position === undefined)
+      .forEach(x => {
+        const found = [];
+        for (let i = 0; i < ticket.length; i++) {
+          if (!done.includes(i) && tickets.every(t => valid(t[i], x))) {
+            found.push(i);
+          }
+        }
+        if (found.length === 1) {
+          x.position = found[0];
+          done.push(x.position);
+        }
+      });
+  }
+
+  return rules
+    .sort((a, b) => a.position - b.position)
+    .map((x, i) => ({ ...x, value: ticket[i] }))
+    .filter(x => x.field.startsWith('departure'))
+    .reduce((mul, x) => mul * x.value, 1);
+}
---
+++ src/2020/day16.spec.js
@@ -0,0 +1,59 @@
+import { part1, part2 } from './day16.js';
+import readInput from '../utils/read-input.js';
+
+const input = readInput(import.meta.url);
+
+describe('day16 2020', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(
+          [
+            'class: 1-3 or 5-7',
+            'row: 6-11 or 33-44',
+            'seat: 13-40 or 45-50',
+            '',
+            'your ticket:',
+            '7,1,14',
+            '',
+            'nearby tickets:',
+            '7,3,47',
+            '40,4,50',
+            '55,2,20',
+            '38,6,12',
+          ].join('\n'),
+        ),
+      ).toEqual(71);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(24980);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(
+        part2(
+          [
+            'departure class: 0-1 or 4-19',
+            'row: 0-5 or 8-19',
+            'seat: 0-13 or 16-19',
+            '',
+            'your ticket:',
+            '11,12,13',
+            '',
+            'nearby tickets:',
+            '3,9,18',
+            '15,1,5',
+            '5,14,9',
+          ].join('\n'),
+        ),
+      ).toEqual(12);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(809376774329);
+    });
+  });
+});
