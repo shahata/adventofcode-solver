59f3d0c0f397dc3910ab07fed1c6773eb3089e90
Shahar Talmi <shahart@wix.com>
2020-12-17 08:58:28 +0200

    update leaderboard

--- src/2020/day17.js
+++ src/2020/day17.js
@@ -1,33 +1,32 @@
-function calc(coordinates) {
-  if (coordinates.length === 0) {
+function offsets(dimensions) {
+  if (dimensions === 0) {
     return [[]];
   } else {
-    const rest = calc(coordinates.slice(1));
+    const rest = offsets(dimensions - 1);
     return [
-      ...rest.map(x => [coordinates[0] - 1, ...x]),
-      ...rest.map(x => [coordinates[0], ...x]),
-      ...rest.map(x => [coordinates[0] + 1, ...x]),
+      ...rest.map(x => [-1, ...x]),
+      ...rest.map(x => [0, ...x]),
+      ...rest.map(x => [1, ...x]),
     ];
   }
 }
 
-function getNeighbors(key) {
+function neighbors(key) {
   const coordinates = key.split(',').map(x => +x);
-  return calc(coordinates)
-    .map(c => c.join(','))
-    .filter(c => c !== key);
+  return offsets(coordinates.length)
+    .map(x => x.map((c, i) => coordinates[i] + c).join(','))
+    .filter(x => x !== key);
 }
 
 export function part1(input, dimensions = 3) {
   let map = new Map();
   input.split('\n').map((line, y) => {
     line.split('').map((char, x) => {
-      const coordinates = new Array(dimensions).fill(0);
-      coordinates[0] = x;
-      coordinates[1] = y;
-      map.set(coordinates.join(','), char === '#');
+      const coordinates = new Array(dimensions - 2).fill(0);
+      map.set([x, y, ...coordinates].join(','), char === '#');
     });
   });
+
   let count;
   for (let i = 0; i < 6; i++) {
     let next = new Map();
@@ -36,15 +35,13 @@ export function part1(input, dimensions = 3) {
 
     for (let key of map.keys()) {
       if (map.get(key)) {
-        missing = missing.concat(
-          getNeighbors(key).filter(key => !map.has(key)),
-        );
+        missing = missing.concat(neighbors(key).filter(key => !map.has(key)));
       }
     }
     missing.forEach(key => map.set(key, false));
 
     for (let key of map.keys()) {
-      const active = getNeighbors(key).filter(key => map.get(key)).length;
+      const active = neighbors(key).filter(key => map.get(key)).length;
       next.set(key, (map.get(key) && active === 2) || active === 3);
       if (next.get(key)) {
         count++;
