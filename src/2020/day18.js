395a2e733135b163621485f37196bcb881a199df
Shahar Talmi <shahart@wix.com>
2020-12-18 08:10:27 +0200

    solved day 18

---
+++ src/2020/day18.js
@@ -0,0 +1,37 @@
+function simple(formula) {
+  let result = 0;
+  let operation = '+';
+  formula.split(' ').forEach(x => {
+    if (x === '+' || x === '*') {
+      operation = x;
+    } else if (operation === '+') {
+      result += +x;
+    } else if (operation === '*') {
+      result *= +x;
+    }
+  });
+  return result;
+}
+
+function solve(formula, precedence) {
+  while (
+    formula.includes('(') ||
+    (precedence && formula.includes('+') && formula.includes('*'))
+  ) {
+    if (precedence) {
+      formula = formula.replace(/(\d+ \+ )+\d+/g, x => simple(x));
+    }
+    formula = formula.replace(/\(([^()]*)\)/g, (a, x) => simple(x));
+  }
+  return simple(formula);
+}
+
+export function part1(input) {
+  const formulas = input.split('\n');
+  return formulas.map(x => solve(x)).reduce((a, b) => a + b);
+}
+
+export function part2(input) {
+  const formulas = input.split('\n');
+  return formulas.map(x => solve(x, true)).reduce((a, b) => a + b);
+}
---
+++ src/2020/day18.spec.js
@@ -0,0 +1,47 @@
+import { part1, part2 } from './day18.js';
+import readInput from '../utils/read-input.js';
+
+const input = readInput(import.meta.url);
+
+describe('day18 2020', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(
+          [
+            '1 + 2 * 3 + 4 * 5 + 6',
+            '2 * 3 + (4 * 5)',
+            '5 + (8 * 3 + 9 + 3 * 4 * 3)',
+            '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',
+            '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2',
+          ].join('\n'),
+        ),
+      ).toEqual(71 + 26 + 437 + 12240 + 13632);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(21993583522852);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(
+        part2(
+          [
+            '1 + 2 * 3 + 4 * 5 + 6',
+            '1 + (2 * 3) + (4 * (5 + 6))',
+            '2 * 3 + (4 * 5)',
+            '5 + (8 * 3 + 9 + 3 * 4 * 3)',
+            '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',
+            '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2',
+          ].join('\n'),
+        ),
+      ).toEqual(231 + 51 + 46 + 1445 + 669060 + 23340);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(122438593522757);
+    });
+  });
+});
