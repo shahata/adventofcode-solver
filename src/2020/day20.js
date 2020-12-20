8d9b6d43e8cee8d70e433044907eddbfc85db698
Shahar Talmi <shahart@wix.com>
2020-12-20 14:17:26 +0200

    solved day 20

--- src/2020/day20.js
+++ src/2020/day20.js
@@ -1,79 +1,43 @@
-function rightBorder(tile) {
-  return parseInt(
-    tile
-      .map(x => x[x.length - 1])
-      .join('')
-      .replaceAll('.', '0')
-      .replaceAll('#', '1'),
-    2,
-  );
+function toBorder(line) {
+  return parseInt(line.replaceAll('.', '0').replaceAll('#', '1'), 2);
+}
+function reverse(line) {
+  return line.split('').reverse().join('');
+}
+function topBorder(tile) {
+  return tile[0];
 }
 function bottomBorder(tile) {
-  return parseInt(
-    tile[tile.length - 1].replaceAll('.', '0').replaceAll('#', '1'),
-    2,
+  return tile[tile.length - 1];
+}
+function leftBorder(tile) {
+  return tile.map(x => x[0]).join('');
+}
+function rightBorder(tile) {
+  return tile.map(x => x[x.length - 1]).join('');
+}
+function rotateRight(tile) {
+  return tile.map((row, i) =>
+    [...tile]
+      .reverse()
+      .map(x => x[i])
+      .join(''),
   );
 }
+function mirrorHorizontal(tile) {
+  return tile.map(line => line.split('').reverse().join(''));
+}
+
 function getBorders(tile) {
   return [
-    parseInt(tile[0].replaceAll('.', '0').replaceAll('#', '1'), 2),
-    parseInt(
-      tile[tile.length - 1].replaceAll('.', '0').replaceAll('#', '1'),
-      2,
-    ),
-    parseInt(
-      tile
-        .map(x => x[0])
-        .join('')
-        .replaceAll('.', '0')
-        .replaceAll('#', '1'),
-      2,
-    ),
-    parseInt(
-      tile
-        .map(x => x[x.length - 1])
-        .join('')
-        .replaceAll('.', '0')
-        .replaceAll('#', '1'),
-      2,
-    ),
-    //
-    parseInt(
-      tile[tile.length - 1]
-        .split('')
-        .reverse()
-        .join('')
-        .replaceAll('.', '0')
-        .replaceAll('#', '1'),
-      2,
-    ),
-    parseInt(
-      tile[0]
-        .split('')
-        .reverse()
-        .join('')
-        .replaceAll('.', '0')
-        .replaceAll('#', '1'),
-      2,
-    ),
-    parseInt(
-      tile
-        .map(x => x[x.length - 1])
-        .reverse()
-        .join('')
-        .replaceAll('.', '0')
-        .replaceAll('#', '1'),
-      2,
-    ),
-    parseInt(
-      tile
-        .map(x => x[0])
-        .reverse()
-        .join('')
-        .replaceAll('.', '0')
-        .replaceAll('#', '1'),
-      2,
-    ),
+    toBorder(topBorder(tile)),
+    toBorder(bottomBorder(tile)),
+    toBorder(leftBorder(tile)),
+    toBorder(rightBorder(tile)),
+    toBorder(reverse(bottomBorder(tile))),
+    toBorder(reverse(topBorder(tile))),
+    toBorder(reverse(rightBorder(tile))),
+    toBorder(reverse(leftBorder(tile))),
   ];
 }
 
@@ -87,72 +51,89 @@ function parse(tile) {
     borders: getBorders([...rest]),
   };
 }
-export function part1(input) {
-  const tiles = input.split('\n\n').map(x => parse(x));
-  const map = new Map();
+
+function countMonsters(image) {
+  const pattern = [
+    /^..................#./,
+    /^#....##....##....###/,
+    /^.#..#..#..#..#..#.../,
+  ];
+  let count = 0;
+  for (let index = 0; index < image.length - 2; index++) {
+    for (let i = 0; i < image.length; i++) {
+      if (
+        image[index].slice(i).match(pattern[0]) &&
+        image[index + 1].slice(i).match(pattern[1]) &&
+        image[index + 2].slice(i).match(pattern[2])
+      ) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+function findCorners(tiles) {
+  const borderToIds = new Map();
   tiles.forEach(({ id, borders }) => {
     borders.forEach(x => {
-      map.set(x, (map.get(x) || []).concat(id));
+      borderToIds.set(x, (borderToIds.get(x) || []).concat(id));
     });
   });
-  let map2 = {};
-  for (let [, ids] of map.entries()) {
+  const idToNeighbors = new Map();
+  for (let [, ids] of borderToIds.entries()) {
     if (ids.length === 2) {
-      map2[ids[0]] = (map2[ids[0]] || new Set()).add(ids[1]);
-      map2[ids[1]] = (map2[ids[1]] || new Set()).add(ids[0]);
+      const neighbors = ids.map(id => idToNeighbors.get(id) || new Set());
+      idToNeighbors.set(ids[0], neighbors[0].add(ids[1]));
+      idToNeighbors.set(ids[1], neighbors[1].add(ids[0]));
     }
   }
-  let result = 1;
-  for (let id in map2) {
-    if (map2[id].size == 2) {
-      result *= id;
+  const corners = [];
+  for (let [id, neighbors] of idToNeighbors.entries()) {
+    if (neighbors.size == 2) {
+      corners.push(+id);
     }
   }
-  return result;
+  return corners;
+}
+
+export function part1(input) {
+  const tiles = input.split('\n\n').map(x => parse(x));
+  const corners = findCorners(tiles);
+  return corners.reduce((a, b) => a * b, 1);
 }
 
 export function part2(input) {
   let tiles = input.split('\n\n').map(x => parse(x));
-  const map = new Map();
-  tiles.forEach(({ id, borders }) => {
-    borders.forEach(x => {
-      map.set(x, (map.get(x) || []).concat(id));
-    });
-  });
-  let map2 = {};
-  for (let [, ids] of map.entries()) {
-    if (ids.length === 2) {
-      map2[ids[0]] = (map2[ids[0]] || new Set()).add(ids[1]);
-      map2[ids[1]] = (map2[ids[1]] || new Set()).add(ids[0]);
-    }
-  }
-  const corners = [];
-  for (let id in map2) {
-    if (map2[id].size == 2) {
-      corners.push(+id);
-    }
-  }
-  //top bottom left right
+  const corners = findCorners(tiles);
 
-  // return corners;
-  // return corners.filter(corner => {
-  //   const tile = tiles.find(x => x.id === corner);
-  //   return (
-  //     map.get(tile.borders[2]).length === 1 && map.get(tile.borders[4]).length === 1
-  //   );
-  // });//?
-  const corner = tiles.find(x => x.id === corners[3]);
-  tiles = tiles.filter(x => x !== corner);
-  corner.borders.slice(0, 4).map(b => {
-    return map.get(b).length;
-  });
+  //0:top 1:bottom 2:left 3:right
   let image = [];
-  image[0] = [corner.tile.map(line => line.split('').reverse().join(''))]; //flip
+  const corner = tiles.find(x => x.id === corners[0]);
+  const sides = corner.borders.map(x => {
+    return tiles.filter(tile => tile.borders.includes(x)).length;
+  });
+  tiles = tiles.filter(x => x !== corner);
+  if (sides[0] === 1 && sides[2] === 1) {
+    //top left
+  } else if (sides[0] === 1 && sides[3] === 1) {
+    //top right
+    corner.tile = mirrorHorizontal(corner.tile);
+  } else if (sides[1] === 1 && sides[2] === 1) {
+    //bottom left
+    corner.tile = rotateRight(corner.tile);
+  } else if (sides[1] === 1 && sides[3] === 1) {
+    //bottom right
+    corner.tile = rotateRight(corner.tile);
+    corner.tile = rotateRight(corner.tile);
+  }
+
   let j = 0;
   let i = 0;
+  image[0] = [corner.tile];
   while (j >= 0) {
     while (i >= 0) {
-      const r = rightBorder(image[j][i]);
+      const r = toBorder(rightBorder(image[j][i]));
       const next = tiles.find(x => x.borders.includes(r));
       tiles = tiles.filter(x => x !== next);
       if (!next) {
@@ -160,33 +141,24 @@ export function part2(input) {
       }
       if (next.borders.indexOf(r) > 3) {
         next.borders = next.borders.slice(4);
-        next.tile = next.tile.map(line => line.split('').reverse().join(''));
-        next.tile = next.tile.reverse();
+        next.tile = rotateRight(next.tile);
+        next.tile = rotateRight(next.tile);
       }
       if (next.borders[0] === r) {
-        next.tile = next.tile.map((row, i) =>
-          next.tile.map(x => x[i]).join(''),
-        );
+        next.tile = rotateRight(next.tile);
+        next.tile = mirrorHorizontal(next.tile);
       }
       if (next.borders[1] === r) {
-        next.tile = next.tile.map((row, i) =>
-          [...next.tile]
-            .reverse()
-            .map(x => x[i])
-            .join(''),
-        );
-      }
-      if (next.borders[2] === r) {
-        next.tile = [...next.tile];
+        next.tile = rotateRight(next.tile);
       }
       if (next.borders[3] === r) {
-        next.tile = next.tile.map(line => line.split('').reverse().join(''));
+        next.tile = mirrorHorizontal(next.tile);
       }
       image[j][i + 1] = next.tile;
       i++;
     }
     i = 0;
-    const b = bottomBorder(image[j][i]);
+    const b = toBorder(bottomBorder(image[j][i]));
     const next = tiles.find(x => x.borders.includes(b));
     tiles = tiles.filter(x => x !== next);
     if (!next) {
@@ -194,22 +166,22 @@ export function part2(input) {
     }
     if (next.borders.indexOf(b) > 3) {
       next.borders = next.borders.slice(4);
-      next.tile = next.tile.map(line => line.split('').reverse().join(''));
-      next.tile = next.tile.reverse();
-    }
-    if (next.borders[0] === b) {
-      next.tile = [...next.tile];
+      next.tile = rotateRight(next.tile);
+      next.tile = rotateRight(next.tile);
     }
     if (next.borders[1] === b) {
-      next.tile = [...next.tile].reverse();
+      next.tile = rotateRight(next.tile);
+      next.tile = rotateRight(next.tile);
+      next.tile = mirrorHorizontal(next.tile);
     }
     if (next.borders[2] === b) {
-      next.tile = next.tile.map((row, i) => next.tile.map(x => x[i]).join(''));
+      next.tile = rotateRight(next.tile);
+      next.tile = mirrorHorizontal(next.tile);
     }
     if (next.borders[3] === b) {
-      next.tile = next.tile.map((row, i) =>
-        next.tile.map(x => x[x.length - 1 - i]).join(''),
-      );
+      next.tile = rotateRight(next.tile);
+      next.tile = rotateRight(next.tile);
+      next.tile = rotateRight(next.tile);
     }
     image[j + 1] = [next.tile];
     j++;
@@ -223,44 +195,20 @@ export function part2(input) {
       }, new Array(row[0].length - 2).fill('')),
     );
   }, []);
-  image;
 
-  const pattern = [
-    /^..................#./,
-    /^#....##....##....###/,
-    /^.#..#..#..#..#..#.../,
-  ];
-  let count = 0;
-  if (image.length === 24) {
-    image = image.map((row, i) => image.map(x => x[x.length - 1 - i]).join(''));
-  } else {
-    image = image.map(line => line.split('').reverse().join(''));
-    image = image.reverse();
-  }
-  // image = image.map((row, i) => image.map(x => x[i]).join(''));
-  // image = image.map((row, i) =>
-  //   [...image]
-  //     .reverse()
-  //     .map(x => x[i])
-  //     .join(''),
-  // );
-  // image = image.map(line => line.split('').reverse().join(''));
-  // image = [...image].reverse();
-  // image = image.map((row, i) => image.map(x => x[i]).join(''));
-  for (let index = 0; index < image.length - 2; index++) {
-    for (let i = 0; i < image.length; i++) {
-      if (
-        image[index].slice(i).match(pattern[0]) &&
-        image[index + 1].slice(i).match(pattern[1]) &&
-        image[index + 2].slice(i).match(pattern[2])
-      ) {
-        count++;
-      }
-    }
-  }
+  const count = [
+    image,
+    rotateRight(image),
+    rotateRight(rotateRight(image)),
+    rotateRight(rotateRight(rotateRight(image))),
+    mirrorHorizontal(image),
+    rotateRight(mirrorHorizontal(image)),
+    rotateRight(rotateRight(mirrorHorizontal(image))),
+    rotateRight(rotateRight(rotateRight(mirrorHorizontal(image)))),
+  ].map(x => countMonsters(x));
 
   const sum = image
     .map(line => line.split('').filter(c => c === '#').length)
     .reduce((a, b) => a + b);
-  return sum - count * 15;
+  return sum - count.find(x => x !== 0) * 15;
 }
