53c1b8e55f26831c88d25ee1b951b368bf807732
Shahar Talmi <shahart@wix.com>
2020-12-22 01:53:52 +0200

    format eta

--- src/utils/renderer.js
+++ src/utils/renderer.js
@@ -81,6 +81,23 @@ export async function downloadIndex(year, bar) {
   bar.tick();
 }
 
+function toHHMMSS(sec_num) {
+  var hours = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - hours * 3600) / 60);
+  var seconds = sec_num - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+  return hours + ':' + minutes + ':' + seconds;
+}
+
 export async function createSolver(year, day) {
   inquirer.registerPrompt('timeout-confirm', TimeoutConfirm);
   const page = await getYearPage(year);
@@ -89,6 +106,7 @@ export async function createSolver(year, day) {
     {
       type: 'timeout-confirm',
       timeout: eta,
+      timeoutTips: t => `(${toHHMMSS(t)})`,
       name: 'create',
       message: `Create solver ${year}/${dayName(day)}?`,
     },
