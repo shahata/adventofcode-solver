b28e67727cfee00dac802b35915d5bb746c0d84d
Shahar Talmi <shahart@wix.com>
2020-12-21 08:16:59 +0200

    solved day 21

---
+++ src/2020/day21.js
@@ -0,0 +1,64 @@
+function parse(input) {
+  let allIngredients = new Map();
+  const allAllergens = new Set();
+  const foods = input
+    .split('\n')
+    .map(line => line.replace(')', '').split(' (contains '))
+    .map(([ingredients, allergens]) => ({
+      ingredients: ingredients.split(' '),
+      allergens: allergens.split(', '),
+    }));
+  foods.forEach(food => {
+    food.ingredients.forEach(x => {
+      const ingredient = allIngredients.get(x);
+      if (ingredient) {
+        ingredient.count++;
+      } else {
+        allIngredients.set(x, { count: 1, mightContain: new Set(), name: x });
+      }
+    });
+    food.allergens.forEach(x => allAllergens.add(x));
+  });
+  allIngredients = [...allIngredients.values()];
+  allIngredients.forEach(
+    ingredient => (ingredient.mightContain = new Set(allAllergens)),
+  );
+  foods.forEach(food => {
+    allIngredients.forEach(ingredient => {
+      if (!food.ingredients.includes(ingredient.name)) {
+        food.allergens.forEach(x => {
+          ingredient.mightContain.delete(x);
+        });
+      }
+    });
+  });
+  return allIngredients;
+}
+
+export function part1(input) {
+  const allIngredients = parse(input);
+  return allIngredients
+    .filter(x => x.mightContain.size === 0)
+    .reduce((sum, { count }) => sum + count, 0);
+}
+
+export function part2(input) {
+  let allIngredients = parse(input);
+  allIngredients = allIngredients.filter(x => x.mightContain.size !== 0);
+  let done;
+  while (done !== allIngredients.length) {
+    done = 0;
+    allIngredients.forEach(ingredient => {
+      if (ingredient.contains) {
+        done++;
+      } else if (ingredient.mightContain.size === 1) {
+        ingredient.contains = [...ingredient.mightContain.values()].pop();
+        allIngredients.forEach(x => x.mightContain.delete(ingredient.contains));
+      }
+    });
+  }
+  return allIngredients
+    .sort((a, b) => a.contains.localeCompare(b.contains))
+    .map(x => x.name)
+    .join(',');
+}
---
+++ src/2020/day21.spec.js
@@ -0,0 +1,46 @@
+import { part1, part2 } from './day21.js';
+import readInput from '../utils/read-input.js';
+
+const input = readInput(import.meta.url);
+
+describe('day21 2020', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(
+          [
+            'mxmxvkd kfcds sqjhc nhms (contains dairy, fish)',
+            'trh fvjkl sbzzf mxmxvkd (contains dairy)',
+            'sqjhc fvjkl (contains soy)',
+            'sqjhc mxmxvkd sbzzf (contains fish)',
+          ].join('\n'),
+        ),
+      ).toEqual(5);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(2556);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(
+        part2(
+          [
+            'mxmxvkd kfcds sqjhc nhms (contains dairy, fish)',
+            'trh fvjkl sbzzf mxmxvkd (contains dairy)',
+            'sqjhc fvjkl (contains soy)',
+            'sqjhc mxmxvkd sbzzf (contains fish)',
+          ].join('\n'),
+        ),
+      ).toEqual('mxmxvkd,sqjhc,fvjkl');
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(
+        'vcckp,hjz,nhvprqb,jhtfzk,mgkhhc,qbgbmc,bzcrknb,zmh',
+      );
+    });
+  });
+});
