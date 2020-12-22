3af5ce4489e92257e054945c78e40a93875808f1
Shahar Talmi <shahart@wix.com>
2020-12-22 08:34:20 +0200

    solved day 22

---
+++ src/2020/day22.js
@@ -0,0 +1,56 @@
+function check1(players, cards) {
+  return cards.indexOf(Math.max(...cards));
+}
+
+function check2(players, cards) {
+  if (players.every((x, i) => x.length >= cards[i])) {
+    const results = subGame(
+      players.map((p, i) => p.slice(0, cards[i])),
+      check2,
+    );
+    return results.findIndex(x => x > 0);
+  } else {
+    return check1(players, cards);
+  }
+}
+
+function subGame(players, check) {
+  const visited = new Set();
+  while (players.filter(x => x.length > 0).length > 1) {
+    const serialized = players.map(p => p.join(',')).join('#');
+    if (visited.has(serialized)) {
+      return players.map((x, i) => (i === 0 ? 1 : 0));
+    }
+    visited.add(serialized);
+
+    const cards = players.map(player => player.shift() || 0);
+    const winner = check(players, cards);
+    players[winner] = players[winner]
+      .concat(cards.splice(winner, 1))
+      .concat(cards);
+  }
+  return players.map(p =>
+    p.map((x, i) => x * (p.length - i)).reduce((a, b) => a + b, 0),
+  );
+}
+
+function parse(input) {
+  return input.split('\n\n').map(player =>
+    player
+      .split('\n')
+      .slice(1)
+      .map(x => +x),
+  );
+}
+
+export function part1(input) {
+  const players = parse(input);
+  const results = subGame(players, check1);
+  return results[0] + results[1];
+}
+
+export function part2(input) {
+  const players = parse(input);
+  const results = subGame(players, check2);
+  return results[0] + results[1];
+}
---
+++ src/2020/day22.spec.js
@@ -0,0 +1,62 @@
+import { part1, part2 } from './day22.js';
+import readInput from '../utils/read-input.js';
+
+const input = readInput(import.meta.url);
+
+describe('day22 2020', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(
+          [
+            'Player 1:',
+            '9',
+            '2',
+            '6',
+            '3',
+            '1',
+            '',
+            'Player 2:',
+            '5',
+            '8',
+            '4',
+            '7',
+            '10',
+          ].join('\n'),
+        ),
+      ).toEqual(306);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(33421);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(
+        part2(
+          [
+            'Player 1:',
+            '9',
+            '2',
+            '6',
+            '3',
+            '1',
+            '',
+            'Player 2:',
+            '5',
+            '8',
+            '4',
+            '7',
+            '10',
+          ].join('\n'),
+        ),
+      ).toEqual(291);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(33651);
+    });
+  });
+});
