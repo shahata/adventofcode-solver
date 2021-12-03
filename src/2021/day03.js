aac563588a04c098cf7d7233bb8f3b4c5fdf8ff5
Shahar Talmi <shahart@wix.com>
2021-12-03 08:29:12 +0200

    small refactor

--- src/2021/day03.js
+++ src/2021/day03.js
@@ -1,40 +1,33 @@
-function findCommonBit(numbers, digit, mostCommon) {
+function findMostCommonBit(numbers, digit) {
   let counter = { 0: 0, 1: 0 };
-  for (const num of numbers) {
-    counter[num[digit]]++;
-  }
-  if (counter['1'] >= counter['0']) {
-    return mostCommon ? '1' : '0';
-  } else {
-    return mostCommon ? '0' : '1';
-  }
+  numbers.forEach(number => counter[number[digit]]++);
+  return counter['1'] >= counter['0'] ? '1' : '0';
 }
 
 export function part1(input) {
   const numbers = input.split('\n');
-  let mostCommon = '',
-    leastCommon = '';
+  let mostCommon = '';
+  let leastCommon = '';
   for (let digit = 0; digit < numbers[0].length; digit++) {
-    mostCommon += findCommonBit(numbers, digit, true);
-    leastCommon += findCommonBit(numbers, digit, false);
+    const bit = findMostCommonBit(numbers, digit);
+    mostCommon += bit;
+    leastCommon += bit === '1' ? '0' : '1';
   }
   return parseInt(mostCommon, 2) * parseInt(leastCommon, 2);
 }
 
 export function part2(input) {
   let numbers = input.split('\n');
+  let numbers2 = input.split('\n');
   for (let digit = 0; numbers.length > 1; digit++) {
     numbers = numbers.filter(
-      num => num[digit] === findCommonBit(numbers, digit, true),
+      number => number[digit] === findMostCommonBit(numbers, digit),
     );
   }
-
-  let numbers2 = input.split('\n');
   for (let digit = 0; numbers2.length > 1; digit++) {
     numbers2 = numbers2.filter(
-      num => num[digit] === findCommonBit(numbers2, digit, false),
+      number => number[digit] !== findMostCommonBit(numbers2, digit),
     );
   }
-
   return parseInt(numbers[0], 2) * parseInt(numbers2[0], 2);
 }
