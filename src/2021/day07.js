8678ca6a7c5e5309da7e7289163daecc2aa158ee
Shahar Talmi <shahar@wix.com>
2021-12-07 07:28:40 +0200

    add some higschool math

--- src/2021/day07.js
+++ src/2021/day07.js
@@ -10,18 +10,13 @@ export function part1(input) {
 }
 
 export function part2(input) {
+  const sum = (a, b) => (Math.abs(a - b) * (Math.abs(a - b) + 1)) / 2;
   const positions = input.split(',').map(x => +x);
   const min = Math.min(...positions);
   const max = Math.max(...positions);
   const distances = [];
   for (let i = min; i <= max; i++) {
-    distances.push(
-      positions.reduce((prev, x) => {
-        let sum = 0;
-        for (let j = 1; j <= Math.abs(x - i); j++) sum += j;
-        return prev + sum;
-      }, 0),
-    );
+    distances.push(positions.reduce((prev, x) => prev + sum(x, i), 0));
   }
   return Math.min(...distances);
 }
