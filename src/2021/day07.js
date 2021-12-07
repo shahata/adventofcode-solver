bc7fb86d344320a15a9481182588525c1f71064b
Shahar Talmi <shahar@wix.com>
2021-12-07 07:49:29 +0200

    refactor

--- src/2021/day07.js
+++ src/2021/day07.js
@@ -1,22 +1,14 @@
-export function part1(input) {
+export function part1(input, cost = (a, b) => Math.abs(a - b)) {
   const positions = input.split(',').map(x => +x);
   const min = Math.min(...positions);
   const max = Math.max(...positions);
   const distances = [];
   for (let i = min; i <= max; i++) {
-    distances.push(positions.reduce((prev, x) => prev + Math.abs(x - i), 0));
+    distances.push(positions.reduce((prev, x) => prev + cost(x, i), 0));
   }
   return Math.min(...distances);
 }
 
 export function part2(input) {
-  const sum = (a, b) => (Math.abs(a - b) * (Math.abs(a - b) + 1)) / 2;
-  const positions = input.split(',').map(x => +x);
-  const min = Math.min(...positions);
-  const max = Math.max(...positions);
-  const distances = [];
-  for (let i = min; i <= max; i++) {
-    distances.push(positions.reduce((prev, x) => prev + sum(x, i), 0));
-  }
-  return Math.min(...distances);
+  return part1(input, (a, b) => (Math.abs(a - b) * (Math.abs(a - b) + 1)) / 2);
 }
