4025c4087089256cb61381c3079ddeaaa2eb4a5f
Shahar Talmi <shahar@wix.com>
2021-12-10 07:37:25 +0200

    solved day 10

---
+++ src/2021/day10.js
@@ -0,0 +1,33 @@
+function clean(line) {
+  let len = 0;
+  while (line.length !== len) {
+    len = line.length;
+    line = line.replaceAll('()', '');
+    line = line.replaceAll('[]', '');
+    line = line.replaceAll('{}', '');
+    line = line.replaceAll('<>', '');
+  }
+  return line;
+}
+
+function score(line) {
+  const points = { ')': 3, ']': 57, '}': 1197, '>': 25137 };
+  return points[line.split('').find(c => ')]}>'.includes(c))] || 0;
+}
+
+function score2(line) {
+  const points = { '(': 1, '[': 2, '{': 3, '<': 4 };
+  const chars = line.split('').reverse();
+  return chars.reduce((total, c) => total * 5 + points[c], 0);
+}
+
+export function part1(input) {
+  const scores = input.split('\n').map(x => score(clean(x)));
+  return scores.reduce((a, b) => a + b);
+}
+
+export function part2(input) {
+  const incomplete = input.split('\n').filter(x => score(clean(x)) === 0);
+  const scores = incomplete.map(x => score2(clean(x))).sort((a, b) => a - b);
+  return scores[Math.floor(scores.length / 2)];
+}
