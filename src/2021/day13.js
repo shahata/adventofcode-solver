4046e052578c9e70d57271d8b1c4b6af097cffa1
Shahar Talmi <shahar@wix.com>
2021-12-13 08:23:55 +0200

    solved day 13

--- src/2021/day13.js
+++ src/2021/day13.js
@@ -42,6 +42,5 @@ export function part1(input) {
 
 export function part2(input) {
   const paper = fold(input);
-  paper.forEach(line => console.log(line.join('')));
   return ocr(paper.map(line => '.' + line.join('')).join('\n'));
 }
