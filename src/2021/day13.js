e2add23cd8ce088c06ac152835ed94131b7a3ea7
Shahar Talmi <shahar@wix.com>
2021-12-13 08:23:18 +0200

    solved day 13

---
+++ src/2021/day13.js
@@ -0,0 +1,47 @@
+import { ocr } from '../utils/ocr.js';
+
+function fold(input, total = Infinity) {
+  let [paper, folds] = input.split('\n\n');
+  paper = paper.split('\n').map(line => line.split(',').map(x => +x));
+  folds = folds.split('\n').map(line => {
+    const [axis, number] = line.replace('fold along ', '').split('=');
+    return { axis, number: +number };
+  });
+  const maxX = Math.max(...paper.map(l => l[0])) + 1;
+  const maxY = Math.max(...paper.map(l => l[1])) + 1;
+  let board = new Array(maxY).fill().map(() => new Array(maxX).fill('.'));
+  paper.forEach(([x, y]) => (board[y][x] = '#'));
+  for (let i = 0; i < total && i < folds.length; i++) {
+    if (folds[i].axis === 'x') {
+      const rest = board.map(line => line.slice(folds[i].number + 1));
+      board = board.map(line => line.slice(0, folds[i].number));
+      rest.forEach((line, y) =>
+        line.forEach((value, x) => {
+          if (value === '#') board[y][folds[i].number - x - 1] = value;
+        }),
+      );
+    } else if (folds[i].axis === 'y') {
+      const rest = board.slice(folds[i].number + 1);
+      board = board.slice(0, folds[i].number);
+      rest.forEach((line, y) =>
+        line.forEach((value, x) => {
+          if (value === '#') board[folds[i].number - y - 1][x] = value;
+        }),
+      );
+    }
+  }
+  return board;
+}
+
+export function part1(input) {
+  const paper = fold(input, 1);
+  let count = 0;
+  paper.forEach(line => line.forEach(value => value === '#' && count++));
+  return count;
+}
+
+export function part2(input) {
+  const paper = fold(input);
+  paper.forEach(line => console.log(line.join('')));
+  return ocr(paper.map(line => '.' + line.join('')).join('\n'));
+}
---
+++ src/2021/day13.spec.js
@@ -0,0 +1,48 @@
+import { part1, part2 } from './day13.js';
+import readInput from '../utils/read-input.js';
+
+const input = readInput(import.meta.url);
+
+describe('day13 2021', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(
+          [
+            '6,10',
+            '0,14',
+            '9,10',
+            '0,3',
+            '10,4',
+            '4,11',
+            '6,0',
+            '6,12',
+            '4,1',
+            '0,13',
+            '10,12',
+            '3,4',
+            '3,0',
+            '8,4',
+            '1,10',
+            '2,14',
+            '8,10',
+            '9,0',
+            '',
+            'fold along y=7',
+            'fold along x=5',
+          ].join('\n'),
+        ),
+      ).toEqual(17);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(775);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual('REUPUPKR');
+    });
+  });
+});
