a740c4a557aab4e0663748f9e7ac8c57c09df056
Shahar Talmi <shahar@wix.com>
2021-12-16 04:38:52 +0200

    refactor

--- src/2021/day15.js
+++ src/2021/day15.js
@@ -1,67 +1,44 @@
-function neighbors(n, maze) {
+function neighbors({ x, y, risk }, maze) {
   return [
-    maze[n.point.y - 1] && maze[n.point.y - 1][n.point.x + 0],
-    maze[n.point.y + 0] && maze[n.point.y + 0][n.point.x - 1],
-    maze[n.point.y + 0] && maze[n.point.y + 0][n.point.x + 1],
-    maze[n.point.y + 1] && maze[n.point.y + 1][n.point.x + 0],
+    { x, y: y - 1, risk: maze[y - 1] && maze[y - 1][x + 0] },
+    { x: x - 1, y, risk: maze[y + 0] && maze[y + 0][x - 1] },
+    { x: x + 1, y, risk: maze[y + 0] && maze[y + 0][x + 1] },
+    { x, y: y + 1, risk: maze[y + 1] && maze[y + 1][x + 0] },
   ]
-    .filter(x => x)
-    .map(p => ({ point: p, totalRisk: n.totalRisk + p.risk }));
+    .filter(x => x.risk !== undefined)
+    .map(p => ({ x: p.x, y: p.y, risk: risk + p.risk }));
 }
 
 function solve(maze) {
   const visited = new Map();
-  const target = maze[maze.length - 1][maze[0].length - 1];
-  const queue = [{ point: maze[0][0], totalRisk: 0 }];
+  const queue = [{ x: 0, y: 0, risk: 0 }];
   while (queue.length > 0) {
     const n = queue.shift();
-    if (n.point.x === target.x && n.point.y === target.y) {
-      return n.totalRisk;
+    if (n.x === maze[0].length - 1 && n.y === maze.length - 1) {
+      return n.risk;
     }
-    neighbors(n, maze).forEach(o => {
-      const v = visited.get(`${o.point.x},${o.point.y}`);
-      if (!v || o.totalRisk < v.totalRisk) {
+    neighbors(n, maze).forEach(neighbor => {
+      const v = visited.get(`${neighbor.x},${neighbor.y}`);
+      if (!v || neighbor.risk < v.risk) {
         if (v) queue.splice(queue.indexOf(v), 1);
-        visited.set(`${o.point.x},${o.point.y}`, o);
-        queue.push(o);
+        visited.set(`${neighbor.x},${neighbor.y}`, neighbor);
+        queue.push(neighbor);
       }
     });
-    queue.sort((a, b) => a.totalRisk - b.totalRisk);
+    queue.sort((a, b) => a.risk - b.risk);
   }
-}
-
-function parse(input) {
-  return input
-    .split('\n')
-    .map((line, y) => line.split('').map((risk, x) => ({ x, y, risk: +risk })));
-}
-
-function inc(line, i) {
-  return line.map(cell => ({
-    ...cell,
-    risk: cell.risk + i > 9 ? cell.risk + i - 9 : cell.risk + i,
-  }));
 }
 
 export function part1(input) {
-  return solve(parse(input));
+  const maze = input.split('\n').map(line => line.split('').map(risk => +risk));
+  return solve(maze);
 }
 
 export function part2(input) {
-  const maze = parse(input);
-  maze.forEach(line => {
-    const base = [...line];
-    for (let i = 1; i < 5; i++) {
-      line.push(
-        ...inc(base, i).map((cell, x) => ({ ...cell, x: x + base.length * i })),
-      );
-    }
-  });
-  const lines = maze.length;
-  for (let i = 1; i < 5; i++) {
-    for (let j = 0; j < lines; j++) {
-      maze.push(inc(maze[j], i).map(cell => ({ ...cell, y: j + lines * i })));
-    }
-  }
+  let maze = input.split('\n').map(line => line.split('').map(risk => +risk));
+  const inc = (line, i) => line.map(n => (n + i > 9 ? n + i - 9 : n + i));
+  const dup = (maze, i) => maze.map(line => inc(line, i));
+  maze = maze.map(l => l.concat(inc(l, 1), inc(l, 2), inc(l, 3), inc(l, 4)));
+  maze = maze.concat(dup(maze, 1), dup(maze, 2), dup(maze, 3), dup(maze, 4));
   return solve(maze);
 }
