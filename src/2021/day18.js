af252b7a652827f3e2016323c12714b0f87e0a0d
Shahar Talmi <shahar@wix.com>
2021-12-18 12:49:36 +0200

    refactor

--- src/2021/day18.js
+++ src/2021/day18.js
@@ -1,85 +1,63 @@
-function magnitude(expression) {
-  while (expression.length > 1) {
-    const i = expression.findIndex(
-      (v, i, a) => v.type === 'number' && a[i + 1].type === 'number',
-    );
-    expression.splice(i - 1, 4, {
-      type: 'number',
-      value: 3 * expression[i].value + 2 * expression[i + 1].value,
-    });
-  }
-  return expression[0].value;
-}
-
-function parse(str) {
-  let expression = [];
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === '[') expression.push({ type: 'start' });
-    if (str[i] === ']') expression.push({ type: 'end' });
-    if (!isNaN(+str[i])) {
-      expression.push({ type: 'number', value: +str[i] });
-    }
-  }
-  return expression;
-}
-
-function reduce(expression = []) {
-  let counter = 0;
-  let lastNumber = -1;
-  for (let i = 0; i < expression.length; i++) {
-    if (expression[i].type === 'start') counter++;
-    if (expression[i].type === 'end') counter--;
-    if (expression[i].type === 'number') lastNumber = i;
-
-    if (counter === 5) {
-      const nextNumber = expression
-        .slice(i + 4)
-        .findIndex(v => v.type === 'number');
-      if (lastNumber !== -1) {
-        expression[lastNumber].value += expression[i + 1].value;
-      }
-      if (nextNumber !== -1) {
-        expression[nextNumber + i + 4].value += expression[i + 2].value;
-      }
-      expression.splice(i, 4, { type: 'number', value: 0 });
-      return true;
+function reduce(snail) {
+  let depth = 0;
+  let last = -1;
+  for (let i = 0; i < snail.length; i++) {
+    if (snail[i].type === '[') depth++;
+    if (snail[i].type === ']') depth--;
+    if (snail[i].type === 'X') last = i;
+    if (depth === 5) {
+      const next = snail.findIndex((v, j) => j >= i + 4 && v.type === 'X');
+      if (last !== -1) snail[last].value += snail[i + 1].value;
+      if (next !== -1) snail[next].value += snail[i + 2].value;
+      return !!snail.splice(i, 4, { type: 'X', value: 0 });
     }
   }
 
-  const splitIndex = expression.findIndex(x => x.value >= 10);
+  const splitIndex = snail.findIndex(x => x.value >= 10);
   if (splitIndex !== -1) {
     const split = [
-      { type: 'start' },
-      { type: 'number', value: Math.floor(expression[splitIndex].value / 2) },
-      { type: 'number', value: Math.ceil(expression[splitIndex].value / 2) },
-      { type: 'end' },
+      { type: '[' },
+      { type: 'X', value: Math.floor(snail[splitIndex].value / 2) },
+      { type: 'X', value: Math.ceil(snail[splitIndex].value / 2) },
+      { type: ']' },
     ];
-    expression.splice(splitIndex, 1, ...split);
-    return true;
+    return !!snail.splice(splitIndex, 1, ...split);
   }
-  return false;
 }
 
 function add(a, b) {
-  const expression = [{ type: 'start' }, ...a, ...b, { type: 'end' }];
-  while (reduce(expression));
-  return expression;
+  const snail = [{ type: '[' }, ...a, ...b, { type: ']' }];
+  while (reduce(snail));
+  return snail;
+}
+
+function magnitude(snail) {
+  const sum = i => 3 * snail[i + 1].value + 2 * snail[i + 2].value;
+  const pair = i => snail[i + 1].type === 'X' && snail[i + 2].type === 'X';
+  while (snail.length > 1) {
+    const next = snail.findIndex((v, i) => pair(i));
+    snail.splice(next, 4, { type: 'X', value: sum(next) });
+  }
+  return snail[0].value;
+}
+
+function parse(str) {
+  str = str.replaceAll(',', '');
+  return str.split('').map(x => ({ type: isNaN(+x) ? x : 'X', value: +x }));
 }
 
 export function part1(input) {
-  const lines = input.split('\n').map(x => parse(x));
+  const lines = input.split('\n').map(parse);
   return magnitude(lines.reduce((a, b) => add(a, b)));
 }
 
 export function part2(input) {
   const lines = input.split('\n');
   let max = 0;
-  for (let i = 0; i < lines.length; i++) {
-    for (let j = 0; j < lines.length; j++) {
-      if (i !== j) {
-        let result = add(parse(lines[i]), parse(lines[j]));
-        max = Math.max(max, magnitude(result));
-      }
+  for (let a of lines) {
+    for (let b of lines) {
+      let result = a !== b ? add(parse(a), parse(b)) : [{ value: 0 }];
+      max = Math.max(max, magnitude(result));
     }
   }
   return max;
