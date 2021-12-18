38846dc571448880bec190c8bda401b5df0634f1
Shahar Talmi <shahar@wix.com>
2021-12-18 18:45:39 +0200

    optimize

--- src/2021/day18.js
+++ src/2021/day18.js
@@ -1,62 +1,59 @@
-function reduce(snail) {
-  let depth = 0;
-  let last = -1;
-  for (let i = 0; i < snail.length; i++) {
-    if (snail[i].type === '[') depth++;
-    if (snail[i].type === ']') depth--;
-    if (snail[i].type === 'X') last = i;
-    if (depth === 5) {
-      const next = snail.findIndex((v, j) => j >= i + 4 && v.type === 'X');
-      if (last !== -1) snail[last].value += snail[i + 1].value;
-      if (next !== -1) snail[next].value += snail[i + 2].value;
-      return !!snail.splice(i, 4, { type: 'X', value: 0 });
-    }
-  }
+const open = -1;
+const close = -2;
 
-  const splitIndex = snail.findIndex(x => x.value >= 10);
-  if (splitIndex !== -1) {
-    const split = [
-      { type: '[' },
-      { type: 'X', value: Math.floor(snail[splitIndex].value / 2) },
-      { type: 'X', value: Math.ceil(snail[splitIndex].value / 2) },
-      { type: ']' },
-    ];
-    return !!snail.splice(splitIndex, 1, ...split);
+function run(snail, split) {
+  let depth = 0;
+  let last, next;
+  for (let i = 0; i < snail.length; i++) {
+    if (split && snail[i] >= 10) {
+      const number = snail[i] / 2;
+      snail.splice(i, 1, open, Math.floor(number), Math.ceil(number), close);
+    }
+    if (depth === 4 && snail[i] === open) {
+      for (next = i + 4; next < snail.length && snail[next] < 0; next++);
+      if (snail[last] !== undefined) snail[last] += snail[i + 1];
+      if (snail[next] !== undefined) snail[next] += snail[i + 2];
+      snail.splice(i, 4, 0);
+      return true;
+    }
+    if (snail[i] === open) depth++;
+    if (snail[i] === close) depth--;
+    if (snail[i] >= 0) last = i;
   }
 }
 
 function add(a, b) {
-  const snail = [{ type: '[' }, ...a, ...b, { type: ']' }];
-  while (reduce(snail));
+  const snail = [open, ...a, ...b, close];
+  while (run(snail, false));
+  while (run(snail, true));
   return snail;
 }
 
 function magnitude(snail) {
-  const sum = i => 3 * snail[i + 1].value + 2 * snail[i + 2].value;
-  const pair = i => snail[i + 1].type === 'X' && snail[i + 2].type === 'X';
+  const sum = i => 3 * snail[i + 1] + 2 * snail[i + 2];
+  const pair = i => snail[i + 1] >= 0 && snail[i + 2] >= 0;
   while (snail.length > 1) {
-    const next = snail.findIndex((v, i) => pair(i));
-    snail.splice(next, 4, { type: 'X', value: sum(next) });
+    const next = snail.findIndex((x, i) => pair(i));
+    snail.splice(next, 4, sum(next));
   }
-  return snail[0].value;
+  return snail[0];
 }
 
 function parse(str) {
-  str = str.replaceAll(',', '');
-  return str.split('').map(x => ({ type: isNaN(+x) ? x : 'X', value: +x }));
+  const snail = str.replaceAll(',', '').split('');
+  return snail.map(x => (x === '[' ? open : x === ']' ? close : +x));
 }
 
 export function part1(input) {
-  const lines = input.split('\n').map(parse);
-  return magnitude(lines.reduce(add));
+  return magnitude(input.split('\n').map(parse).reduce(add));
 }
 
 export function part2(input) {
-  const lines = input.split('\n');
+  const lines = input.split('\n').map(parse);
   let max = 0;
   for (let a of lines) {
     for (let b of lines) {
-      max = Math.max(max, a !== b ? magnitude(add(parse(a), parse(b))) : 0);
+      max = Math.max(max, a !== b ? magnitude(add(a, b)) : 0);
     }
   }
   return max;
