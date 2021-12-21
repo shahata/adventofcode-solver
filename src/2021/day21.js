242c7733caa8e4ea4475928a4b26e03851f5f778
Shahar Talmi <shahar@wix.com>
2021-12-21 16:27:50 +0200

    refactor

--- src/2021/day21.js
+++ src/2021/day21.js
@@ -14,21 +14,21 @@ export function part1(input) {
   return Math.min(...score) * (i - 1);
 }
 
-function play(player1, player2, memory = new Map()) {
-  if (player2.score >= 21) return [0, 1];
+function play(currPlayer, prevPlayer, memory = new Map()) {
+  if (prevPlayer.score >= 21) return [0, 1];
 
-  const key = JSON.stringify({ player1, player2 });
+  const key = JSON.stringify({ player1: currPlayer, player2: prevPlayer });
   const result = memory.get(key);
   if (result) return result;
 
   const wins = [0, 0];
   const odds = { 3: 1, 9: 1, 4: 3, 8: 3, 5: 6, 7: 6, 6: 7 };
   for (const key in odds) {
-    let position = player1.position + +key;
+    let position = currPlayer.position + +key;
     if (position > 10) position -= 10;
     const next = play(
-      player2,
-      { position, score: player1.score + position },
+      prevPlayer,
+      { position, score: currPlayer.score + position },
       memory,
     );
     wins[0] += next[1] * odds[key];
