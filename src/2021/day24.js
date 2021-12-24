9652b20430bcb4fb5c823d2c2280091e814db0c2
Shahar Talmi <shahar@wix.com>
2021-12-24 22:32:57 +0200

    refactor

--- src/2021/day24.js
+++ src/2021/day24.js
@@ -1,45 +1,31 @@
-export function part1(input) {
-  const result = new Array(14).fill(9);
+function solve(input, digit) {
+  const result = new Array(14).fill(digit);
   const stack = [];
   let push;
   input.split('\n').forEach((line, i) => {
     const value = +line.split(' ').pop();
+    const curr = Math.floor(i / 18);
     if (i % 18 === 4) push = value === 1;
     if (push && i % 18 === 15) {
-      stack.push({ index: Math.floor(i / 18), diff: +value });
+      stack.push({ index: curr, diff: value });
     }
     if (!push && i % 18 === 5) {
       let { index, diff } = stack.pop();
-      diff += +value;
-      if (diff <= 0) {
-        result[Math.floor(i / 18)] = 9 + diff;
+      diff += value;
+      if (result[index] + diff > 0 && result[index] + diff < 10) {
+        result[curr] = result[index] + diff;
       } else {
-        result[index] = 9 - diff;
+        result[index] = result[curr] - diff;
       }
     }
   });
   return +result.join('');
 }
 
+export function part1(input) {
+  return solve(input, 9);
+}
+
 export function part2(input) {
-  const result = new Array(14).fill(1);
-  const stack = [];
-  let push;
-  input.split('\n').forEach((line, i) => {
-    const value = +line.split(' ').pop();
-    if (i % 18 === 4) push = value === 1;
-    if (push && i % 18 === 15) {
-      stack.push({ index: Math.floor(i / 18), diff: +value });
-    }
-    if (!push && i % 18 === 5) {
-      let { index, diff } = stack.pop();
-      diff += +value;
-      if (diff <= 0) {
-        result[index] = 1 - diff;
-      } else {
-        result[Math.floor(i / 18)] = 1 + diff;
-      }
-    }
-  });
-  return +result.join('');
+  return solve(input, 1);
 }
--- src/2021/day24.spec.js
+++ src/2021/day24.spec.js
@@ -5,20 +5,12 @@ const input = readInput(import.meta.url);
 
 describe('day24 2021', () => {
   describe('part1', () => {
-    it('should work for part 1 examples', () => {
-      // expect(part1('1')).toEqual(0);
-    });
-
     it('should work for part 1 input', () => {
       expect(part1(input)).toEqual(53999995829399);
     });
   });
 
   describe('part2', () => {
-    it('should work for part 2 examples', () => {
-      // expect(part2('1')).toEqual(0);
-    });
-
     it('should work for part 2 input', () => {
       expect(part2(input)).toEqual(11721151118175);
     });
