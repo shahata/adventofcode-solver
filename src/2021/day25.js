f6144638f40db57ddec85b6f289c805d3e6ed8c2
Shahar Talmi <shahar@wix.com>
2021-12-25 12:40:03 +0200

    refactor

--- src/2021/day25.js
+++ src/2021/day25.js
@@ -1,30 +1,32 @@
 export function part1(input) {
-  let sea = input
-    .split('\n')
-    .map((line, y) => line.split('').map((c, x) => ({ c, x, y })));
+  let sea = input.split('\n').map(line => line.split('').map(c => c));
   let next, i;
   for (i = 0; JSON.stringify(sea) !== JSON.stringify(next); i++) {
     sea = next || sea;
-    next = sea.map(line => line.map(({ x, y }) => ({ c: '.', x, y })));
-    sea.flat().forEach(({ c, x, y }) => {
-      if (c === '>') {
-        const n = x + 1 === sea[0].length ? 0 : x + 1;
-        if (sea[y][n].c === '.') {
-          next[y][n] = { c, x: n, y };
-        } else {
-          next[y][x] = { c, x, y };
+    next = sea.map(line => line.map(() => '.'));
+    sea.forEach((line, y) => {
+      line.forEach((c, x) => {
+        if (c === '>') {
+          const n = (x + 1) % line.length;
+          if (sea[y][n] === '.') {
+            next[y][n] = c;
+          } else {
+            next[y][x] = c;
+          }
         }
-      }
+      });
     });
-    sea.flat().forEach(({ c, x, y }) => {
-      if (c === 'v') {
-        let n = y + 1 === sea.length ? 0 : y + 1;
-        if (sea[n][x].c !== 'v' && next[n][x].c === '.') {
-          next[n][x] = { c, x, y: n };
-        } else {
-          next[y][x] = { c, x, y };
+    sea.forEach((line, y) => {
+      line.forEach((c, x) => {
+        if (c === 'v') {
+          const n = (y + 1) % sea.length;
+          if (sea[n][x] !== c && next[n][x] === '.') {
+            next[n][x] = c;
+          } else {
+            next[y][x] = c;
+          }
         }
-      }
+      });
     });
   }
   return i;
