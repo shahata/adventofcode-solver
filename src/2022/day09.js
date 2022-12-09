b07896871d81e6957222e4310ca2f623806ec8ab
Shahar Talmi <shahar@wix.com>
2022-12-09 07:50:36 +0200

    solved day 9

---
+++ src/2022/day09.js
@@ -0,0 +1,36 @@
+export function part1(input, len = 2) {
+  const steps = input.split('\n').map(line => {
+    const [direction, count] = line.split(' ');
+    return { direction, count: +count };
+  });
+  let knots = new Array(len).fill().map(() => ({ x: 0, y: 0 }));
+  let visited = new Set([`${knots[len - 1].x},${knots[len - 1].y}`]);
+  for (const step of steps) {
+    for (let i = 0; i < step.count; i++) {
+      if (step.direction === 'R') knots[0].x++;
+      if (step.direction === 'L') knots[0].x--;
+      if (step.direction === 'D') knots[0].y++;
+      if (step.direction === 'U') knots[0].y--;
+      for (let j = 1; j < knots.length; j++) {
+        const H = knots[j - 1];
+        const T = knots[j];
+        if (H.x === T.x && Math.abs(H.y - T.y) === 2) {
+          T.y = H.y > T.y ? T.y + 1 : T.y - 1;
+        }
+        if (H.y === T.y && Math.abs(H.x - T.x) === 2) {
+          T.x = H.x > T.x ? T.x + 1 : T.x - 1;
+        }
+        if (Math.abs(H.x - T.x) === 2 || Math.abs(H.y - T.y) === 2) {
+          T.x = H.x > T.x ? T.x + 1 : T.x - 1;
+          T.y = H.y > T.y ? T.y + 1 : T.y - 1;
+        }
+      }
+      visited.add(`${knots[len - 1].x},${knots[len - 1].y}`);
+    }
+  }
+  return visited.size;
+}
+
+export function part2(input) {
+  return part1(input, 10);
+}
---
+++ src/2022/day09.spec.js
@@ -0,0 +1,36 @@
+import { part1, part2 } from './day09.js';
+import readInput from '../utils/read-input.js';
+
+const input = readInput(import.meta.url);
+
+describe('day09 2022', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(
+          ['R 4', 'U 4', 'L 3', 'D 1', 'R 4', 'D 1', 'L 5', 'R 2'].join('\n'),
+        ),
+      ).toEqual(13);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(6376);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(
+        part2(
+          ['R 5', 'U 8', 'L 8', 'D 3', 'R 17', 'D 10', 'L 25', 'U 20'].join(
+            '\n',
+          ),
+        ),
+      ).toEqual(36);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(2607);
+    });
+  });
+});
