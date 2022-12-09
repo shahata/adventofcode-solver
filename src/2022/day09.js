bc43ff74aa424ff23f641f619719b305e7d259a6
Shahar Talmi <shahar@wix.com>
2022-12-09 08:09:24 +0200

    solved day 9

--- src/2022/day09.js
+++ src/2022/day09.js
@@ -1,16 +1,13 @@
 export function part1(input, len = 2) {
-  const steps = input.split('\n').map(line => {
-    const [direction, count] = line.split(' ');
-    return { direction, count: +count };
-  });
+  const steps = input.split('\n').map(line => line.split(' '));
   const knots = new Array(len).fill().map(() => ({ x: 0, y: 0 }));
   const visited = new Set([`${knots[len - 1].x},${knots[len - 1].y}`]);
-  for (const step of steps) {
-    for (let i = 0; i < step.count; i++) {
-      if (step.direction === 'R') knots[0].x++;
-      if (step.direction === 'L') knots[0].x--;
-      if (step.direction === 'D') knots[0].y++;
-      if (step.direction === 'U') knots[0].y--;
+  for (const [direction, count] of steps) {
+    for (let i = 0; i < +count; i++) {
+      if (direction === 'R') knots[0].x++;
+      if (direction === 'L') knots[0].x--;
+      if (direction === 'D') knots[0].y++;
+      if (direction === 'U') knots[0].y--;
       for (let j = 1; j < knots.length; j++) {
         const [H, T] = [knots[j - 1], knots[j]];
         if (Math.abs(H.x - T.x) === 2 || Math.abs(H.y - T.y) === 2) {
