9b204b73bf642c7bd3f069bb4e7cc8812b57dac6
Shahar Talmi <shahar@wix.com>
2022-12-10 09:59:37 +0200

    fix ocr

--- src/utils/ocr.js
+++ src/utils/ocr.js
@@ -36,8 +36,8 @@ const alphabet = {
 
 export function ocr(image) {
   let lines = image.trim().split('\n');
-  let width = lines.length === 6 ? 4 : 6;
-  let spaces = lines.length === 6 ? 1 : 2;
+  const width = lines.length === 6 ? 4 : 6;
+  const spaces = lines.length === 6 ? 1 : 2;
   let result = '';
   if (lines.length === 6 && lines[0].length % 5 !== 0) {
     lines = lines.map(x => x.replace(/^\./, ''));
