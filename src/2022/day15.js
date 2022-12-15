31e7ee46c4c14d665d03ab2d2fadb07b92c0ad3f
Shahar Talmi <shahar@wix.com>
2022-12-15 17:10:30 +0200

    small refactor

--- src/2022/day15.js
+++ src/2022/day15.js
@@ -1,40 +1,39 @@
-export function part1(input, y = 2000000) {
-  const sensors = input.split('\n').map(line => {
+function parse(input) {
+  return input.split('\n').map(line => {
     const [, sx, sy, bx, by] = line.match(
       /^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$/,
     );
     return { sx: +sx, sy: +sy, bx: +bx, by: +by };
   });
+}
+
+export function part1(input, y = 2000000) {
+  const sensors = parse(input);
   const options = new Set();
-  let beacons = new Set();
   for (const { sx, sy, bx, by } of sensors) {
     const distance = Math.abs(sx - bx) + Math.abs(sy - by) - Math.abs(sy - y);
-    for (let i = sx - distance; i <= sx + distance; i++) options.add(i);
-    if (by === y) beacons.add(bx);
+    for (let x = sx - distance; x <= sx + distance; x++) {
+      if (bx !== x || by !== y) options.add(x);
+    }
   }
-  return options.size - beacons.size;
+  return options.size;
 }
 
 export function part2(input, size = 4000000) {
-  const sensors = input.split('\n').map(line => {
-    const [, sx, sy, bx, by] = line.match(
-      /^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$/,
-    );
-    return { sx: +sx, sy: +sy, bx: +bx, by: +by };
-  });
-  const map = new Array(size + 1).fill().map(() => []);
+  const sensors = parse(input);
+  const lines = new Array(size + 1).fill().map(() => []);
   for (const { sx, sy, bx, by } of sensors) {
-    map.forEach((ranges, y) => {
+    lines.forEach((ranges, y) => {
       const distance = Math.abs(sx - bx) + Math.abs(sy - by) - Math.abs(sy - y);
       if (distance >= 0) ranges.push([sx - distance, sx + distance]);
       if (by === y) ranges.push([bx, bx]);
     });
   }
-  for (let y = 0; y < map.length; y++) {
-    const ranges = map[y].sort((a, b) => a[0] - b[0]);
+  for (let y = 0; y < lines.length; y++) {
+    const ranges = lines[y].sort((a, b) => a[0] - b[0]);
     let guess = 0;
     for (const range of ranges) {
-      if (range[0] <= guess && range[1] >= guess) guess = range[1] + 1;
+      if (guess >= range[0] && guess <= range[1]) guess = range[1] + 1;
     }
     if (guess <= size) return guess * 4000000 + y;
   }
