d2e5251016e2e24a6bbb767348077de1386367cf
Shahar Talmi <shahar@wix.com>
2022-12-16 19:53:27 +0200

    optimized a bit

--- src/2022/day16.js
+++ src/2022/day16.js
@@ -1,9 +1,9 @@
-function distance(valves, valve, id) {
-  const queue = [{ id: valve.id, steps: 0 }];
-  const visited = new Set(`${valve.id},0`);
+function distance(valves, from, to) {
+  const queue = [{ id: from, steps: 0 }];
+  const visited = new Set(`${from},0`);
   while (queue.length > 0) {
     const current = queue.shift();
-    if (current.id === id) return current.steps;
+    if (current.id === to) return current.steps;
     const neighbors = valves[current.id].destination
       .map(id => ({ id, steps: current.steps + 1 }))
       .filter(x => !visited.has(`${x.id},${x.steps}`));
@@ -25,7 +25,7 @@ function parse(input) {
   Object.values(valves).forEach(valve => {
     valve.paths = Object.values(valves)
       .filter(({ id, rate }) => id !== valve.id && rate > 0)
-      .map(({ id }) => ({ id, distance: distance(valves, valve, id) }));
+      .map(({ id }) => ({ id, distance: distance(valves, valve.id, id) }));
   });
   return valves;
 }
@@ -34,13 +34,13 @@ function best(valves, current, open, pressure, time, elephant) {
   if (time === 0) return pressure;
   const results = valves[current].paths
     .filter(({ id, distance }) => !open.has(id) && time > distance)
-    .flatMap(({ id, distance }) => {
+    .map(({ id, distance }) => {
       const nextOpen = new Set(open).add(id);
       const nextPressure = pressure + valves[id].rate * (time - distance - 1);
-      return [
+      return Math.max(
         best(valves, id, nextOpen, nextPressure, time - distance - 1, elephant),
-        elephant && best(valves, 'AA', nextOpen, nextPressure, 26, !elephant),
-      ];
+        elephant ? best(valves, 'AA', nextOpen, nextPressure, 26, false) : 0,
+      );
     });
   return Math.max(pressure, ...results);
 }
