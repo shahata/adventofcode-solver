42df11f02fa6866e755b908da2dfcbe71b7a39e2
Shahar Talmi <shahar@wix.com>
2022-12-16 12:50:27 +0200

    smalle refactor

--- src/2022/day16.js
+++ src/2022/day16.js
@@ -27,10 +27,11 @@ function parse(input) {
       .filter(({ id }) => id !== valve.id)
       .map(({ id }) => ({ id, distance: distance(valves, valve, id) }));
   });
-  return valves;
+  const open = Object.keys(valves).filter(id => valves[id].rate === 0);
+  return { valves, open };
 }
 
-function dfs(valves, current, open, pressure, time, elephant) {
+function best(valves, current, open, pressure, time, elephant) {
   if (time === 0) return pressure;
   const results = valves[current].paths
     .filter(({ id, distance }) => !open.has(id) && time > distance)
@@ -38,21 +39,19 @@ function dfs(valves, current, open, pressure, time, elephant) {
       const nextOpen = new Set(open).add(id);
       const nextPressure = pressure + valves[id].rate * (time - distance - 1);
       return [
-        dfs(valves, id, nextOpen, nextPressure, time - distance - 1, elephant),
-        elephant && dfs(valves, 'AA', nextOpen, nextPressure, 26, !elephant),
+        best(valves, id, nextOpen, nextPressure, time - distance - 1, elephant),
+        elephant && best(valves, 'AA', nextOpen, nextPressure, 26, !elephant),
       ];
     });
   return Math.max(pressure, ...results);
 }
 
 export function part1(input) {
-  const valves = parse(input);
-  const open = Object.keys(valves).filter(id => valves[id].rate === 0);
-  return dfs(valves, 'AA', new Set(open), 0, 30, false);
+  const { valves, open } = parse(input);
+  return best(valves, 'AA', new Set(open), 0, 30, false);
 }
 
 export function part2(input) {
-  const valves = parse(input);
-  const open = Object.keys(valves).filter(id => valves[id].rate === 0);
-  return dfs(valves, 'AA', new Set(open), 0, 26, true);
+  const { valves, open } = parse(input);
+  return best(valves, 'AA', new Set(open), 0, 26, true);
 }
