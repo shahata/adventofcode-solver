a68f9385f269c31b5ea52a933bd1c78a85ae43ef
Shahar Talmi <shahar@wix.com>
2022-12-17 16:25:25 +0200

    some refactor

--- src/2022/day17.js
+++ src/2022/day17.js
@@ -1,103 +1,95 @@
-const shapes = [
-  ['####'],
-  ['.#.', '###', '.#.'],
-  ['..#', '..#', '###'],
-  ['#', '#', '#', '#'],
-  ['##', '##'],
-];
-
 function toCells(shape) {
-  const result = [];
+  const cells = [];
   for (let y = 0; y < shape.length; y++) {
     for (let x = 0; x < shape[y].length; x++) {
-      if (shape[y][x] === '#') result.push({ x, y });
+      if (shape[y][x] === '#') cells.push({ x, y });
     }
   }
-  return result;
+  return { cells, shapeHeight: shape.length };
+}
+
+function moveIfPossible(cave, pos, cells, offset) {
+  if (
+    cells.every(
+      cell =>
+        cave[pos.y + cell.y + offset.y] &&
+        cave[pos.y + cell.y + offset.y][pos.x + cell.x + offset.x] === '.',
+    )
+  ) {
+    pos.x += offset.x;
+    pos.y += offset.y;
+    return true;
+  }
+  return false;
+}
+
+function detectLoop(cave, height, memory) {
+  if (memory) {
+    const snapshot = JSON.stringify(cave.slice(0, 1000));
+    const found = memory.filter(s => s.snapshot === snapshot);
+    if (found.length === 2) {
+      const heightDiff = found[1].height - found[0].height;
+      const indexDiff = memory.indexOf(found[1]) - memory.indexOf(found[0]);
+      memory = null;
+      return { indexDiff, heightDiff };
+    } else {
+      memory.push({ snapshot, height });
+    }
+  }
+}
+
+function dropShape(cave, cells, stream) {
+  const pos = { x: 2, y: 0 };
+  let resting = false;
+  let falling = false;
+  while (!resting) {
+    if (!falling) {
+      const currentStream = stream.input[stream.index++ % stream.input.length];
+      const offset = { x: currentStream === '<' ? -1 : 1, y: 0 };
+      moveIfPossible(cave, pos, cells, offset);
+    } else if (!moveIfPossible(cave, pos, cells, { x: 0, y: 1 })) {
+      cells.forEach(cell => (cave[pos.y + cell.y][pos.x + cell.x] = '#'));
+      resting = true;
+    }
+    falling = !falling;
+  }
+  return pos.y;
 }
 
 export function part1(input, times = 2022) {
-  let memory = [];
-  const memory2 = [];
+  const stream = { input, index: 0 };
+  const shapes = [
+    ['####'],
+    ['.#.', '###', '.#.'],
+    ['..#', '..#', '###'],
+    ['#', '#', '#', '#'],
+    ['##', '##'],
+  ].map(toCells);
+
   const cave = [];
+  let memory = [];
   let depth = 0;
-  let si = 0,
-    height = 0;
+  let height = 0;
   for (let i = 0; i < times; i++) {
-    const shape = shapes[i % shapes.length];
-    const cells = toCells(shape);
-    while (depth < height + 3 + shape.length) {
+    const { cells, shapeHeight } = shapes[i % shapes.length];
+    while (depth < height + shapeHeight + 3) {
       cave.unshift(new Array(7).fill('.'));
       depth++;
     }
-    while (depth > height + 3 + shape.length) {
+    while (depth > height + shapeHeight + 3) {
       cave.shift();
       depth--;
     }
-    if (memory) {
-      const snapshot = JSON.stringify({ lines: cave.slice(0, 1000), shape });
-      const found = memory.filter(s => s === snapshot);
-      if (found.length === 2) {
-        const i1 = memory.indexOf(found[0]);
-        const i2 = memory.lastIndexOf(found[1]);
-        const hdiff = memory2[i2] - memory2[i1];
-        const idiff = i2 - i1;
-        const mul = Math.floor((times - i) / idiff);
-        i += mul * idiff;
-        height += mul * hdiff;
-        depth += mul * hdiff;
-        memory = null;
-      } else {
-        memory.push(snapshot);
-        memory2.push(height);
-      }
+    const loop = detectLoop(cave, height, memory);
+    if (loop) {
+      const mul = Math.floor((times - i) / loop.indexDiff);
+      i += mul * loop.indexDiff;
+      height += mul * loop.heightDiff;
+      depth += mul * loop.heightDiff;
+      memory = null;
     }
-    const location = { x: 2, y: 0 };
-    let resting = false;
-    let falling = false;
-    while (!resting) {
-      if (!falling) {
-        const stream = input[si % input.length];
-        si++;
-        if (stream === '<') {
-          if (
-            cells.every(
-              cell =>
-                cave[location.y + cell.y][location.x + cell.x - 1] === '.',
-            )
-          ) {
-            location.x--;
-          }
-        } else if (stream === '>') {
-          if (
-            cells.every(
-              cell =>
-                cave[location.y + cell.y][location.x + cell.x + 1] === '.',
-            )
-          ) {
-            location.x++;
-          }
-        }
-        falling = true;
-      } else {
-        if (
-          cells.every(
-            cell =>
-              cave[location.y + cell.y + 1] &&
-              cave[location.y + cell.y + 1][location.x + cell.x] === '.',
-          )
-        ) {
-          location.y++;
-        } else {
-          cells.forEach(cell => {
-            height = Math.max(depth - (location.y + cell.y), height);
-            cave[location.y + cell.y][location.x + cell.x] = '#';
-          });
-          resting = true;
-        }
-        falling = false;
-      }
-    }
+    const y = dropShape(cave, cells, stream);
+    height = Math.max(height, depth - y);
   }
   return height;
 }
