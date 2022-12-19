8221bb72a2ffce3f03877839490f7259e51e9b83
Shahar Talmi <shahar@wix.com>
2022-12-20 00:59:26 +0200

    solved day 19

---
+++ src/2022/day19.js
@@ -0,0 +1,79 @@
+function parse(input) {
+  const indexMap = { ore: 0, clay: 1, obsidian: 2, geode: 3 };
+  return input.split('\n').map(line => {
+    let [, plans] = line.split(': ');
+    plans = plans.split('. ').map(line => {
+      const [, cost] = line.match(/^Each [^\s]+ robot costs ([^.]*)\.?$/);
+      const requirements = [0, 0, 0, 0];
+      cost.split(' and ').forEach(s => {
+        const [resourceCount, resourceType] = s.split(' ');
+        requirements[indexMap[resourceType]] = +resourceCount;
+      });
+      return requirements;
+    });
+    return plans;
+  });
+}
+
+function best(blueprint, timeAvailable) {
+  const queue = [
+    {
+      resources: [0, 0, 0, 0],
+      robots: [1, 0, 0, 0],
+      time: timeAvailable,
+    },
+  ];
+  let max = 0;
+  const maxNeeded = [
+    //completely bogus heuristic
+    Math.max(...blueprint.map(cost => cost[0])),
+    Math.max(...blueprint.map(cost => cost[1])),
+    Math.max(...blueprint.map(cost => cost[2])),
+    Infinity,
+  ];
+  while (queue.length > 0) {
+    const next = queue.pop();
+    let { resources, robots, time } = next;
+    max = Math.max(max, resources[3] + robots[3] * time);
+
+    blueprint.forEach((requirements, robotType) => {
+      let buildTime = 1;
+      requirements.forEach((cost, r) => {
+        const timeForResource = Math.ceil((cost - resources[r]) / robots[r]);
+        if (cost > 0) buildTime = Math.max(buildTime, timeForResource + 1);
+      });
+      if (buildTime < time && resources[robotType] <= maxNeeded[robotType]) {
+        const nextResources = [...resources];
+        const nextRobots = [...robots];
+        requirements.forEach((cost, r) => {
+          nextResources[r] += robots[r] * buildTime - cost;
+        });
+        nextRobots[robotType]++;
+        queue.push({
+          resources: nextResources,
+          robots: nextRobots,
+          time: time - buildTime,
+        });
+      }
+    });
+  }
+  return max;
+}
+
+export function part1(input) {
+  const blueprints = parse(input);
+  let score = 0;
+  blueprints.forEach((blueprint, i) => {
+    score += (i + 1) * best(blueprint, 24);
+  });
+  return score;
+}
+
+export function part2(input) {
+  const blueprints = parse(input);
+  let score = 1;
+  blueprints.slice(0, 3).forEach(blueprint => {
+    score *= best(blueprint, 32);
+  });
+  return score;
+}
---
+++ src/2022/day19.spec.js
@@ -0,0 +1,40 @@
+import { part1, part2 } from './day19.js';
+import readInput from '../utils/read-input.js';
+
+const input = readInput(import.meta.url);
+
+describe('day19 2022', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(
+          [
+            'Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.',
+            'Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.',
+          ].join('\n'),
+        ),
+      ).toEqual(33);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(1589);
+    });
+  });
+
+  describe('part2', () => {
+    // it('should work for part 2 examples', () => {
+    //   expect(
+    //     part2(
+    //       [
+    //         'Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.',
+    //         'Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.',
+    //       ].join('\n'),
+    //     ),
+    //   ).toEqual(56 * 62);
+    // });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(29348);
+    });
+  });
+});
