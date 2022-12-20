89d085ed8b245688c546e19ba0e43138a1b88161
Shahar Talmi <shahar@wix.com>
2022-12-20 18:08:54 +0200

    update leaderboard

--- src/2022/day20.js
+++ src/2022/day20.js
@@ -1,10 +1,11 @@
 function mix(original, numbers) {
   for (const move of original) {
-    let i = numbers.indexOf(move);
-    const next = numbers.splice(i, 1)[0];
-    const pos = (i + next.num) % numbers.length;
-    if (pos === 0 && next.num !== 0) numbers.push(next);
-    else numbers.splice(pos, 0, next);
+    const prev = numbers.indexOf(move);
+    numbers.splice(prev, 1);
+
+    const next = (prev + move.num) % numbers.length;
+    if (next === 0 && move.num !== 0) numbers.push(move);
+    else numbers.splice(next, 0, move);
   }
 }
 
@@ -12,11 +13,14 @@ export function part1(input, key = 1, times = 1) {
   const original = input.split('\n').map(x => ({ num: +x * key }));
   const numbers = original.slice(0);
   for (let i = 0; i < times; i++) mix(original, numbers);
+
   const base = numbers.findIndex(x => x.num === 0);
   if (numbers.length < 1000) return numbers.map(x => x.num).join(', ');
-  return [1000, 2000, 3000]
-    .map(n => numbers[base + n].num)
-    .reduce((a, b) => a + b);
+  return (
+    numbers[base + 1000].num +
+    numbers[base + 2000].num +
+    numbers[base + 3000].num
+  );
 }
 
 export function part2(input) {
