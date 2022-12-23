8ee5b9e8e35a85fa6de39d29ebb8dd4e56c53b62
Shahar Talmi <shahar@wix.com>
2022-12-23 09:56:46 +0200

    small refactor

--- src/2022/day23.js
+++ src/2022/day23.js
@@ -3,26 +3,26 @@ function getOptions(check, key) {
   switch (check) {
     case 'north':
       return [
-        { x: x - 1, y: y - 1 },
         { x, y: y - 1 },
+        { x: x - 1, y: y - 1 },
         { x: x + 1, y: y - 1 },
       ].map(({ x, y }) => `${x},${y}`);
     case 'south':
       return [
-        { x: x - 1, y: y + 1 },
         { x, y: y + 1 },
+        { x: x - 1, y: y + 1 },
         { x: x + 1, y: y + 1 },
       ].map(({ x, y }) => `${x},${y}`);
     case 'west':
       return [
-        { x: x - 1, y: y - 1 },
         { x: x - 1, y },
+        { x: x - 1, y: y - 1 },
         { x: x - 1, y: y + 1 },
       ].map(({ x, y }) => `${x},${y}`);
     case 'east':
       return [
-        { x: x + 1, y: y - 1 },
         { x: x + 1, y },
+        { x: x + 1, y: y - 1 },
         { x: x + 1, y: y + 1 },
       ].map(({ x, y }) => `${x},${y}`);
   }
@@ -32,11 +32,11 @@ function round(elves, checks) {
   const proposals = new Map();
   for (const key of elves.keys()) {
     const all = checks.flatMap(check => getOptions(check, key));
-    if (all.every(o => !elves.has(o))) continue;
+    if (all.every(option => !elves.has(option))) continue;
     for (const check of checks) {
       const options = getOptions(check, key);
-      const decision = options[1];
       if (options.every(option => !elves.has(option))) {
+        const decision = options[0];
         proposals.set(decision, (proposals.get(decision) || []).concat(key));
         break;
       }
@@ -65,18 +65,10 @@ export function part1(input) {
   const checks = ['north', 'south', 'west', 'east'];
   for (let i = 0; i < 10; i++) round(elves, checks);
 
-  let minX = Infinity;
-  let minY = Infinity;
-  let maxX = -Infinity;
-  let maxY = -Infinity;
-  for (const key of elves.keys()) {
-    const [x, y] = key.split(',').map(n => +n);
-    minX = Math.min(minX, x);
-    minY = Math.min(minY, y);
-    maxX = Math.max(maxX, x);
-    maxY = Math.max(maxY, y);
-  }
-  return (maxX - minX + 1) * (maxY - minY + 1) - elves.size;
+  const keys = [...elves.keys()].map(key => key.split(',').map(n => +n));
+  const xs = keys.map(key => key[0]).sort((a, b) => b - a);
+  const ys = keys.map(key => key[1]).sort((a, b) => b - a);
+  return (xs[0] - xs.pop() + 1) * (ys[0] - ys.pop() + 1) - elves.size;
 }
 
 export function part2(input) {
