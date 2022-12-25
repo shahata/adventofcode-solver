29bb5e938a273416a2874171b47a83e63c84a9cb
Shahar Talmi <shahar@wix.com>
2022-12-25 07:44:17 +0200

    finished 2022!!!

---
+++ src/2022/day25.spec.js
@@ -0,0 +1,40 @@
+import { part1, part2 } from './day25.js';
+import readInput from '../utils/read-input.js';
+
+const input = readInput(import.meta.url);
+
+describe('day25 2022', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(
+          [
+            '1=-0-2',
+            '12111',
+            '2=0=',
+            '21',
+            '2=01',
+            '111',
+            '20012',
+            '112',
+            '1=-1=',
+            '1-12',
+            '12',
+            '1=',
+            '122',
+          ].join('\n'),
+        ),
+      ).toEqual('2=-1=0');
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual('20=02=120-=-2110-0=1');
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 input', () => {
+      expect(part2()).toEqual(undefined);
+    });
+  });
+});
