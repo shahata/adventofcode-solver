7363384ae84f5a4446e640869d203cf443d918ba
Shahar Talmi <shahar@wix.com>
2023-01-08 23:15:15 +0200

    damn it

---
+++ src/utils/urls.js
@@ -0,0 +1,16 @@
+import pkg from '../../package-lock.json' assert { type: 'json' };
+
+const v = p => pkg.dependencies[p].version;
+const skypack = (name, suffix = '') => ({
+  [name]: `https://cdn.skypack.dev/${name}@${v(name)}${suffix}?min`,
+});
+const unpkg = (name, suffix = '') => ({
+  [name]: `https://unpkg.com/${name}@${v(name)}${suffix}`,
+});
+
+export const imports = {
+  ...skypack('combinatorial-generators'),
+  ...skypack('chart.js'),
+  ...unpkg('node-forge', '/dist/forge.min.js'),
+  ...unpkg('es-module-shims'),
+};
