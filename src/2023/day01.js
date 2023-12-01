c0a24906c4c580a36f291a4a3710246f296e2946
Shahar Talmi <shahar@wix.com>
2023-12-01 18:24:50 +0200

    day 1

--- src/2023/day01.js
+++ src/2023/day01.js
@@ -1,10 +1,8 @@
-const rs = s => s.split('').reverse().join('');
-
 export function part1(input) {
   const lines = input.split('\n');
   const numbers = lines.map(line => {
-    const first = +line.match(/[0-9]/).at(0);
-    const last = +rs(line).match(/[0-9]/).at(0);
+    const first = +line.match(/[0-9]/g).at(0);
+    const last = +line.match(/[0-9]/g).at(-1);
     return first * 10 + last;
   });
   return numbers.reduce((a, b) => a + b, 0);
@@ -23,15 +21,15 @@ export function part2(input) {
     'eight',
     'nine',
   ];
-  const regex = new RegExp('[0-9]|' + letters.join('|'));
-  const regex2 = new RegExp('[0-9]|' + rs(letters.join('|')));
+  const reverse = s => s.split('').reverse().join('');
+  const regex = new RegExp(`[0-9]|${letters.join('|')}`);
+  const regex2 = new RegExp(`[0-9]|${reverse(letters.join('|'))}`);
   const lines = input.split('\n');
   const numbers = lines.map(line => {
     const first = line.match(regex).at(0);
-    const last = rs(line).match(regex2).at(0);
-
+    const last = reverse(reverse(line).match(regex2).at(0));
     const a = Number.isNaN(+first) ? letters.indexOf(first) : +first;
-    const b = Number.isNaN(+last) ? letters.indexOf(rs(last)) : +last;
+    const b = Number.isNaN(+last) ? letters.indexOf(last) : +last;
     return a * 10 + b;
   });
   return numbers.reduce((a, b) => a + b, 0);
