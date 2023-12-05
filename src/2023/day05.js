418c5074ac96dc451dc96a46e5ea54c9fbcc7b1f
Shahar Talmi <shahar@wix.com>
2023-12-05 13:19:38 +0200

    refactor

--- src/2023/day05.js
+++ src/2023/day05.js
@@ -1,4 +1,4 @@
-export function part1(input) {
+function parse(input) {
   let [seeds, ...maps] = input.split('\n\n');
   seeds = seeds.split(': ')[1].split(' ').map(Number);
   maps = maps.map(m =>
@@ -7,53 +7,41 @@ export function part1(input) {
       .slice(1)
       .map(r => r.split(' ').map(Number)),
   );
-  for (let map of maps) {
-    seeds = seeds.map(seed => {
-      const next = map.find(
-        ([, source, length]) => seed >= source && seed < source + length,
-      );
-      if (next) {
-        seed = next[0] + (seed - next[1]);
-      }
-      return seed;
-    });
+  return { seeds, maps };
+}
+
+function translate(map, seed) {
+  const next = map.find(x => seed >= x[1] && seed < x[1] + x[2]);
+  return { seed: next ? next[0] + (seed - next[1]) : seed, next };
+}
+
+export function part1(input) {
+  let { seeds, maps } = parse(input);
+  for (const map of maps) {
+    seeds = seeds.map(seed => translate(map, seed).seed);
   }
   return Math.min(...seeds);
 }
 
 export function part2(input) {
-  let [seeds, ...maps] = input.split('\n\n');
-  seeds = seeds.split(': ')[1].split(' ').map(Number);
-  maps = maps.map(m =>
-    m
-      .split('\n')
-      .slice(1)
-      .map(r => r.split(' ').map(Number)),
-  );
+  let { seeds, maps } = parse(input);
   let seeds2 = [];
   for (let i = 0; i < seeds.length; i += 2) {
-    seeds2.push([seeds[i], seeds[i + 1]]);
+    seeds2.push({ start: seeds[i], length: seeds[i + 1] });
   }
-  for (let map of maps) {
+  for (const map of maps) {
     seeds2 = seeds2.flatMap(range => {
       const result = [];
-      while (range[1] > 0) {
-        const next = map.find(
-          ([, source, length]) =>
-            range[0] >= source && range[0] < source + length,
-        );
-        if (next) {
-          const length = Math.min(range[1], next[2] - (range[0] - next[1]));
-          result.push([next[0] + (range[0] - next[1]), length]);
-          range[0] += length;
-          range[1] -= length;
-        } else {
-          result.push([...range]);
-          range[1] = 0;
-        }
+      while (range.length > 0) {
+        const { seed, next } = translate(map, range.start);
+        const remaining = next ? next[2] - (range.start - next[1]) : Infinity;
+        const length = Math.min(range.length, remaining);
+        result.push({ start: seed, length });
+        range.start += length;
+        range.length -= length;
       }
       return result;
     });
   }
-  return Math.min(...seeds2.map(x => x[0]));
+  return Math.min(...seeds2.map(x => x.start));
 }
