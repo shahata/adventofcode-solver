0f8b81f9107dc8c43e750edc798efc71326d9b8a
Shahar Talmi <shahar@wix.com>
2023-12-09 16:49:15 +0200

    screenshots

--- tests/screenshot.spec.ts
+++ tests/screenshot.spec.ts
@@ -1,10 +1,20 @@
 import { test } from '@playwright/test';
+import url from 'node:url';
 
-test('create screenshot', async ({ page }) => {
-  await page.goto('/');
+test('create events screenshot', async ({ page }) => {
+  await page.goto(url.resolve(import.meta.url, '../src/2023/events.html'));
   await page.waitForTimeout(1000);
   await page.screenshot({
-    path: 'test-results/screenshot.png',
+    path: 'src/static/events-screenshot.png',
+    clip: { x: 0, y: 0, width: 1030, height: 420 },
+  });
+});
+
+test('create solver screenshot', async ({ page }) => {
+  await page.goto(url.resolve(import.meta.url, '../src/2023/solver.html'));
+  await page.waitForTimeout(1000);
+  await page.screenshot({
+    path: 'src/static/solver-screenshot.png',
     clip: { x: 0, y: 0, width: 1030, height: 420 },
   });
 });
