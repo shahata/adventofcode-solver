00041edbfd3d5f5d27e585a2076cab674e5948fc
Shahar Talmi <shahar@wix.com>
2023-12-09 16:15:52 +0200

    testing playwright

--- tests/screenshot.spec.ts
+++ tests/screenshot.spec.ts
@@ -1,9 +1,7 @@
 import { test } from '@playwright/test';
 
 test('create screenshot', async ({ page }) => {
-  await page.goto(
-    'https://shahata.github.io/adventofcode-solver/src/2023/events.html',
-  );
+  await page.goto('/');
   await page.waitForTimeout(1000);
   await page.screenshot({
     path: 'test-results/screenshot.png',
