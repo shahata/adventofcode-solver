4ee08bd0a74383d6b45a0b53f4ec07fe52865569
Shahar Talmi <shahar@wix.com>
2023-12-09 13:20:21 +0200

    refactor

--- src/2023/day09.js
+++ src/2023/day09.js
@@ -1,14 +1,18 @@
-function calcHistory(sequence) {
-  const history = [sequence];
-  let differences = sequence;
-  while (differences.some(x => x !== 0)) {
-    differences = differences.reduce(
-      (acc, curr, i) => (i === 0 ? [] : [...acc, curr - differences[i - 1]]),
-      [],
-    );
-    history.push(differences);
-  }
-  return history;
+function solve(input, fn) {
+  const sequences = input.split('\n').map(line => line.split(' ').map(Number));
+  const histories = sequences.map(sequence => {
+    const history = [sequence];
+    let differences = sequence;
+    while (differences.some(x => x !== 0)) {
+      differences = differences.reduce(
+        (acc, curr, i, arr) => (i === 0 ? acc : acc.concat(curr - arr[i - 1])),
+        [],
+      );
+      history.push(differences);
+    }
+    return history;
+  });
+  return histories.map(fn).reduce((a, b) => a + b);
 }
 
 function getNextNumber(history) {
@@ -20,21 +24,15 @@ function getNextNumber(history) {
 
 function getPrevNumber(history) {
   for (let i = history.length - 2; i >= 0; i--) {
-    history[i].unshift(history[i][0] - history[i + 1][0]);
+    history[i].unshift(history[i].at(0) - history[i + 1].at(0));
   }
-  return history[0][0];
+  return history[0].at(0);
 }
 
 export function part1(input) {
-  const sequences = input.split('\n').map(line => line.split(' ').map(Number));
-  const histories = sequences.map(calcHistory);
-  const next = histories.map(getNextNumber);
-  return next.reduce((a, b) => a + b);
+  return solve(input, getNextNumber);
 }
 
 export function part2(input) {
-  const sequences = input.split('\n').map(line => line.split(' ').map(Number));
-  const histories = sequences.map(calcHistory);
-  const next = histories.map(getPrevNumber);
-  return next.reduce((a, b) => a + b);
+  return solve(input, getPrevNumber);
 }
