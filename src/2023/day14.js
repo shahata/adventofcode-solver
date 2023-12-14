d2c78d34b7bb82b3c42aac45906ac2fdb2e3d819
Shahar Talmi <shahar@wix.com>
2023-12-14 08:39:46 +0200

    refactor

--- src/2023/day14.js
+++ src/2023/day14.js
@@ -1,82 +1,40 @@
-function north(lines) {
+function vertical(lines, add = 1) {
   for (let x = 0; x < lines[0].length; x++) {
-    let target = 0;
-    for (let y = 0; y < lines.length; y++) {
+    let target = add > 0 ? 0 : lines.length - 1;
+    for (let y = target; y >= 0 && y < lines.length; y += add) {
       if (lines[y][x] === 'O') {
         lines[y][x] = '.';
         lines[target][x] = 'O';
-        target++;
+        target += add;
       }
-      if (lines[y][x] === '#') {
-        target = y + 1;
-      }
+      if (lines[y][x] === '#') target = y + add;
     }
   }
 }
 
-function west(lines) {
+function horizontal(lines, add = 1) {
   for (let y = 0; y < lines.length; y++) {
-    let target = 0;
-    for (let x = 0; x < lines[0].length; x++) {
+    let target = add > 0 ? 0 : lines[0].length - 1;
+    for (let x = target; x >= 0 && x < lines[0].length; x += add) {
       if (lines[y][x] === 'O') {
         lines[y][x] = '.';
         lines[y][target] = 'O';
-        target++;
+        target += add;
       }
-      if (lines[y][x] === '#') {
-        target = x + 1;
-      }
-    }
-  }
-}
-
-function south(lines) {
-  for (let x = 0; x < lines[0].length; x++) {
-    let target = lines.length - 1;
-    for (let y = lines.length - 1; y >= 0; y--) {
-      if (lines[y][x] === 'O') {
-        lines[y][x] = '.';
-        lines[target][x] = 'O';
-        target--;
-      }
-      if (lines[y][x] === '#') {
-        target = y - 1;
-      }
-    }
-  }
-}
-
-function east(lines) {
-  for (let y = 0; y < lines.length; y++) {
-    let target = lines[0].length - 1;
-    for (let x = lines[0].length - 1; x >= 0; x--) {
-      if (lines[y][x] === 'O') {
-        lines[y][x] = '.';
-        lines[y][target] = 'O';
-        target--;
-      }
-      if (lines[y][x] === '#') {
-        target = x - 1;
-      }
+      if (lines[y][x] === '#') target = x + add;
     }
   }
 }
 
 function weight(lines) {
-  let result = 0;
-  for (let i = 0; i < lines[0].length; i++) {
-    for (let j = 0; j < lines.length; j++) {
-      if (lines[j][i] === 'O') {
-        result += lines.length - j;
-      }
-    }
-  }
-  return result;
+  return lines.reduce((acc, line, y) => {
+    return acc + line.filter(x => x === 'O').length * (lines.length - y);
+  }, 0);
 }
 
 export function part1(input) {
   const lines = input.split('\n').map(line => line.split(''));
-  north(lines);
+  vertical(lines, 1);
   return weight(lines);
 }
 
@@ -84,19 +42,15 @@ export function part2(input) {
   const lines = input.split('\n').map(line => line.split(''));
   const memory = new Map();
   const count = 1000000000;
-  for (let i = 0; i < count; i++) {
-    north(lines);
-    west(lines);
-    south(lines);
-    east(lines);
+  for (let i = 1; i <= count; i++) {
+    const key = lines.map(line => line.join('')).join('\n');
+    if (memory.has(key)) i = count - ((count - i) % (i - memory.get(key)));
+    else memory.set(key, i);
 
-    const key = JSON.stringify(lines);
-    const index = memory.get(key);
-    if (index !== undefined) {
-      i += Math.floor((count - i - 1) / (i - index)) * (i - index);
-    } else {
-      memory.set(key, i);
-    }
+    vertical(lines, 1);
+    horizontal(lines, 1);
+    vertical(lines, -1);
+    horizontal(lines, -1);
   }
   return weight(lines);
 }
