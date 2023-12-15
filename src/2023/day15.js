5f101446cb49f079cb57e8ad27583c9ec1d82d21
Shahar Talmi <shahar@wix.com>
2023-12-15 07:42:29 +0200

    solved day 15

---
+++ src/2023/day15.js
@@ -0,0 +1,36 @@
+function hash(word) {
+  let hash = 0;
+  for (let i = 0; i < word.length; i++) {
+    hash += word.charCodeAt(i);
+    hash *= 17;
+    hash %= 256;
+  }
+  return hash;
+}
+export function part1(input) {
+  const words = input.split(',');
+  return words.map(hash).reduce((sum, hash) => sum + hash, 0);
+}
+
+export function part2(input) {
+  const operations = input.split(',').map(x => {
+    const [label, focal] = x.split(/[-=]/);
+    return focal === ''
+      ? { op: '-', label }
+      : { op: '=', label, focal: +focal };
+  });
+  const hashmap = new Array(256).fill().map(() => []);
+  for (const operation of operations) {
+    const box = hash(operation.label);
+    const i = hashmap[box].findIndex(x => x.label === operation.label);
+    if (operation.op === '-') {
+      if (i !== -1) hashmap[box].splice(i, 1);
+    } else {
+      if (i === -1) hashmap[box].push(operation);
+      else hashmap[box][i] = operation;
+    }
+  }
+  return hashmap
+    .flatMap((box, i) => box.map(({ focal }, j) => (i + 1) * (j + 1) * focal))
+    .reduce((sum, power) => sum + power, 0);
+}
---
+++ src/2023/day15.spec.js
@@ -0,0 +1,31 @@
+import { part1, part2 } from './day15.js';
+import readInput from '../utils/read-input.js';
+
+const input = readInput(import.meta.url);
+
+describe('day15 2023', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(part1('HASH')).toEqual(52);
+      expect(
+        part1('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'),
+      ).toEqual(1320);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(519041);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 examples', () => {
+      expect(
+        part2('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'),
+      ).toEqual(145);
+    });
+
+    it('should work for part 2 input', () => {
+      expect(part2(input)).toEqual(260530);
+    });
+  });
+});
