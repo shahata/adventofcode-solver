07f5c32356756e5c8a71472980dc4325a60d0803
Shahar Talmi <shahar@wix.com>
2023-12-17 23:20:57 +0200

    optimize

--- src/2023/day17.js
+++ src/2023/day17.js
@@ -1,35 +1,49 @@
+function jump(map, { x, y, total, d, s }, minSteps) {
+  for (; s < minSteps; s++) {
+    total += map[y]?.[x] || 0;
+    if (d === 'right') x++;
+    if (d === 'left') x--;
+    if (d === 'up') y--;
+    if (d === 'down') y++;
+  }
+  return { x, y, total, d, s };
+}
+
 function getNext(current, map, minSteps, maxSteps) {
   const next = [];
   const { x, y, total, d, s } = current;
   if (s < maxSteps) {
-    if (d === 'right') next.push({ x: x + 1, y, d: 'right', s: s + 1 });
-    if (d === 'left') next.push({ x: x - 1, y, d: 'left', s: s + 1 });
-    if (d === 'up') next.push({ x, y: y - 1, d: 'up', s: s + 1 });
-    if (d === 'down') next.push({ x, y: y + 1, d: 'down', s: s + 1 });
+    if (d === 'right') next.push({ x: x + 1, y, d: 'right', s: s + 1, total });
+    if (d === 'left') next.push({ x: x - 1, y, d: 'left', s: s + 1, total });
+    if (d === 'up') next.push({ x, y: y - 1, d: 'up', s: s + 1, total });
+    if (d === 'down') next.push({ x, y: y + 1, d: 'down', s: s + 1, total });
   }
   if (s >= minSteps && (d === 'right' || d === 'left')) {
-    if (s >= minSteps) next.push({ x, y: y - 1, d: 'up', s: 1 });
-    if (s >= minSteps) next.push({ x, y: y + 1, d: 'down', s: 1 });
+    next.push({ x, y: y - 1, d: 'up', s: 1, total });
+    next.push({ x, y: y + 1, d: 'down', s: 1, total });
   }
   if (s >= minSteps && (d === 'up' || d === 'down')) {
-    if (s >= minSteps) next.push({ x: x - 1, y, d: 'left', s: 1 });
-    if (s >= minSteps) next.push({ x: x + 1, y, d: 'right', s: 1 });
+    next.push({ x: x - 1, y, d: 'left', s: 1, total });
+    next.push({ x: x + 1, y, d: 'right', s: 1, total });
   }
   return next
+    .map(n => (n.s < minSteps ? jump(map, n, minSteps) : n))
     .filter(({ x, y }) => map[y]?.[x] !== undefined)
-    .map(n => ({ ...n, total: total + map[n.y][n.x] }));
+    .map(n => ({ ...n, total: n.total + map[n.y][n.x] }));
 }
 
 export function part1(input, minSteps = 0, maxSteps = 3) {
   const map = input.split('\n').map(line => line.split('').map(Number));
-  const queue = [{ x: 0, y: 0, total: 0, d: 'right', s: 0 }];
-  const visited = new Map();
   const key = ({ x, y, d, s }) => `${x},${y},${d},${s}`;
+  const queue = [
+    { x: 0, y: 0, total: 0, d: 'right', s: 0 },
+    { x: 0, y: 0, total: 0, d: 'down', s: 0 },
+  ];
+  const visited = new Map(queue.map(n => [key(n), n]));
   let min = Infinity;
   while (queue.length > 0) {
     const current = queue.shift();
     if (current.x === map[0].length - 1 && current.y === map.length - 1) {
-      if (current.s < minSteps) continue;
       min = Math.min(min, current.total);
       continue;
     }
