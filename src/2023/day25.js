03fc5d5a3f01fbd79a8d9b0fd710eb2d3ebf2f10
Shahar Talmi <shahar@wix.com>
2023-12-25 08:57:19 +0200

    solved day25

---
+++ src/2023/day25.js
@@ -0,0 +1,37 @@
+import { mincut } from '@graph-algorithm/minimum-cut';
+
+function graphSize(graph, component, ignored, visited = new Set()) {
+  let result = 1;
+  visited.add(component);
+  graph[component].forEach(c => {
+    if (!visited.has(c) && !ignored[component]?.includes(c)) {
+      result += graphSize(graph, c, ignored, visited);
+    }
+  });
+  return result;
+}
+
+function toGraph(edges) {
+  const graph = {};
+  edges.forEach(([a, b]) => {
+    graph[a] = (graph[a] || []).concat(b);
+    graph[b] = (graph[b] || []).concat(a);
+  });
+  return graph;
+}
+
+export function part1(input) {
+  const edges = [];
+  input.split('\n').forEach(line => {
+    let [component, connections] = line.split(': ');
+    connections = connections.split(' ');
+    connections.forEach(connection => edges.push([component, connection]));
+  });
+  const graph = toGraph(edges);
+  const ignored = toGraph([...mincut(edges)]);
+  const size = graphSize(graph, Object.keys(graph)[0], {});
+  const x = graphSize(graph, Object.keys(graph)[0], ignored);
+  return x * (size - x);
+}
+
+export const part2 = () => undefined;
---
+++ src/2023/day25.spec.js
@@ -0,0 +1,40 @@
+import { part1, part2 } from './day25.js';
+import readInput from '../utils/read-input.js';
+
+const input = readInput(import.meta.url);
+
+describe('day25 2023', () => {
+  describe('part1', () => {
+    it('should work for part 1 examples', () => {
+      expect(
+        part1(
+          [
+            'jqt: rhn xhk nvd',
+            'rsh: frs pzl lsr',
+            'xhk: hfx',
+            'cmg: qnr nvd lhk bvb',
+            'rhn: xhk bvb hfx',
+            'bvb: xhk hfx',
+            'pzl: lsr hfx nvd',
+            'qnr: nvd',
+            'ntq: jqt hfx bvb xhk',
+            'nvd: lhk',
+            'lsr: lhk',
+            'rzs: qnr cmg lsr rsh',
+            'frs: qnr lhk lsr',
+          ].join('\n'),
+        ),
+      ).toEqual(54);
+    });
+
+    it('should work for part 1 input', () => {
+      expect(part1(input)).toEqual(562978);
+    });
+  });
+
+  describe('part2', () => {
+    it('should work for part 2 input', () => {
+      expect(part2()).toEqual(undefined);
+    });
+  });
+});
