8d51ac8e3b045adc2edca5529dada45b0d6a05b9
Shahar Talmi <shahar@wix.com>
2024-01-10 07:07:12 +0200

    learned some math

--- src/2023/day24.js
+++ src/2023/day24.js
@@ -32,26 +32,16 @@ export function part1(input, min = 2e14, max = 4e14) {
   return result;
 }
 
-// Cramer's Rule
-function det3x3(m) {
-  return (
-    m[0] * m[4] * m[8] +
-    m[1] * m[5] * m[6] +
-    m[2] * m[3] * m[7] -
-    m[0] * m[5] * m[7] -
-    m[1] * m[3] * m[8] -
-    m[2] * m[4] * m[6]
-  );
+function det(m) {
+  if (m.length === 0) return 1n;
+  let [l, ...r] = m;
+  r = l.map((n, i) => n * det(r.map(row => row.toSpliced(i, 1))));
+  return r.reduce((a, b, i) => (i % 2 === 0 ? a + b : a - b), 0n);
 }
 
-// Cramer's Rule
-function det4x4(m) {
-  return (
-    m[0] * det3x3([m[5], m[6], m[7], m[9], m[10], m[11], m[13], m[14], m[15]]) -
-    m[1] * det3x3([m[4], m[6], m[7], m[8], m[10], m[11], m[12], m[14], m[15]]) +
-    m[2] * det3x3([m[4], m[5], m[7], m[8], m[9], m[11], m[12], m[13], m[15]]) -
-    m[3] * det3x3([m[4], m[5], m[6], m[8], m[9], m[10], m[12], m[13], m[14]])
-  );
+function cramersRule(A, B) {
+  const detA = det(A);
+  return B.map((_, i) => det(A.map((r, j) => r.toSpliced(i, 1, B[j]))) / detA);
 }
 
 // px0 + vx0 t0 = pxr + vxr t0
@@ -61,16 +51,16 @@ function det4x4(m) {
 // t0 = (pyr - py0) / (vy0 - vyr)
 
 // (pxr - px0) / (vx0 - vxr) = (pyr - py0) / (vy0 - vyr)
+// (pxr - px0) (vy0 - vyr) = (pyr - py0) (vx0 - vxr)
+// pxr vy0 - pxr vyr - px0 vy0 + px0 vyr = pyr vx0 - pyr vxr - py0 vx0 + py0 vxr
 
-// pxr vy0 - pxr vyr + px0 vyr - px0 vy0 + py0 vx0 - py0 vxr + pyr vxr - pyr vx0 = 0
-// pxr vy1 - pxr vyr + px1 vyr - px1 vy1 + py1 vx1 - py1 vxr + pyr vxr - pyr vx1 = 0
+// pxr vy0 - pyr vx0 - vxr py0 + vyr px0 + pyr vxr - pxr vyr = px0 vy0 - py0 vx0
+// pxr vy1 - pyr vx1 - vxr py1 + vyr px1 + pyr vxr - pxr vyr = px1 vy1 - py1 vx1
 
-// pxr (vy0 - vy1) + pyr (vx1 - vx0) + vxr (py1 - py0) + vyr (px0 - px1) - px0 vy0 + py0 vx0 + px1 vy1 - py1 vx1 = 0
-
-// pxr (vy0 - vy1) + pyr (vx1 - vx0) + vxr (py1 - py0) + vyr (px0 - px1) - px0 vy0 + py0 vx0 + px1 vy1 - py1 vx1 = 0
-// pxr (vy1 - vy2) + pyr (vx2 - vx1) + vxr (py2 - py1) + vyr (px1 - px2) - px1 vy1 + py1 vx1 + px2 vy2 - py2 vx2 = 0
-// pxr (vy2 - vy3) + pyr (vx3 - vx2) + vxr (py3 - py2) + vyr (px2 - px3) - px2 vy2 + py2 vx2 + px3 vy3 - py3 vx3 = 0
-// pxr (vy3 - vy4) + pyr (vx4 - vx3) + vxr (py4 - py3) + vyr (px3 - px4) - px3 vy3 + py3 vx3 + px4 vy4 - py4 vx4 = 0
+// pxr (vy0 - vy1) + pyr (vx1 - vx0) + vxr (py1 - py0) + vyr (px0 - px1) = px0 vy0 - py0 vx0 - px1 vy1 + py1 vx1
+// pxr (vy1 - vy2) + pyr (vx2 - vx1) + vxr (py2 - py1) + vyr (px1 - px2) = px1 vy1 - py1 vx1 - px2 vy2 + py2 vx2
+// pxr (vy2 - vy3) + pyr (vx3 - vx2) + vxr (py3 - py2) + vyr (px2 - px3) = px2 vy2 - py2 vx2 - px3 vy3 + py3 vx3
+// pxr (vy3 - vy4) + pyr (vx4 - vx3) + vxr (py4 - py3) + vyr (px3 - px4) = px3 vy3 - py3 vx3 - px4 vy4 + py4 vx4
 
 function solve(hails) {
   const [px0, py0, pz0, vx0, vy0, vz0] = hails[0];
@@ -84,43 +74,16 @@ function solve(hails) {
     [vy1 - vy2, vx2 - vx1, py2 - py1, px1 - px2],
     [vy2 - vy3, vx3 - vx2, py3 - py2, px2 - px3],
     [vy3 - vy4, vx4 - vx3, py4 - py3, px3 - px4],
-  ].flat();
-
-  const b = [
-    px0 * vy0 - py0 * vx0 + py1 * vx1 - px1 * vy1,
-    px1 * vy1 - py1 * vx1 + py2 * vx2 - px2 * vy2,
-    px2 * vy2 - py2 * vx2 + py3 * vx3 - px3 * vy3,
-    px3 * vy3 - py3 * vx3 + py4 * vx4 - px4 * vy4,
   ];
 
-  const den = det4x4(A);
-  const pxr =
-    det4x4(
-      [
-        [b[0], A[1], A[2], A[3]],
-        [b[1], A[5], A[6], A[7]],
-        [b[2], A[9], A[10], A[11]],
-        [b[3], A[13], A[14], A[15]],
-      ].flat(),
-    ) / den;
-  const pyr =
-    det4x4(
-      [
-        [A[0], b[0], A[2], A[3]],
-        [A[4], b[1], A[6], A[7]],
-        [A[8], b[2], A[10], A[11]],
-        [A[12], b[3], A[14], A[15]],
-      ].flat(),
-    ) / den;
-  const vxr =
-    det4x4(
-      [
-        [A[0], A[1], b[0], A[3]],
-        [A[4], A[5], b[1], A[7]],
-        [A[8], A[9], b[2], A[11]],
-        [A[12], A[13], b[3], A[15]],
-      ].flat(),
-    ) / den;
+  const B = [
+    px0 * vy0 - py0 * vx0 - px1 * vy1 + py1 * vx1,
+    px1 * vy1 - py1 * vx1 - px2 * vy2 + py2 * vx2,
+    px2 * vy2 - py2 * vx2 - px3 * vy3 + py3 * vx3,
+    px3 * vy3 - py3 * vx3 - px4 * vy4 + py4 * vx4,
+  ];
+
+  const [pxr, pyr, vxr] = cramersRule(A, B);
 
   const t0 = (pxr - px0) / (vx0 - vxr);
   const t1 = (pxr - px1) / (vx1 - vxr);
