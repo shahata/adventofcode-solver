a0685be29b908504bb64235486e4cad2e775fe95
Shahar Talmi <shahar@wix.com>
2024-01-10 08:49:53 +0200

    simplify

--- src/2023/day24.js
+++ src/2023/day24.js
@@ -32,6 +32,34 @@ export function part1(input, min = 2e14, max = 4e14) {
   return result;
 }
 
+// px0 + vx0 t0 = pxr + vxr t0
+// py0 + vy0 t0 = pyr + vyr t0
+
+// t0 = (pxr - px0) / (vx0 - vxr)
+// t0 = (pyr - py0) / (vy0 - vyr)
+
+// (pxr - px0) / (vx0 - vxr) = (pyr - py0) / (vy0 - vyr)
+// (pxr - px0) (vy0 - vyr) = (pyr - py0) (vx0 - vxr)
+// pxr vy0 - pxr vyr - px0 vy0 + px0 vyr = pyr vx0 - pyr vxr - py0 vx0 + py0 vxr
+
+// pxr vy0 - pyr vx0 - vxr py0 + vyr px0 + pyr vxr - pxr vyr = px0 vy0 - py0 vx0
+// -
+// pxr vyN - pyr vxN - vxr pyN + vyr pxN + pyr vxr - pxr vyr = pxN vyN - pyN vxN
+// =
+// pxr (vy0 - vyN) + pyr (vxN - vx0) + vxr (pyN - py0) + vyr (px0 - pxN) = px0 vy0 - py0 vx0 - pxN vyN + pyN vxN
+
+// pxr (vy0 - vyN) + pyr (vxN - vx0) + pzr (0) + vxr (pyN - py0) + vyr (px0 - pxN) + vzr (0) = px0 vy0 - py0 vx0 - pxN vyN + pyN vxN
+// pxr (vz0 - vzN) + pyr (0) + pzr (vxN - vx0) + vxr (pzN - pz0) + vyr (0) + vzr (px0 - pxN) = px0 vz0 - pz0 vx0 - pxN vzN + pzN vxN
+
+function add(A, B, hails, i) {
+  const [px0, py0, pz0, vx0, vy0, vz0] = hails[0];
+  const [pxN, pyN, pzN, vxN, vyN, vzN] = hails[i];
+  A.push([vy0 - vyN, vxN - vx0, 0n, pyN - py0, px0 - pxN, 0n]);
+  B.push(px0 * vy0 - py0 * vx0 - pxN * vyN + pyN * vxN);
+  A.push([vz0 - vzN, 0n, vxN - vx0, pzN - pz0, 0n, px0 - pxN]);
+  B.push(px0 * vz0 - pz0 * vx0 - pxN * vzN + pzN * vxN);
+}
+
 function det(m) {
   if (m.length === 0) return 1n;
   let [l, ...r] = m;
@@ -44,52 +72,11 @@ function cramersRule(A, B) {
   return B.map((_, i) => det(A.map((r, j) => r.toSpliced(i, 1, B[j]))) / detA);
 }
 
-// px0 + vx0 t0 = pxr + vxr t0
-// py0 + vy0 t0 = pyr + vyr t0
-
-// t0 = (pxr - px0) / (vx0 - vxr)
-// t0 = (pyr - py0) / (vy0 - vyr)
-
-// (pxr - px0) / (vx0 - vxr) = (pyr - py0) / (vy0 - vyr)
-// (pxr - px0) (vy0 - vyr) = (pyr - py0) (vx0 - vxr)
-// pxr vy0 - pxr vyr - px0 vy0 + px0 vyr = pyr vx0 - pyr vxr - py0 vx0 + py0 vxr
-
-// pxr vy0 - pyr vx0 - vxr py0 + vyr px0 + pyr vxr - pxr vyr = px0 vy0 - py0 vx0
-// pxr vy1 - pyr vx1 - vxr py1 + vyr px1 + pyr vxr - pxr vyr = px1 vy1 - py1 vx1
-
-// pxr (vy0 - vy1) + pyr (vx1 - vx0) + vxr (py1 - py0) + vyr (px0 - px1) = px0 vy0 - py0 vx0 - px1 vy1 + py1 vx1
-// pxr (vy0 - vy2) + pyr (vx2 - vx0) + vxr (py2 - py0) + vyr (px0 - px2) = px0 vy0 - py0 vx0 - px2 vy2 + py2 vx2
-// pxr (vy0 - vy3) + pyr (vx3 - vx0) + vxr (py3 - py0) + vyr (px0 - px3) = px0 vy0 - py0 vx0 - px3 vy3 + py3 vx3
-// pxr (vy0 - vy4) + pyr (vx4 - vx0) + vxr (py4 - py0) + vyr (px0 - px4) = px0 vy0 - py0 vx0 - px4 vy4 + py4 vx4
-// pxr (vz0 - vz1) + pzr (vx1 - vx0) + vxr (pz1 - pz0) + vzr (px0 - px1) = px0 vz0 - pz0 vx0 - px1 vz1 + pz1 vx1
-// pxr (vz0 - vz2) + pzr (vx2 - vx0) + vxr (pz2 - pz0) + vzr (px0 - px2) = px0 vz0 - pz0 vx0 - px2 vz2 + pz2 vx2
-
 function solve(hails) {
-  const [px0, py0, pz0, vx0, vy0, vz0] = hails[0];
-  const [px1, py1, pz1, vx1, vy1, vz1] = hails[1];
-  const [px2, py2, pz2, vx2, vy2, vz2] = hails[2];
-  const [px3, py3, , vx3, vy3] = hails[3];
-  const [px4, py4, , vx4, vy4] = hails[4];
-
-  const A = [
-    [vy0 - vy1, vx1 - vx0, 0n, py1 - py0, px0 - px1, 0n],
-    [vy0 - vy2, vx2 - vx0, 0n, py2 - py0, px0 - px2, 0n],
-    [vy0 - vy3, vx3 - vx0, 0n, py3 - py0, px0 - px3, 0n],
-    [vy0 - vy4, vx4 - vx0, 0n, py4 - py0, px0 - px4, 0n],
-    [vz0 - vz1, 0n, vx1 - vx0, pz1 - pz0, 0n, px0 - px1],
-    [vz0 - vz2, 0n, vx2 - vx0, pz2 - pz0, 0n, px0 - px2],
-  ];
-
-  const B = [
-    px0 * vy0 - py0 * vx0 - px1 * vy1 + py1 * vx1,
-    px0 * vy0 - py0 * vx0 - px2 * vy2 + py2 * vx2,
-    px0 * vy0 - py0 * vx0 - px3 * vy3 + py3 * vx3,
-    px0 * vy0 - py0 * vx0 - px4 * vy4 + py4 * vx4,
-    px0 * vz0 - pz0 * vx0 - px1 * vz1 + pz1 * vx1,
-    px0 * vz0 - pz0 * vx0 - px2 * vz2 + pz2 * vx2,
-  ];
-
-  const [pxr, pyr, pzr] = cramersRule(A, B);
+  const A = [];
+  const B = [];
+  for (let i = 1; i <= 4; i++) add(A, B, hails, i);
+  const [pxr, pyr, pzr] = cramersRule(A.slice(0, 6), B.slice(0, 6));
   return pxr + pyr + pzr;
 }
 
@@ -102,7 +89,7 @@ export function part2(input) {
     try {
       return solve(hails.slice(i));
     } catch {
-      //
+      //just a hack to avoid parallel hailstones
     }
   }
 }
