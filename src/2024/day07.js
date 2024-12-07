22025112b3e7136d09f9a4fc6a0d4b974a3348ca
Shahar Talmi <shahar@wix.com>
2024-12-07 08:03:48 +0200

    refactor

--- src/2024/day07.js
+++ src/2024/day07.js
@@ -1,16 +1,19 @@
 function getPossibleAnswers(numbers, thirdOperator) {
-  if (numbers.length === 1) return numbers;
-  return getPossibleAnswers(numbers.slice(1), thirdOperator).flatMap(answer => {
-    return [answer + numbers[0], answer * numbers[0]].concat(
-      thirdOperator ? +`${answer}${numbers[0]}` : [],
-    );
-  });
+  let result = [numbers[0]];
+  for (let i = 1; i < numbers.length; i++) {
+    result = result.flatMap(answer => {
+      const options = [answer + numbers[i], answer * numbers[i]];
+      if (thirdOperator) options.push(+`${answer}${numbers[i]}`);
+      return options;
+    });
+  }
+  return result;
 }
 
 export function part1(input, thirdOperator = false) {
   const equations = input.split("\n").map(line => {
     const [result, numbers] = line.split(": ");
-    return [+result, numbers.split(" ").map(Number).reverse()];
+    return [+result, numbers.split(" ").map(Number)];
   });
   const solvable = equations.filter(([result, numbers]) => {
     return getPossibleAnswers(numbers, thirdOperator).includes(result);
