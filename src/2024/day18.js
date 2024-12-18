b5cb8e85082a8a24b8e6ff1cabb65ea0744485ad
Shahar Talmi <shahar@wix.com>
2024-12-18 07:29:55 +0200

    small fix

--- src/2024/day18.js
+++ src/2024/day18.js
@@ -1,32 +1,29 @@
 export function part1(input, wh = 70, fallen = 1024) {
-  let bytes = input.split("\n").map(line => line.split(",").map(Number));
-  let current = { x: 0, y: 0, steps: 0 };
-  let visited = new Set([`${current.x},${current.y}`]);
-  for (let i = 0; i < fallen; i++) visited.add(`${bytes[i][0]},${bytes[i][1]}`);
-
-  let queue = [current];
+  let bytes = input.split("\n");
+  let queue = [{ x: 0, y: 0, steps: 0 }];
+  let visited = new Set(["0,0", ...bytes.slice(0, fallen)]);
   while (queue.length) {
-    current = queue.shift();
-    if (current.x === wh && current.y === wh) return current.steps;
+    let curr = queue.shift();
+    if (curr.x === wh && curr.y === wh) return curr.steps;
     const neighbors = [
-      { x: current.x + 1, y: current.y },
-      { x: current.x - 1, y: current.y },
-      { x: current.x, y: current.y + 1 },
-      { x: current.x, y: current.y - 1 },
-    ].filter(({ x, y }) => {
-      const key = `${x},${y}`;
-      return x >= 0 && x <= wh && y >= 0 && y <= wh && !visited.has(key);
-    });
+      { x: curr.x + 1, y: curr.y },
+      { x: curr.x - 1, y: curr.y },
+      { x: curr.x, y: curr.y + 1 },
+      { x: curr.x, y: curr.y - 1 },
+    ].filter(
+      ({ x, y }) =>
+        x >= 0 && x <= wh && y >= 0 && y <= wh && !visited.has(`${x},${y}`),
+    );
     neighbors.forEach(neighbor => {
       visited.add(`${neighbor.x},${neighbor.y}`);
-      queue.push({ ...neighbor, steps: current.steps + 1 });
+      queue.push({ ...neighbor, steps: curr.steps + 1 });
     });
   }
 }
 
 export function part2(input, wh = 70, fallen = 1024) {
-  let bytes = input.split("\n").map(line => line.split(",").map(Number));
+  let bytes = input.split("\n");
   for (let i = fallen; i < bytes.length; i++) {
-    if (!part1(input, wh, i)) return bytes[i - 1].join(",");
+    if (!part1(input, wh, i)) return bytes[i - 1];
   }
 }
