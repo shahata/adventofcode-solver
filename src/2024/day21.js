f168464aab132a047433f66f9c24163d159370f6
Shahar Talmi <shahar@wix.com>
2024-12-21 10:53:00 +0200

    refactor

--- src/2024/day21.js
+++ src/2024/day21.js
@@ -1,32 +1,17 @@
-const keypad = {
-  7: { x: 0, y: 0 },
-  8: { x: 1, y: 0 },
-  9: { x: 2, y: 0 },
-  4: { x: 0, y: 1 },
-  5: { x: 1, y: 1 },
-  6: { x: 2, y: 1 },
-  1: { x: 0, y: 2 },
-  2: { x: 1, y: 2 },
-  3: { x: 2, y: 2 },
-  X: { x: 0, y: 3 },
-  0: { x: 1, y: 3 },
-  A: { x: 2, y: 3 },
-};
-
-const arrowpad = {
-  "X": { x: 0, y: 0 },
-  "^": { x: 1, y: 0 },
-  "A": { x: 2, y: 0 },
-  "<": { x: 0, y: 1 },
-  "v": { x: 1, y: 1 },
-  ">": { x: 2, y: 1 },
-};
+function topad(str) {
+  const result = {};
+  const pad = str.split("\n").map(row => row.split(""));
+  pad.map((row, y) => row.map((c, x) => (result[c] = { x, y })));
+  return result;
+}
 
 let memory = {};
-function type(code, pads) {
-  if (pads.length === 0) return code.length;
-  if (memory[`${code},${pads.length}`]) return memory[`${code},${pads.length}`];
-  let pad = pads[0];
+const keypad = topad("789\n456\n123\nX0A");
+const arrowpad = topad("X^A\n<v>");
+function type(code, robots, human = false) {
+  if (robots === 0) return code.length;
+  if (memory[`${code},${robots}`]) return memory[`${code},${robots}`];
+  let pad = human ? keypad : arrowpad;
   let from = pad["A"];
   let pushes = 0;
   for (let button of code) {
@@ -37,7 +22,7 @@ function type(code, pads) {
       const { x, y, push } = queue.shift();
       if (x === pad["X"].x && y === pad["X"].y) continue;
       if (x === to.x && y === to.y) {
-        min = Math.min(min, type(`${push}A`, pads.slice(1)));
+        min = Math.min(min, type(`${push}A`, human ? robots : robots - 1));
       }
       if (to.x > x) queue.push({ x: x + 1, y, push: `${push}>` });
       if (to.x < x) queue.push({ x: x - 1, y, push: `${push}<` });
@@ -47,26 +32,21 @@ function type(code, pads) {
     pushes += min;
     from = to;
   }
-  memory[`${code},${pads.length}`] = pushes;
+  memory[`${code},${robots}`] = pushes;
   return pushes;
 }
 
-export function part1(input) {
-  const codes = input.split("\n");
-  return codes
-    .map(code => {
-      let pushes = type(code, [keypad, arrowpad, arrowpad]);
-      return parseInt(code) * pushes;
-    })
+function solve(input, robots) {
+  return input
+    .split("\n")
+    .map(code => parseInt(code) * type(code, robots, true))
     .reduce((a, b) => a + b, 0);
 }
 
+export function part1(input) {
+  return solve(input, 2);
+}
+
 export function part2(input) {
-  const codes = input.split("\n");
-  return codes
-    .map(code => {
-      let pushes = type(code, [keypad, ...new Array(25).fill(arrowpad)]);
-      return parseInt(code) * pushes;
-    })
-    .reduce((a, b) => a + b, 0);
+  return solve(input, 25);
 }
