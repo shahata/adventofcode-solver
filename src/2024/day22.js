b310894c042b18f38e39a4d841f8bdf7c1d2bfb7
Shahar Talmi <shahar@wix.com>
2024-12-22 14:38:52 +0200

    refactor

--- src/2024/day22.js
+++ src/2024/day22.js
@@ -14,6 +14,7 @@ export function part1(input) {
 export function part2(input) {
   let numbers = input.split("\n").map(BigInt);
   let cache = new Map();
+  let max = 0;
   for (let prev of numbers) {
     let visited = new Set();
     let diffs = [];
@@ -21,15 +22,17 @@ export function part2(input) {
       let next = hash(prev);
       diffs.push(Number((next % 10n) - (prev % 10n)));
       prev = next;
-      if (diffs.length >= 4) {
-        let key = diffs.slice(-4).join(",");
-        if (!visited.has(key)) {
-          let sum = (cache.get(key) || 0) + Number(next % 10n);
-          cache.set(key, sum);
-          visited.add(key);
-        }
-      }
+      if (diffs.length < 4) continue;
+
+      let key = diffs.join(",");
+      diffs.shift();
+      if (visited.has(key)) continue;
+
+      let sum = (cache.get(key) || 0) + Number(next % 10n);
+      max = Math.max(max, sum);
+      cache.set(key, sum);
+      visited.add(key);
     }
   }
-  return Math.max(...cache.values());
+  return max;
 }
