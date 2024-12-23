b27705a5ee27ee0d056a2d45bf9b9f5d33f91c46
Shahar Talmi <shahar@wix.com>
2024-12-23 09:46:06 +0200

    optimize

--- src/2024/day23.js
+++ src/2024/day23.js
@@ -1,30 +1,33 @@
-function addOneToNetworks(networks, connections = networks) {
+function addOneToNetworks(networks, map) {
   let set = new Set();
   networks.forEach(computers => {
-    let candidates = computers
-      .map(c => {
-        return connections
-          .filter(pair => pair.includes(c))
-          .map(pair => pair.find(x => x !== c));
-      })
-      .map(x => new Set(x));
+    let candidates = computers.map(c => map.get(c));
     let result = candidates.reduce((a, b) => a.intersection(b));
     result.forEach(x => set.add([...computers, x].sort().join(",")));
   });
   return set;
 }
 
+function parse(input) {
+  let networks = input.split("\n").map(line => line.split("-"));
+  let map = new Map();
+  networks.forEach(pair => {
+    map.set(pair[0], (map.get(pair[0]) || new Set()).add(pair[1]));
+    map.set(pair[1], (map.get(pair[1]) || new Set()).add(pair[0]));
+  });
+  return { networks, map };
+}
+
 export function part1(input) {
-  let connections = input.split("\n").map(line => line.split("-"));
-  let set = addOneToNetworks(connections);
+  let { networks, map } = parse(input);
+  let set = addOneToNetworks(networks, map);
   return [...set].filter(x => x.match(/(^t|,t)/)).length;
 }
 
 export function part2(input) {
-  let connections = input.split("\n").map(line => line.split("-"));
-  let networks = connections;
+  let { networks, map } = parse(input);
   while (networks.length > 1) {
-    let next = addOneToNetworks(networks, connections);
+    let next = addOneToNetworks(networks, map);
     networks = [...next].map(x => x.split(","));
   }
   return networks[0].sort().join(",");
