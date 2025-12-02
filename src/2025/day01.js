b76cfafa95826d1b1cc952f6213653a9b39e1ac2
Shahar Talmi <shahar@wix.com>
2025-12-02 06:29:02 +0200

    refactor

--- src/2025/day01.js
+++ src/2025/day01.js
@@ -1,48 +1,31 @@
+function parse(input) {
+  return input.split("\n").map(x => {
+    let [, direction, count] = x.match(/(L|R)(\d+)/);
+    return { direction: direction === "L" ? -1 : 1, count: +count };
+  });
+}
+
 export function part1(input) {
   let password = 0;
-  input
-    .split("\n")
-    .map(x => {
-      let [, direction, count] = x.match(/(L|R)(\d+)/);
-      return { direction, count: +count };
-    })
-    .reduce((prev, curr) => {
-      if (curr.direction === "L") prev -= curr.count;
-      if (curr.direction === "R") prev += curr.count;
-      while (prev < 0) prev += 100;
-      while (prev >= 100) prev -= 100;
-      if (prev === 0) password++;
-      return prev;
-    }, 50);
-
+  parse(input).reduce((prev, curr) => {
+    let next = (prev + curr.direction * curr.count) % 100;
+    if (next < 0) next += 100;
+    if (next === 0) password++;
+    return next;
+  }, 50);
   return password;
 }
 
 export function part2(input) {
   let password = 0;
-  input
-    .split("\n")
-    .map(x => {
-      let [, direction, count] = x.match(/(L|R)(\d+)/);
-      return { direction, count: +count };
-    })
-    .reduce((prev, curr) => {
-      if (curr.direction === "L") {
-        for (let i = 0; i < curr.count; i++) {
-          prev--;
-          if (prev === 0) password++;
-          if (prev === -1) prev = 99;
-        }
-      }
-      if (curr.direction === "R") {
-        for (let i = 0; i < curr.count; i++) {
-          prev++;
-          if (prev === 100) prev = 0;
-          if (prev === 0) password++;
-        }
-      }
-      return prev;
-    }, 50);
-
+  parse(input).reduce((prev, curr) => {
+    for (let i = 0; i < curr.count; i++) {
+      prev += curr.direction;
+      if (prev === -1) prev = 99;
+      if (prev === 100) prev = 0;
+      if (prev === 0) password++;
+    }
+    return prev;
+  }, 50);
   return password;
 }
