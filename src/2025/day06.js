3ba67bb4dc5f576923c7308d1d4135c6c55fe347
Shahar Talmi <shahar@wix.com>
2025-12-06 10:05:20 +0200

    refactor

--- src/2025/day06.js
+++ src/2025/day06.js
@@ -1,40 +1,31 @@
+const reducers = {
+  "+": arr => arr.reduce((a, b) => a + b, 0),
+  "*": arr => arr.reduce((a, b) => a * b, 1),
+};
+
 export function part1(input) {
-  let lines = input.split("\n").map(x => x.trim().split(/\s+/)); //?
+  let lines = input.split("\n").map(x => x.trim().split(/\s+/));
+  let ops = lines.pop();
   let sum = 0;
   for (let i = 0; i < lines[0].length; i++) {
-    let op = lines[lines.length - 1][i];
-    let result = op === "+" ? 0 : 1;
-    for (let x = 0; x < lines.length - 1; x++) {
-      if (op === "+") result += +lines[x][i];
-      else result *= +lines[x][i];
-    }
-    sum += result;
+    let arr = lines.map(x => +x[i]);
+    sum += reducers[ops[i]](arr);
   }
   return sum;
 }
 
 export function part2(input) {
   let lines = input.split("\n");
-  let op = "";
-  let result;
+  let ops = lines.pop();
+  let arr = [];
   let sum = 0;
-  for (let i = 0; i < lines[0].length; i++) {
-    if (op === "") {
-      op = lines[lines.length - 1][i];
-      result = op === "+" ? 0 : 1;
-    }
-    let digits = lines
-      .map(x => x[i])
-      .join("")
-      .replace(/[+*]/, "")
-      .trim();
-    if (digits === "") {
-      sum += result;
-      op = "";
-      continue;
-    }
-    if (op === "+") result += +digits;
-    else result *= +digits;
+  let reducer;
+  for (let i = 0; i <= lines[0].length; i++) {
+    let digits = lines.map(x => x[i]).join("");
+    digits = digits.trim();
+    reducer = reducers[ops[i]] || reducer;
+    if (digits) arr.push(+digits);
+    else sum += reducer(arr.splice(0));
   }
-  return sum + result;
+  return sum;
 }
