dee2e1c3acdc3afba3e88c48a568e0a9b90c3638
Shahar Talmi <shahar@wix.com>
2025-12-07 09:43:39 +0200

    refactor

--- src/2025/day07.js
+++ src/2025/day07.js
@@ -1,44 +1,31 @@
-export function part1(input) {
+function solve(input) {
   let map = input.split("\n");
-  let curr = 0;
-  let beams = new Set([map[0].indexOf("S")]);
+  let curr = map.findIndex(x => x.includes("S"));
+  let beams = new Map([[map[curr].indexOf("S"), 1]]);
   let splits = 0;
-  while (map[curr]) {
-    let next = new Set();
-    beams.forEach(x => {
-      if (map[curr][x] === ".") {
-        next.add(x);
-      } else {
-        next.add(x - 1);
-        next.add(x + 1);
-        splits++;
-      }
-    });
-    beams = next;
-    curr++;
-  }
-  return splits;
-}
-
-export function part2(input) {
-  let map = input.split("\n");
-  let curr = 0;
-  let beams = new Map([[map[0].indexOf("S"), 1]]);
   let timelines = 1;
   while (map[curr]) {
     let next = new Map();
     let add = (x, count) => next.set(x, (next.get(x) || 0) + count);
     beams.forEach((count, x) => {
-      if (map[curr][x] === ".") {
-        add(x, count);
-      } else {
+      if (map[curr][x] === ".") add(x, count);
+      else {
         add(x - 1, count);
         add(x + 1, count);
+        splits++;
         timelines += count;
       }
     });
     beams = next;
     curr++;
   }
-  return timelines;
+  return { splits, timelines };
+}
+
+export function part1(input) {
+  return solve(input).splits;
+}
+
+export function part2(input) {
+  return solve(input).timelines;
 }
