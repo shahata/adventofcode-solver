4593a7aef6bf7cd740a7f170b14a75f02308f801
Shahar Talmi <shahar@wix.com>
2025-12-11 10:22:46 +0200

    refactor

--- src/2025/day11.js
+++ src/2025/day11.js
@@ -1,29 +1,21 @@
-function memoize(fn) {
-  let memo = {};
-  return (...x) => {
-    let s = JSON.stringify(x);
-    return (memo[s] = memo[s] ?? fn(...x));
-  };
-}
+import { memoize } from "../utils/memoize.js";
 
 let solve = memoize((devices, curr, dac = true, fft = true) => {
   if (curr === "out") return dac && fft ? 1 : 0;
-  if (curr === "dac") dac = true;
-  if (curr === "fft") fft = true;
   return devices[curr].reduce(
-    (sum, next) => sum + solve(devices, next, dac, fft),
+    (sum, next) =>
+      sum + solve(devices, next, dac || curr === "dac", fft || curr === "fft"),
     0,
   );
 });
 
 function parse(input) {
-  let devices = {};
-  input.split("\n").forEach(line => {
-    let [name, rest] = line.split(": ");
-    let outputs = rest.split(" ");
-    devices[name] = outputs;
-  });
-  return devices;
+  return Object.fromEntries(
+    input.split("\n").map(line => {
+      let [name, rest] = line.split(": ");
+      return [name, rest.split(" ")];
+    }),
+  );
 }
 
 export function part1(input) {
